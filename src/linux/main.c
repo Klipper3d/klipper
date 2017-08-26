075980f234be1a407b242487d7949ba2338fdb6e
Kevin O'Connor <kevin@koconnor.net>
2017-08-26 19:47:31 -0400

    linux: WIP - add support for enabling realtime scheduling

--- src/linux/main.c
+++ src/linux/main.c
@@ -4,7 +4,9 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
+#include </usr/include/sched.h> // sched_setscheduler
 #include <stdio.h> // fprintf
+#include <string.h> // memset
 #include <unistd.h> // getopt
 #include "board/misc.h" // console_sendf
 #include "command.h" // DECL_CONSTANT
@@ -15,6 +17,25 @@ DECL_CONSTANT(MCU, "linux");
 
 
 /****************************************************************
+ * Real-time setup
+ ****************************************************************/
+
+static int
+realtime_setup(void)
+{
+    struct sched_param sp;
+    memset(&sp, 0, sizeof(sp));
+    sp.sched_priority = 1;
+    int ret = sched_setscheduler(0, SCHED_FIFO, &sp);
+    if (ret < 0) {
+        report_errno("sched_setscheduler", ret);
+        return -1;
+    }
+    return 0;
+}
+
+
+/****************************************************************
  * Startup
  ****************************************************************/
 
@@ -22,20 +43,29 @@ DECL_CONSTANT(MCU, "linux");
 int
 main(int argc, char **argv)
 {
-    int opt, watchdog = 0;
-    while ((opt = getopt(argc, argv, "w")) != -1) {
+    int opt, watchdog = 0, realtime = 0;
+    while ((opt = getopt(argc, argv, "wr")) != -1) {
         switch (opt) {
         case 'w':
             watchdog = 1;
             break;
+        case 'r':
+            realtime = 1;
+            break;
         default:
-            fprintf(stderr, "Usage: %s [-w]\n", argv[0]);
+            fprintf(stderr, "Usage: %s [-w] [-r]\n", argv[0]);
             return -1;
         }
     }
 
     if (watchdog) {
         int ret = watchdog_setup();
+        if (ret)
+            return ret;
+    }
+
+    if (realtime) {
+        int ret = realtime_setup();
         if (ret)
             return ret;
     }
