d80267735187a37c9b597b18e6b3764809faebe6
Kevin O'Connor <kevin@koconnor.net>
2018-01-26 20:19:44 -0500

    lcd_st7920: WIP - merge in Marcio's lcd updates
    Attempt to merge Marcio's lcd python demo code with Kevin's updated
    mcu code.
    
    Signed-off-by: Marcio Teixeira <marcio@alephobjects.com>
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/lcd_st7920.c
+++ src/lcd_st7920.c
@@ -12,41 +12,73 @@
 #include "sched.h" // DECL_TASK
 
 struct st7920 {
-    struct task_wake wake;
-    uint32_t nexttime;
-    uint8_t flags;
+    uint32_t next_cmd_time;
+    uint8_t is_busy;
     struct gpio_out sclk, sid;
-    uint8_t cur_pos, next_pos, end_pos;
-    uint8_t buffer[32];
 };
 
 static struct st7920 *main_st7920;
 
-#define SYNC_CMD  0xf80000UL
-#define SYNC_DATA 0xfa0000UL
+
+/****************************************************************
+ * Transmit functions
+ ****************************************************************/
+
+#define SYNC_CMD  0xf8
+#define SYNC_DATA 0xfa
+
+#define CMD_WAIT_TICKS timer_from_us(72)
+
+static inline void
+st7920_xmit_byte(struct gpio_out sclk, struct gpio_out sid, uint8_t data)
+{
+    uint8_t bits = 8, last_b = 0;
+    for (;;) {
+        if ((data ^ last_b) & 0x80)
+            gpio_out_toggle(sid);
+        gpio_out_toggle(sclk);
+        last_b = data;
+        data <<= 1;
+        if (!--bits)
+            break;
+        gpio_out_toggle(sclk);
+    }
+    gpio_out_toggle(sclk);
+}
 
 static void
-st7920_xmit(struct st7920 *s, uint32_t data)
+st7920_xmit_data(struct st7920 *s, uint8_t count, uint8_t *data)
 {
     struct gpio_out sclk = s->sclk, sid = s->sid;
-    uint8_t i, last_b = 0;
-    for (i=0; i<3; i++) {
-        uint8_t b = data >> 16, j;
-        data <<= 8;
-        for (j=0; j<8; j++) {
-            if ((b ^ last_b) & 0x80)
-                gpio_out_toggle(sid);
-            gpio_out_toggle(sclk);
-            last_b = b;
-            b <<= 1;
-            asm("nop\n nop");
-            gpio_out_toggle(sclk);
-        }
+    st7920_xmit_byte(sclk, sid, SYNC_DATA);
+    while (count--) {
+        uint8_t b = *data++;
+        st7920_xmit_byte(sclk, sid, b & 0xf0);
+        st7920_xmit_byte(sclk, sid, b << 4);
     }
-    s->nexttime = timer_read_time() + timer_from_us(72);
-    if (last_b & 0x80)
-        gpio_out_toggle(sid);
 }
+
+static void
+st7920_xmit_cmds(struct st7920 *s, uint8_t count, uint8_t *cmds)
+{
+    struct gpio_out sclk = s->sclk, sid = s->sid;
+    while (count--) {
+        uint8_t cmd = *cmds++;
+        if (s->is_busy)
+            while (timer_is_before(timer_read_time(), s->next_cmd_time))
+                ;
+        st7920_xmit_byte(sclk, sid, SYNC_CMD);
+        st7920_xmit_byte(sclk, sid, cmd & 0xf0);
+        st7920_xmit_byte(sclk, sid, cmd << 4);
+        s->next_cmd_time = timer_read_time() + CMD_WAIT_TICKS;
+        s->is_busy = 1;
+    }
+}
+
+
+/****************************************************************
+ * Interface
+ ****************************************************************/
 
 void
 command_config_st7920(uint32_t *args)
@@ -63,80 +95,38 @@ DECL_COMMAND(command_config_st7920,
              "config_st7920 oid=%c sclk_pin=%u sid_pin=%u");
 
 void
-command_st7920_burst(uint32_t *args)
+command_st7920_send_cmds(uint32_t *args)
 {
     struct st7920 *s = main_st7920;
     if (!s)
         shutdown("st7920 not configured");
     uint8_t len = args[0];
-    char *cmd_then_data = (void*)(size_t)args[1];
-    uint8_t end_pos = s->end_pos;
-    if (!len || end_pos + len + 1 > sizeof(s->buffer))
-        shutdown("st7920 buffer overflow");
-    s->buffer[end_pos] = len;
-    memcpy(&s->buffer[end_pos+1], cmd_then_data, len);
-    s->end_pos = end_pos + len + 1;
-    if (!end_pos) {
-        s->nexttime = timer_read_time();
-        sched_wake_task(&s->wake);
-    }
+    uint8_t *cmds = (void*)(size_t)args[1];
+    st7920_xmit_cmds(s, len, cmds);
 }
-DECL_COMMAND(command_st7920_burst, "st7920_burst cmd_then_data=%*s");
-
-static void
-st7920_report(uint8_t end_pos)
-{
-    sendf("st7920_position position=%c", end_pos);
-}
+DECL_COMMAND(command_st7920_send_cmds, "st7920_send_cmds cmds=%*s");
 
 void
-command_st7920_get_position(uint32_t *args)
+command_st7920_send_data(uint32_t *args)
 {
     struct st7920 *s = main_st7920;
     if (!s)
         shutdown("st7920 not configured");
-    st7920_report(s->end_pos);
+    uint8_t len = args[0];
+    uint8_t *data = (void*)(size_t)args[1];
+    st7920_xmit_data(s, len, data);
 }
-DECL_COMMAND(command_st7920_get_position, "st7920_get_position");
+DECL_COMMAND(command_st7920_send_data, "st7920_send_data data=%*s");
 
 void
 st7920_task(void)
 {
     struct st7920 *s = main_st7920;
-    if (!s || !sched_check_wake(&s->wake))
-        // Not active
+    if (!s || !s->is_busy)
         return;
-    if (timer_is_before(timer_read_time(), s->nexttime)) {
-        // Need to wait longer before writing next byte
-        sched_wake_task(&s->wake);
-        return;
-    }
-    uint32_t xmit;
-    if (s->cur_pos == s->next_pos) {
-        // Move buffer to make space for new data at end of buffer
-        uint8_t count = s->end_pos - s->next_pos;
-        if (s->next_pos && count)
-            memmove(s->buffer, &s->buffer[s->next_pos], count);
-        s->end_pos = count;
-        st7920_report(s->end_pos);
-        if (!count) {
-            // No more bytes to write
-            s->cur_pos = s->next_pos = 0;
-            return;
-        }
-        s->next_pos = s->buffer[0] + 1;
-        s->cur_pos = 2;
-        // Send command byte
-        uint8_t cmd = s->buffer[1];
-        xmit = SYNC_CMD | (unsigned)((cmd & 0xf0) << 8) | (uint8_t)(cmd << 4);
-    } else {
-        // Send data byte
-        uint8_t data = s->buffer[s->cur_pos++];
-        xmit = SYNC_DATA | (unsigned)((data & 0xf0) << 8) | (uint8_t)(data << 4);
-    }
-    // XXX - ideally xmit would be done prior to bookkeeping..
-    st7920_xmit(s, xmit);
-    sched_wake_task(&s->wake);
+    // Avoid 32bit rollover on s->next_cmd_time
+    if (!timer_is_before(timer_read_time(), s->next_cmd_time))
+        s->is_busy = 0;
 }
 DECL_TASK(st7920_task);
 
@@ -148,6 +138,5 @@ st7920_shutdown(void)
         return;
     gpio_out_write(s->sclk, 0);
     gpio_out_write(s->sid, 0);
-    s->cur_pos = s->next_pos = s->end_pos = 0;
 }
 DECL_SHUTDOWN(st7920_shutdown);
