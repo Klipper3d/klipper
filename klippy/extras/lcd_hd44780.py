c219eae32634a626053d97c9399335b1ca870722
Kevin O'Connor <kevin@koconnor.net>
2018-02-11 20:37:23 -0500

    lcd_hd44780: Extract screen updating code from low-level code

--- klippy/extras/lcd_hd44780.py
+++ klippy/extras/lcd_hd44780.py
@@ -7,11 +7,11 @@ import logging
 
 BACKGROUND_PRIORITY_CLOCK = 0x7fffffff00000000
 
-class PrinterHD44780:
+class HD44780:
     def __init__(self, config):
-        printer = config.get_printer()
+        self.printer = config.get_printer()
         # pin config
-        ppins = printer.lookup_object('pins')
+        ppins = self.printer.lookup_object('pins')
         pins = [ppins.lookup_pin('digital_out', config.get(name + '_pin'))
                 for name in ['rs', 'e', 'd4', 'd5', 'd6', 'd7']]
         mcu = None
@@ -30,67 +30,91 @@ class PrinterHD44780:
         self.mcu.add_config_object(self)
         self.cmd_queue = self.mcu.alloc_command_queue()
         self.send_data_cmd = self.send_cmds_cmd = None
-        # output buffers
-        self.pending_data = []
-        self.pending_type = None
-        # work timer
-        self.reactor = printer.get_reactor()
-        self.work_timer = self.reactor.register_timer(self.work_event)
-        self.init_timer = self.reactor.register_timer(self.init_event)
-        self.position = 0
+        # framebuffers
+        self.text_framebuffers = [
+            [bytearray([' ']*20), bytearray([0x00]*20), 0x80, 0],
+            [bytearray([' ']*20), bytearray([0x00]*20), 0x80, 0x40],
+            [bytearray([' ']*20), bytearray([0x00]*20), 0x80, 20],
+            [bytearray([' ']*20), bytearray([0x00]*20), 0x80, 0x40 + 20]]
+        self.sprite_framebuffer = [
+            [bytearray([0x00]*40), bytearray([0xff]*40), 0x40, 0]]
+        self.graphics_framebuffer = [
+            [bytearray([0x00]*16), bytearray([0xff]*16), 0x00, 0] * 64]
+        self.pending_framebuffers = (
+            self.sprite_framebuffer + self.text_framebuffers)
     def build_config(self):
         self.send_cmds_cmd = self.mcu.lookup_command(
             "hd44780_send_cmds cmds=%*s")
         self.send_data_cmd = self.mcu.lookup_command(
             "hd44780_send_data data=%*s")
+    def send(self, cmd_type, data):
+        self.mcu.send(cmd_type.encode(data),
+                      reqclock=BACKGROUND_PRIORITY_CLOCK, cq=self.cmd_queue)
+    def flush(self):
+        for fb in self.pending_framebuffers:
+            new_data, old_data, cmd, offset = fb
+            diffs = [[i, 1] for i, (nd, od) in enumerate(zip(new_data, old_data))
+                     if nd != od]
+            for i in range(len(diffs)-2, -1, -1):
+                if diffs[i][0] + 4 >= diffs[i+1][0] and diffs[i+1][1] < 16:
+                    diffs[i][1] = diffs[i+1][1] + diffs[i+1][0] - diffs[i][0]
+                    del diffs[i+1]
+            for pos, count in diffs:
+                self.send(self.send_cmds_cmd, [cmd + offset + pos])
+                self.send(self.send_data_cmd, new_data[pos:pos+count])
+            fb[1] = new_data
+        del self.pending_framebuffers[:]
+    def init(self):
+        reactor = self.printer.get_reactor()
+        for cmd in [0x33, 0x33, 0x33, 0x22]:
+            self.mcu.send(self.send_cmds_cmd.encode([cmd]), cq=self.cmd_queue)
+            reactor.pause(reactor.monotonic() + .100)
+        self.send(self.send_cmds_cmd, [0x08, 0x06, 0x10, 0x0c])
+        self.flush()
+
+class PrinterLCD:
+    def __init__(self, config):
+        printer = config.get_printer()
+        self.lcd_chip = HD44780(config)
+        # work timer
+        self.reactor = printer.get_reactor()
+        self.work_timer = self.reactor.register_timer(self.work_event)
+        self.init_timer = self.reactor.register_timer(self.init_event)
+        self.position = 0
     def printer_state(self, state):
         if state == 'ready':
             self.reactor.update_timer(self.init_timer, self.reactor.NOW)
-    def flush(self):
-        if not self.pending_data:
-            return
-        if self.pending_type == 'cmd':
-            msg = self.send_cmds_cmd.encode(self.pending_data)
-        else:
-            msg = self.send_data_cmd.encode(self.pending_data)
-        self.mcu.send(msg, reqclock=BACKGROUND_PRIORITY_CLOCK, cq=self.cmd_queue)
-        del self.pending_data[:]
-    def send_data(self, byte):
-        if self.pending_type != 'data' or len(self.pending_data) >= 16:
-            self.flush()
-            self.pending_type = 'data'
-        self.pending_data.append(byte)
-    def send_command(self, cmd):
-        if self.pending_type != 'cmd' or len(self.pending_data) >= 16:
-            self.flush()
-            self.pending_type = 'cmd'
-        self.pending_data.append(cmd)
     def init_event(self, eventtime):
         self.reactor.unregister_timer(self.init_timer)
         self.init_timer = None
-        for cmd in [0x33, 0x33, 0x33, 0x22]:
-            self.mcu.send(self.send_cmds_cmd.encode([cmd]), cq=self.cmd_queue)
-            self.reactor.pause(self.reactor.monotonic() + .100)
-        self.send_command(0x08)
-        self.send_command(0x06)
-        self.send_command(0x10)
-        self.send_command(0x01)
-        self.flush()
-        self.reactor.pause(self.reactor.monotonic() + .100)
-        self.send_command(0x0c)
-        self.flush()
+        self.lcd_chip.init()
         self.reactor.update_timer(self.work_timer, self.reactor.NOW)
         return self.reactor.NEVER
-    def work_event(self, eventtime):
-        if self.position < 40:
-            self.send_command(0x80 | self.position)
+    def update_framebuffer(self, fb, pos, data):
+        new_fb_data = fb[0]
+        len_fb_data = len(new_fb_data)
+        if new_fb_data is not fb[1]:
+            # Some changes are already pending to this buffer
+            new_fb_data[pos:pos+len(data)] = data
         else:
-            self.send_command(0xc0 | (self.position - 40))
-        self.send_data(ord(' '))
-        self.send_data(ord('#'))
-        self.flush()
+            new_fb_data = bytearray(fb[1])
+            new_fb_data[pos:pos+len(data)] = data
+            if new_fb_data == fb[1]:
+                # No update
+                return
+            fb[0] = new_fb_data
+            self.lcd_chip.pending_framebuffers.append(fb)
+        if len(new_fb_data) > len_fb_data:
+            del new_fb_data[len_fb_data:]
+    def write_str(self, x, y, data):
+        self.update_framebuffer(self.lcd_chip.text_framebuffers[y], x, data)
+    def work_event(self, eventtime):
+        x = self.position % 20
+        y = self.position // 20
+        self.write_str(x, y, " #")
+        self.lcd_chip.flush()
         self.position = (self.position + 1) % 80
         return eventtime + .100
 
 def load_config(config):
-    return PrinterHD44780(config)
+    return PrinterLCD(config)
