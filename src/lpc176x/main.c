65613aeddfb9ef86905cb1dade9e773a02ef3c27
Kevin O'Connor <kevin@koconnor.net>
2018-05-08 15:25:22 -0400

    lpc176x: Enable watchdog
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/lpc176x/main.c
+++ src/lpc176x/main.c
@@ -12,6 +12,29 @@ DECL_CONSTANT(MCU, "lpc176x");
 
 
 /****************************************************************
+ * watchdog handler
+ ****************************************************************/
+
+void
+watchdog_reset(void)
+{
+    LPC_WDT->WDFEED = 0xaa;
+    LPC_WDT->WDFEED = 0x55;
+}
+DECL_TASK(watchdog_reset);
+
+void
+watchdog_init(void)
+{
+    LPC_WDT->WDTC = 4000000 / 2; // 500ms timeout
+    LPC_WDT->WDCLKSEL = 1<<31; // Lock to internal RC
+    LPC_WDT->WDMOD = 0x03; // select reset and enable
+    watchdog_reset();
+}
+DECL_INIT(watchdog_init);
+
+
+/****************************************************************
  * misc functions
  ****************************************************************/
 
