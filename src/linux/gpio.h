7388a6f71d781b9bc051f8e7eda9dfc1fe0814c4
Ricardo Amezquita <ramezquitao@gmail.com>
2018-06-17 19:57:58 -0500

    linux: Add support for bi-directional SPI communication
    Signed-off-by: Ricardo Amézquita <ramezquitao@cihologramas.com>
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/linux/gpio.h
+++ src/linux/gpio.h
@@ -21,6 +21,7 @@ void gpio_adc_cancel_sample(struct gpio_adc g);
 
 struct spi_config {
     int fd;
+    int rate;
 };
 struct spi_config spi_setup(uint32_t bus, uint8_t mode, uint32_t rate);
 void spi_prepare(struct spi_config config);
