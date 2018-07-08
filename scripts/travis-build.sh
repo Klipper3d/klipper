6f6d047277d1a3c9ce99557ba6eaed14055a46b7
Kevin O'Connor <kevin@koconnor.net>
2018-07-08 09:29:13 -0400

    travis-build: Narrow portions of test run with "set +x"
    No need to include all the test preparation in the verbose output - as
    that can make the output more confusing.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -35,13 +35,13 @@ finish_test()
 # Check for whitespace errors
 ######################################################################
 
-start_test check_whitespace "Check whitespace"
 WS_DIRS="config/ docs/ klippy/ scripts/ src/ test/"
 WS_EXCLUDE="-path scripts/kconfig -prune"
 WS_FILES="-o -iname '*.[csh]' -o -name '*.py' -o -name '*.sh'"
 WS_FILES="$WS_FILES -o -name '*.md' -o -name '*.cfg'"
 WS_FILES="$WS_FILES -o -name '*.test' -o -name '*.config'"
 WS_FILES="$WS_FILES -o -iname '*.lds' -o -iname 'Makefile' -o -iname 'Kconfig'"
+start_test check_whitespace "Check whitespace"
 eval find $WS_DIRS $WS_EXCLUDE $WS_FILES | xargs ./scripts/check_whitespace.py
 finish_test check_whitespace "Check whitespace"
 
@@ -54,15 +54,15 @@ DICTDIR=${BUILD_DIR}/dict
 mkdir -p ${DICTDIR}
 
 for TARGET in test/configs/*.config ; do
-    start_test mcu_compile "$TARGET"
     make clean
     make distclean
     unset CC
+    start_test mcu_compile "$TARGET"
     cp ${TARGET} .config
     make olddefconfig
     make V=1
-    cp out/klipper.dict ${DICTDIR}/$(basename ${TARGET} .config).dict
     finish_test mcu_compile "$TARGET"
+    cp out/klipper.dict ${DICTDIR}/$(basename ${TARGET} .config).dict
 done
 
 
