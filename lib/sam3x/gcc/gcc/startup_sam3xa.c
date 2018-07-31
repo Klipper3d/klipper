5e9463369a959f81ab2be3f268317c64580f5dfb
Kevin O'Connor <kevin@koconnor.net>
2018-07-31 10:52:56 -0400

    lib: Modify SAM3X CMSIS files to work with GCC's lto feature
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- lib/sam3x/gcc/gcc/startup_sam3xa.c
+++ lib/sam3x/gcc/gcc/startup_sam3xa.c
@@ -132,7 +132,7 @@ void CAN0_Handler   ( void ) __attribute__ ((weak, alias("Dummy_Handler")));
 void CAN1_Handler   ( void ) __attribute__ ((weak, alias("Dummy_Handler")));
 
 /* Exception Table */
-__attribute__ ((section(".vectors")))
+__attribute__ ((section(".vectors"))) __attribute__((externally_visible))
 const DeviceVectors exception_table = {
 
         /* Configure Initial Stack Pointer, using linker-generated symbols */
