3689dc887dbade8f1ec39ce511faf76911115464
Kevin O'Connor <kevin@koconnor.net>
2018-08-17 12:32:45 -0400

    avr: Drop support for watchdog interrupt capability
    It's not necessary to raise an interrupt on a watchdog event and some
    AVR chips do not support the capability.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/avr/watchdog.c
+++ src/avr/watchdog.c
@@ -1,31 +1,18 @@
 // Initialization of AVR watchdog timer.
 //
-// Copyright (C) 2016  Kevin O'Connor <kevin@koconnor.net>
+// Copyright (C) 2016-2018  Kevin O'Connor <kevin@koconnor.net>
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include <avr/interrupt.h> // WDT_vect
 #include <avr/wdt.h> // wdt_enable
-#include "command.h" // shutdown
+#include "command.h" // DECL_COMMAND_FLAGS
 #include "irq.h" // irq_disable
 #include "sched.h" // DECL_TASK
-
-static uint8_t watchdog_shutdown;
-
-ISR(WDT_vect)
-{
-    watchdog_shutdown = 1;
-    shutdown("Watchdog timer!");
-}
 
 void
 watchdog_reset(void)
 {
     wdt_reset();
-    if (watchdog_shutdown) {
-        WDTCSR = 1<<WDIE;
-        watchdog_shutdown = 0;
-    }
 }
 DECL_TASK(watchdog_reset);
 
@@ -34,7 +21,6 @@ watchdog_init(void)
 {
     // 0.5s timeout, interrupt and system reset
     wdt_enable(WDTO_500MS);
-    WDTCSR = 1<<WDIE;
 }
 DECL_INIT(watchdog_init);
 
