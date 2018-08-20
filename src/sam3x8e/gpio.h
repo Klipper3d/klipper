109eff0191fef3b4e939b3d506d6c31fcbd6ab0c
Kevin O'Connor <kevin@koconnor.net>
2018-08-20 16:27:46 -0400

    sam3x8e: Add support for gpio_x_reset()
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/sam3x8e/gpio.h
+++ src/sam3x8e/gpio.h
@@ -10,6 +10,7 @@ struct gpio_out {
     uint32_t bit;
 };
 struct gpio_out gpio_out_setup(uint8_t pin, uint8_t val);
+void gpio_out_reset(struct gpio_out g, uint8_t val);
 void gpio_out_toggle_noirq(struct gpio_out g);
 void gpio_out_toggle(struct gpio_out g);
 void gpio_out_write(struct gpio_out g, uint8_t val);
@@ -19,6 +20,7 @@ struct gpio_in {
     uint32_t bit;
 };
 struct gpio_in gpio_in_setup(uint8_t pin, int8_t pull_up);
+void gpio_in_reset(struct gpio_in g, int8_t pull_up);
 uint8_t gpio_in_read(struct gpio_in g);
 
 struct gpio_adc {
