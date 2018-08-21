07acc2bbd06d6165972eefaf50714191db4f9b7d
Miguel Almeida <ma@blockstec.com>
2018-08-21 20:49:32 +0100

    More grammar corrections

--- klippy/extras/filament_runout_sensor.py
+++ klippy/extras/filament_runout_sensor.py
@@ -1,4 +1,4 @@
-# Support for multiple filament runout sensors
+# Support for multiple filament run out sensors
 #
 # Copyright (C) 2018  Miguel Moitinho <miguel@moitinho.net>
 # Copyright (C) 2018  Kevin O'Connor <kevin@koconnor.net>
@@ -14,9 +14,9 @@ class FilamentRunoutSensor:
 
         buttons = self.printer.try_load_module(config, "buttons")
 
-        buttons.register_buttons([config.get('pin')], self.runnout_callback)
+        buttons.register_buttons([config.get('pin')], self.runout_callback)
 
-    def runnout_callback(self, eventtime, state):
+    def runout_callback(self, eventtime, state):
         self.gcode.respond_info("Sensor {name} state {state}".format(
             name=self.name,
             state=state
