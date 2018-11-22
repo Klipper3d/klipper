4bbd6310864b231f26c3c069052a610a2178a456
Kevin O'Connor <kevin@koconnor.net>
2018-11-21 20:33:44 -0500

    lpc176x: Add an enable_peripheral_clock() helper function
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/lpc176x/internal.h
+++ src/lpc176x/internal.h
@@ -2,6 +2,10 @@
 #define __LPC176X_INTERNAL_H
 // Local definitions for lpc176x code
 
+#define PCLK_TIMER0 1
+#define PCLK_UART0 3
+#define PCLK_ADC 12
+void enable_peripheral_clock(uint32_t pclk);
 void gpio_peripheral(int bank, int pin, int func, int pullup);
 
 #endif // internal.h
