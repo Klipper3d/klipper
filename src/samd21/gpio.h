58d61c788701845346594de64f63df2d4206cd80
Kevin O'Connor <kevin@koconnor.net>
2018-11-28 19:56:41 -0500

    samd21: Add support for basic i2c support (write only)
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/samd21/gpio.h
+++ src/samd21/gpio.h
@@ -21,4 +21,13 @@ struct gpio_in gpio_in_setup(uint8_t pin, int8_t pull_up);
 void gpio_in_reset(struct gpio_in g, int8_t pull_up);
 uint8_t gpio_in_read(struct gpio_in g);
 
+struct i2c_config {
+    uint8_t addr;
+};
+
+struct i2c_config i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr);
+void i2c_write(struct i2c_config config, uint8_t write_len, uint8_t *write);
+void i2c_read(struct i2c_config config, uint8_t reg_len, uint8_t *reg
+              , uint8_t read_len, uint8_t *read);
+
 #endif // gpio.h
