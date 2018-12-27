3e96d500bb058e32e9bad6d2b0054840d36909a5
Trevor Jones <trevorjones141@gmail.com>
2018-12-26 18:08:55 -0700

    example pin config for mmu2 and start stubbing out some filament selector code.
    Signed-off-by: TrevJonez <trevorjones141@gmail.com>

---
+++ klippy/extras/ips_filament_selector.py
@@ -0,0 +1,21 @@
+# Filament selector based on Idler/Pulley/Selector style control
+#
+# Copyright (C) 2018  Trevor Jones <trevorjones141@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import logging
+import stepper, homing
+
+
+class IdlerPulleyFilamentSelector:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.gcode = self.printer.lookup_object('gcode')
+        # Setup axis rails
+        self.rails = [stepper.LookupMultiRail(config.getsection('stepper_' + n))
+                      for n in ['i', 'p', 's']]
+        for rail, axis in zip(self.rails, 'ips'):
+            rail.setup_itersolve('cartesian_stepper_alloc', axis)
+
+def load_config(config):
+    return IdlerPulleyFilamentSelector(config)
