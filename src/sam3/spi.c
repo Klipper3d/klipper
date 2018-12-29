bf196c302870325cc4562d9ae571231138f85f28
Kevin O'Connor <kevin@koconnor.net>
2018-12-28 21:18:39 -0500

    sam3: Add additional SPI and USART buses to spi.c
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/sam3/spi.c
+++ src/sam3/spi.c
@@ -25,9 +25,13 @@ struct spi_info {
 static const struct spi_info spi_bus[] = {
 #if CONFIG_MACH_SAM3X8E
     { SPI0, ID_SPI0, GPIO('A', 25), GPIO('A', 26), GPIO('A', 27), 'A', 'A' },
+    { USART0, ID_USART0, GPIO('A', 10), GPIO('A', 11), GPIO('A', 17), 'A', 'B'},
+    { USART1, ID_USART1, GPIO('A', 12), GPIO('A', 13), GPIO('A', 16), 'A', 'A'},
+    { USART2, ID_USART2, GPIO('B', 21), GPIO('B', 20), GPIO('B', 24), 'A', 'A'},
 #elif CONFIG_MACH_SAM4E8E
     { USART0, ID_USART0, GPIO('B', 0), GPIO('B', 1), GPIO('B', 13), 'C', 'C' },
     { USART1, ID_USART1, GPIO('A', 21), GPIO('A', 22), GPIO('A', 23), 'A', 'A'},
+    { SPI, ID_SPI, GPIO('A', 12), GPIO('A', 13), GPIO('A', 14), 'A', 'A' },
 #endif
 };
 
