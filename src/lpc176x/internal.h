8f7fc4e6a9ed6b5df39ede47c2accea000db86f9
Kevin O'Connor <kevin@koconnor.net>
2019-01-04 10:41:46 -0500

    lpc176x: Move ADC code to new adc.c file
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/lpc176x/internal.h
+++ src/lpc176x/internal.h
@@ -2,6 +2,10 @@
 #define __LPC176X_INTERNAL_H
 // Local definitions for lpc176x code
 
+#define GPIO(PORT, NUM) ((PORT) * 32 + (NUM))
+#define GPIO2PORT(PIN) ((PIN) / 32)
+#define GPIO2BIT(PIN) (1<<((PIN) % 32))
+
 #define PCLK_TIMER0 1
 #define PCLK_UART0 3
 #define PCLK_ADC 12
