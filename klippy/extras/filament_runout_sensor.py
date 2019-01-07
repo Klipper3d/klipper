b8f8d51c003a9b2155d2d0f472060550f7760ac1
Paulo Drugos <paulodrugos@gmail.com>
2019-01-07 18:17:35 -0200

    Changed how the module should be configurated in printer.cfg

--- klippy/extras/filament_runout_sensor.py
+++ klippy/extras/filament_runout_sensor.py
@@ -9,41 +9,32 @@
 class FilamentRunoutSensor:
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.name = config.get_name().split(' ')[-1]
+        self.pin = config.get('pin')
+        self.last_state = 0
         buttons = self.printer.try_load_module(config, "buttons")
-        FilamentRunoutSensor.last_states = {}
-        self.pins = config.get('pins').split(',')
-        for pin in self.pins:
-            FilamentRunoutSensor.last_states[pin] = 0
-            f = self.create_callback(pin)
-            buttons.register_buttons([pin], f)
+        buttons.register_buttons([self.pin], self.runout_callback)
         gcode = self.printer.lookup_object('gcode')
+        gcode.register_command("QUERY_FILAMENT_SENSOR",None)
         gcode.register_command("QUERY_FILAMENT_SENSOR", self.cmd_QUERY_FILAMENT_SENSOR,
                                desc=self.cmd_QUERY_FILAMENT_SENSOR_help)
-    cmd_QUERY_FILAMENT_SENSOR_help = "Report on the status of each filament runout sensor"
+    cmd_QUERY_FILAMENT_SENSOR_help = "Report on the status of filament runout sensors"
     def cmd_QUERY_FILAMENT_SENSOR(self, params):
         gcode = self.printer.lookup_object('gcode')
-        try:
-            s=0
-            for pin in FilamentRunoutSensor.last_states:
-                response = "Sensor "+str(s)+": "
-                if FilamentRunoutSensor.last_states[pin]:
-                    response += "Filament present"
-                else:
-                    response += "Filament NOT present"
-                gcode.respond(response)
-                s+=1
-        except Exception as e:
-            gcode.respond(str(e))
-    def create_callback(self,pin):
-        def _f(eventtime, state):
-            toolhead = self.printer.lookup_object('toolhead')
-            gcode = self.printer.lookup_object('gcode')
-            FilamentRunoutSensor.last_states[pin] = state
-            if state:
-                return
-            if toolhead.get_status(eventtime)['status'] == "Printing":
-                gcode.respond_info("Filament runout sensor triggered! Pausing print job.")
-                gcode.respond_info("action:pause")
-        return _f
-def load_config(config):
+        response = self.name+": "
+        if self.last_state:
+            response += "Filament present"
+        else:
+            response += "Filament NOT present"
+        gcode.respond(response)
+    def runout_callback(self,eventtime, state):
+        toolhead = self.printer.lookup_object('toolhead')
+        gcode = self.printer.lookup_object('gcode')
+        self.last_state = state
+        if state:
+            return
+        if toolhead.get_status(eventtime)['status'] == "Printing":
+            gcode.respond_info("Filament runout sensor triggered! Pausing print job.")
+            gcode.respond_info("action:pause")
+def load_config_prefix(config):
     return FilamentRunoutSensor(config)
