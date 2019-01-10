0119e42d13e6088ceb020c7f885d2a64afecbf98
Kevin O'Connor <kevin@koconnor.net>
2019-01-10 12:17:57 -0500

    homing: Always start endstop checking before the nominal move start
    Make sure the endstop checking is started prior to any movement.  This
    helps ensure the verify_movement check is accurate.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/homing.py
+++ klippy/homing.py
@@ -6,6 +6,7 @@
 import logging, math
 
 HOMING_STEP_DELAY = 0.00000025
+HOMING_START_DELAY = 0.001
 ENDSTOP_SAMPLE_TIME = .000015
 ENDSTOP_SAMPLE_COUNT = 4
 
@@ -55,6 +56,7 @@ class Homing:
             mcu_endstop.home_start(
                 print_time, ENDSTOP_SAMPLE_TIME, ENDSTOP_SAMPLE_COUNT,
                 min_step_dist / speed)
+        self.toolhead.dwell(HOMING_START_DELAY, check_stall=False)
         # Issue move
         error = None
         try:
