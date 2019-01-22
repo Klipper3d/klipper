064d88ee3aca038a9c389674f3289d9dbde9a2b9
Mustafa YILDIZ <mydiz@hotmail.com>
2019-01-22 19:18:10 +0300

    Code fixed according to Kevin's comments. Added RESET_FILAMENT_WIDTH_SENSOR command to reset readings. M407 command changed with QUERY_FILAMENT_WIDTH
    Signed-off-by: Mustafa YILDIZ <mydiz@hotmail.com>

--- klippy/extras/filament_width_sensor.py
+++ klippy/extras/filament_width_sensor.py
@@ -1,6 +1,6 @@
 # Support for filament width sensor
 #
-# Copyright (C) 2019  Mustafa YILDIZ <mydiz(at)hotmail.com>
+# Copyright (C) 2019  Mustafa YILDIZ <mydiz@hotmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
@@ -14,9 +14,9 @@ class FilamentWidthSensor:
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
         self.pin = config.get('pin')
-        self.nominal_filament_dia = float(config.get('default_nominal_filament_dia'))
-        self.measurement_delay_mm = float(config.get('measurement_delay_cm')) * 10
-        self.measurement_max_difference = float(config.get('max_difference'))
+        self.nominal_filament_dia = config.getfloat('default_nominal_filament_dia', above=1.0)
+        self.measurement_delay = config.getfloat('measurement_delay', above=0.)
+        self.measurement_max_difference = config.getfloat('max_difference', above=0.)
         self.max_diameter = self.nominal_filament_dia + self.measurement_max_difference
         self.min_diameter = self.nominal_filament_dia - self.measurement_max_difference
         #Filament array [position, filamentWidth]
@@ -39,31 +39,33 @@ class FilamentWidthSensor:
         # Load printer objects
         self.gcode = self.printer.lookup_object('gcode')
         self.toolhead = self.printer.lookup_object('toolhead')
-        self.heater_bed = self.printer.lookup_object('heater_bed', None)
-        self.gcode.register_command('M407', self.cmd_M407)
+        self.gcode.register_command('QUERY_FILAMENT_WIDTH', self.cmd_M407)
+        self.gcode.register_command('RESET_FILAMENT_WIDTH_SENSOR', self.cmd_ClearFilamentArray)
 
         # Start extrude factor update timer
         self.reactor.update_timer(self.extrude_factor_update_timer, self.reactor.NOW)
 
     def adc_callback(self, read_time, read_value):
         # read sensor value
-        self.lastFilamentWidthReading = float("{0:.2f}".format(max(.00001, min(.99999, read_value)) * 5))
-        pos = self.toolhead.get_position()
-        last_epos = pos[3]
+        self.lastFilamentWidthReading = round (read_value * 5, 2)
+
+    def update_filament_array(self,last_epos):
         #fill array
         if len(self.filament_array) > 0:
             # Get last reading position in array & calculate next reading position
             next_reading_position = self.filament_array[-1][0] + MEASUREMENT_INTERVAL_MM
-            if next_reading_position <= (last_epos + self.measurement_delay_mm):
-                self.filament_array.append([last_epos + self.measurement_delay_mm, self.lastFilamentWidthReading])
+            if next_reading_position <= (last_epos + self.measurement_delay):
+                self.filament_array.append([last_epos + self.measurement_delay, self.lastFilamentWidthReading])
         else:
             # add first item to array
-            self.filament_array.append([self.measurement_delay_mm + last_epos, self.lastFilamentWidthReading])
+            self.filament_array.append([self.measurement_delay + last_epos, self.lastFilamentWidthReading])
 
     def extrude_factor_update_event(self, eventtime):
         # Update extrude factor
         pos = self.toolhead.get_position()
         last_epos = pos[3]
+        # Update filament array for lastFilamentWidthReading
+        self.update_filament_array(last_epos)
         # Does filament exists
         if self.lastFilamentWidthReading > 0.5:
             if len(self.filament_array) > 0:
@@ -74,11 +76,11 @@ class FilamentWidthSensor:
                     filament_width = item[1]
                     if (filament_width <= self.max_diameter) and (filament_width >= self.min_diameter):
                         percentage = round(self.nominal_filament_dia / filament_width * 100)
-                        self.gcode.cmd_M221({'S': percentage})
+                        self.gcode.run_script("M221 S"+str(percentage))
                     else:
-                        self.gcode.cmd_M221({'S': 100})
+                        self.gcode.run_script("M221 S100")
         else:
-            self.gcode.cmd_M221({'S': 100})
+            self.gcode.run_script("M221 S100")
             self.filament_array = []
         return eventtime + 1
 
@@ -89,6 +91,9 @@ class FilamentWidthSensor:
         else:
             response += "Filament NOT present"
         self.gcode.respond(response)
+    def cmd_ClearFilamentArray(self, params):
+        self.filament_array = []
+        self.gcode.respond("Filament width measurements cleared!")
 
 def load_config(config):
     return FilamentWidthSensor(config)
