d9a53c55bd6d5e6f6168891f8bf26d03ef67f736
Matt Janus <fraggin-right-github@variabl.es>
2019-02-12 10:52:25 -0500

    added copyright

--- src/spi_software.c
+++ src/spi_software.c
@@ -1,6 +1,6 @@
 // Software SPI emulation
 //
-// Copyright (C) 2019  XXX
+// Copyright (C) 2016,2017  Kevin O'Connor <kevin@koconnor.net>
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
