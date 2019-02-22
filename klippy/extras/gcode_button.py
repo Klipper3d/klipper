f4265302e90c0d5843e7ce6926e1f4f51aeb1566
Alec Plumb <alec@etherwalker.com>
2019-02-22 10:05:03 -0800

    Execute gcode

--- klippy/extras/gcode_button.py
+++ klippy/extras/gcode_button.py
@@ -7,6 +7,7 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
+
 class GCodeButton:
     def __init__(self, config):
         self.printer = config.get_printer()
@@ -14,12 +15,16 @@ class GCodeButton:
         self.pin = config.get('pin')
         self.last_state = 0
         buttons = self.printer.try_load_module(config, "buttons")
-        buttons.register_buttons([self.pin], self.runout_callback)
+        buttons.register_buttons([self.pin], self.button_callback)
         gcode = self.printer.lookup_object('gcode')
+        self.press_gcode = config.get('press_gcode', '')
+        self.release_gcode = config.get('release_gcode', '')
         gcode.register_mux_command("QUERY_BUTTON", "BUTTON", self.name,
                                         self.cmd_QUERY_BUTTON,
                                         desc=self.cmd_QUERY_BUTTON_help)
+
     cmd_QUERY_BUTTON_help = "Report on the state of a button"
+
     def cmd_QUERY_BUTTON(self, params):
         gcode = self.printer.lookup_object('gcode')
         response = self.name+": "
@@ -28,11 +33,15 @@ class GCodeButton:
         else:
             response += "RELEASED"
         gcode.respond(response)
-    def runout_callback(self,eventtime, state):
+
+    def button_callback(self, eventtime, state):
         toolhead = self.printer.lookup_object('toolhead')
         gcode = self.printer.lookup_object('gcode')
         self.last_state = state
-        self.cmd_QUERY_BUTTON({})
-        # TODO: execute gcode from config
+        if state and bool(self.press_gcode):
+           gcode.run_script_from_command(self.press_gcode)
+        if (not state) and bool(self.release_gcode):
+            gcode.run_script_from_command(self.release_gcode)
+
 def load_config_prefix(config):
     return GCodeButton(config)
