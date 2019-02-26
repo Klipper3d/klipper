64aaaeadee7f4b4537b1dedeb330f295ef444ad6
Rui Caridade <rui.mcbc@gmail.com>
2019-02-26 21:20:03 +0000

    Code documentation

--- klippy/extras/bed_screws.py
+++ klippy/extras/bed_screws.py
@@ -124,32 +124,43 @@ class BedScrews:
     cmd_BED_SCREWS_CALCULATE_help = "Tool to help adjust bed leveling screws by " \
                                     "calculating the number of turns to level it."
     def cmd_BED_SCREWS_CALCULATE(self, params):
+        # Don't execute this command if BED_SCREWS_ADJUST is running
         if self.state is not None:
             raise self.gcode.error(
                 "Already in bed_screws helper; use ABORT to exit before using this command")
-        threads_factor = {3: 0.5, 4: 0.7, 5: 0.8}
         toolhead = self.printer.lookup_object('toolhead')
+        # Factor used for M3, M4 and M5 bed screws
+        threads_factor = {3: 0.5, 4: 0.7, 5: 0.8}
+        # Prepare a place to save the Z read values
         screws_info = []
+        # Perform a G28
         self.gcode.run_script_from_command("G28")
+        # Probe the bed in the screw define points
         for coord, name in self.screws:
             pos = toolhead.get_position()
+            # First move Z up
             self.move((pos[0], pos[1], self.horizontal_move_z), self.speed)
+            # Move to probe position
             self.move((coord[0], coord[1], self.horizontal_move_z), self.speed)
+            # Probe the bed
             self.gcode.run_script_from_command("PROBE")
             pos = toolhead.get_position()
+            # Save read Z value
             screws_info.append((pos[2], coord, name))
-
+        # When finish move Z up
         pos = toolhead.get_position()
         self.move((pos[0], pos[1], self.horizontal_move_z), self.speed)
-
+        # Process the read Z values and
         for i, screw_info in enumerate(screws_info):
             if i == 0:
+                # First screw is the base position used for comparison
                 z_base, coord_base, name_base = screw_info
+                # Show the results
                 self.gcode.respond_info("%s (Base): X %.1f, Y %.1f, Z %.5f" %
                                         (name_base, coord_base[0], coord_base[1], z_base))
             else:
+                # Calculate how knob must me adjusted for other positions
                 z, coord, name = screw_info
-
                 diff = z_base - z
                 if abs(diff) < 0.001:
                     adjust = 0
@@ -162,7 +173,7 @@ class BedScrews:
                 full_turns = math.trunc(adjust)
                 decimal_part = adjust - full_turns
                 minutes = round(decimal_part * 60, 0)
-
+                # Show the results
                 self.gcode.respond_info("%s : X %.1f, Y %.1f, Z %.5f : Adjust -> %s %02d:%02d" % (
                       name, coord[0], coord[1], z, sign, abs(full_turns), abs(minutes)))
 
