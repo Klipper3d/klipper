51051478af133c2faa4f257e5e0c49008f2d731f
Kevin O'Connor <kevin@koconnor.net>
2019-02-27 13:31:31 -0500

    st7920: Wrap code to 80 columns
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/display/st7920.py
+++ klippy/extras/display/st7920.py
@@ -76,8 +76,8 @@ class ST7920:
             if new_data == old_data:
                 continue
             # Find the position of all changed bytes in this framebuffer
-            diffs = [[i, 1] for i, (nd, od) in enumerate(zip(new_data, old_data))
-                     if nd != od]
+            diffs = [[i, 1] for i, (n, o) in enumerate(zip(new_data, old_data))
+                     if n != o]
             # Batch together changes that are close to each other
             for i in range(len(diffs)-2, -1, -1):
                 pos, count = diffs[i]
