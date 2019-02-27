f7d8fde50246f6a9f43e768bb41a1e32129f4f4b
Kevin O'Connor <kevin@koconnor.net>
2019-02-27 18:47:28 -0500

    tmc2208: Report write-only registers in DUMP_TMC
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/tmc2208.py
+++ klippy/extras/tmc2208.py
@@ -352,6 +352,13 @@ class TMC2208:
         self.printer.lookup_object('toolhead').get_last_move_time()
         gcode = self.printer.lookup_object('gcode')
         logging.info("DUMP_TMC %s", self.name)
+        gcode.respond_info("========== Write-only registers ==========")
+        for reg_name, val in self.regs.items():
+            if reg_name not in ReadRegisters:
+                msg = self.fields.pretty_format(reg_name, val)
+                logging.info(msg)
+                gcode.respond_info(msg)
+        gcode.respond_info("========== Queried registers ==========")
         for reg_name in ReadRegisters:
             try:
                 val = self.get_register(reg_name)
