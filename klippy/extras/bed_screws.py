56257817d8664e159005f6eddc18d60343bd5d64
Rui Caridade <rui.mcbc@gmail.com>
2019-02-28 10:36:40 +0000

    Make "screw_thread' optional and with a default

--- klippy/extras/bed_screws.py
+++ klippy/extras/bed_screws.py
@@ -45,7 +45,7 @@ class BedScrews:
         threads = {'CW-M3': 0, 'CCW-M3': 1, 'CW-M4': 2, 'CCW-M4': 3,
                    'CW-M5': 4, 'CCW-M5': 5}
         self.thread = config.getchoice('screw_thread', threads,
-                                       default=0)
+                                       default='CW-M3')
         # Register command
         self.gcode = self.printer.lookup_object('gcode')
         self.gcode.register_command("BED_SCREWS_ADJUST",
