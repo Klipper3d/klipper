587db5c315f856f5d62328725b7b73659677e34f
Kevin O'Connor <kevin@koconnor.net>
2019-06-18 13:24:54 -0400

    tmc2660: Don't allow configuration of DEDGE
    Setting DEDGE would just break the code.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/tmc2660.py
+++ klippy/extras/tmc2660.py
@@ -242,7 +242,6 @@ class TMC2660:
         self.get_microsteps = mh.get_microsteps
         self.get_phase = mh.get_phase
         set_config_field = self.fields.set_config_field
-        set_config_field(config, "DEDGE", 0)
         set_config_field(config, "INTPOL", True, 'interpolate')
         # CHOPCONF
         set_config_field(config, "TBL", 2)
