8e0165e563fd4a52baa97b1e25449edb1b5c5e72
Kevin O'Connor <kevin@koconnor.net>
2019-06-25 21:25:01 -0400

    homing: Implement homing via new toolhead "drip" movement
    Rework the low-level implementation of homing movement.  The existing
    mechanism buffers all homing movement into the micro-controller prior
    to starting the home.  Replace with a system that buffers all movement
    into the host look-ahead buffer and then "drip feed" those moves to
    the micro-controllers.  Then clear the host look-ahead buffer when all
    endstops trigger.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/bltouch.py
+++ klippy/extras/bltouch.py
@@ -157,10 +157,11 @@ class BLTouchEndstopWrapper:
             if s.get_mcu_position() == mcu_pos:
                 raise homing.EndstopError("BLTouch failed to deploy")
         self.mcu_endstop.home_finalize()
-    def home_start(self, print_time, sample_time, sample_count, rest_time):
+    def home_start(self, print_time, sample_time, sample_count, rest_time,
+                   notify=None):
         rest_time = min(rest_time, ENDSTOP_REST_TIME)
-        self.mcu_endstop.home_start(
-            print_time, sample_time, sample_count, rest_time)
+        self.mcu_endstop.home_start(print_time, sample_time, sample_count,
+                                    rest_time, notify=notify)
     def get_position_endstop(self):
         return self.position_endstop
     cmd_BLTOUCH_DEBUG_help = "Send a command to the bltouch for debugging"
