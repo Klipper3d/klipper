1487f8a257a7f9c620f163620adf16171581e206
Kevin O'Connor <kevin@koconnor.net>
2019-07-25 14:55:10 -0400

    stm32f4: Add support for GPIO
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

---
+++ src/stm32f4/gpio.h
@@ -0,0 +1,24 @@
+#ifndef __STM32F4_GPIO_H
+#define __STM32F4_GPIO_H
+
+#include <stdint.h> // uint32_t
+
+struct gpio_out {
+    void *regs;
+    uint32_t bit;
+};
+struct gpio_out gpio_out_setup(uint32_t pin, uint32_t val);
+void gpio_out_reset(struct gpio_out g, uint32_t val);
+void gpio_out_toggle_noirq(struct gpio_out g);
+void gpio_out_toggle(struct gpio_out g);
+void gpio_out_write(struct gpio_out g, uint32_t val);
+
+struct gpio_in {
+    void *regs;
+    uint32_t bit;
+};
+struct gpio_in gpio_in_setup(uint32_t pin, int32_t pull_up);
+void gpio_in_reset(struct gpio_in g, int32_t pull_up);
+uint8_t gpio_in_read(struct gpio_in g);
+
+#endif // gpio.h
