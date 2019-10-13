2893cca2e53aa765d5dad77a5db7d28966de2c06
Simo Apell <34631067+sapell@users.noreply.github.com>
2019-10-13 14:08:40 +0300

    Create extruder_stepper.py, update example-extras.cfg
    extruder_stepper: Implement support for dual stepper extruders
    
    Instantiates a stepper object based on extruder_stepper config section. Adds the stepper to the extruder in the connect event handler add_stepper_to_extruder.
    
    Update example-extras.cfg to include an example of extruder_stepper config.
    
    Todo: Error handling for invalid config items.
    
    Signed-off-by: Simo Apell <simo.apell@live.fi>

---
+++ klippy/extras/extruder_stepper.py
@@ -0,0 +1,25 @@
+# Code for handling printer nozzle extruders
+#
+# Copyright (C) 2019-2020  Simo Apell <simo.apell@live.fi>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import logging
+import stepper, extruder
+
+class ExtruderStepper:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.name = config.get_name()
+        self.extruder_name = config.get('add_to_extruder')
+        self.stepper = stepper.PrinterStepper(config)
+        self.printer.register_event_handler("klippy:connect", self.add_stepper_to_extruder)
+    def add_stepper_to_extruder(self):
+        extruder_list = extruder.get_printer_extruders(self.printer)
+        for e in extruder_list:
+            if e.name == self.extruder_name:
+                e.add_stepper(self.stepper)
+                break
+
+def load_config(config):
+    return ExtruderStepper(config)
+
