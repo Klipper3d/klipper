Klipper3d/klipper

commit f723a3141e02d038c8f12aca477d3c38c8283f4a

atsam: Allow PB4-PB7,PB10-PB12 to be used as gpio on sam4
A separate "sysio" register must be set in order to use some port B
pins on the sam4 mcu.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/atsam/gpio.c
@@ -60,6 +60,9 @@ set_pull_up(Pio *regs, uint32_t bit, int32_t pull_up)
         regs->PIO_PUDR = bit;
         regs->PIO_PPDDR = bit;
     }
+    // Check if this pin is a "system IO pin" and disable if so
+    if (regs == PIOB && (bit & 0x1cf0))
+        MATRIX->CCFG_SYSIO |= bit;
 #endif
 }
 

commit 081c24326dbf256b06d1abf9927500cf7dd4ae39

atsam: sysio support is only available on sam4s
Fix sam4e build error introduced in commit f723a314.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/atsam/gpio.c
@@ -60,6 +60,8 @@ set_pull_up(Pio *regs, uint32_t bit, int32_t pull_up)
         regs->PIO_PUDR = bit;
         regs->PIO_PPDDR = bit;
     }
+#endif
+#if CONFIG_MACH_SAM4S
     // Check if this pin is a "system IO pin" and disable if so
     if (regs == PIOB && (bit & 0x1cf0))
         MATRIX->CCFG_SYSIO |= bit;