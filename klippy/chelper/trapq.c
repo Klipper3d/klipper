32f61b01e93f91f3a02733c93007d84adbb9fa98
Kevin O'Connor <kevin@koconnor.net>
2019-11-07 11:32:53 -0500

    trapq: Use C99 method for declaring struct members in trapq_append()
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/chelper/trapq.c
+++ klippy/chelper/trapq.c
@@ -28,8 +28,8 @@ trapq_append(struct trapq *tq, double print_time
              , double axes_r_x, double axes_r_y, double axes_r_z
              , double start_v, double cruise_v, double accel)
 {
-    struct coord start_pos = { x: start_pos_x, y: start_pos_y, z: start_pos_z };
-    struct coord axes_r = { x: axes_r_x, y: axes_r_y, z: axes_r_z };
+    struct coord start_pos = { .x=start_pos_x, .y=start_pos_y, .z=start_pos_z };
+    struct coord axes_r = { .x=axes_r_x, .y=axes_r_y, .z=axes_r_z };
     if (accel_t) {
         struct move *m = move_alloc();
         m->print_time = print_time;
