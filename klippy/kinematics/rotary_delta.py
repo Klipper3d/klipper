5345c0a5a426f28d4dba89a056ca97e05d1477e9
Kevin O'Connor <kevin@koconnor.net>
2019-12-12 20:44:26 -0500

    rotary_delta: wip - don't assume arms are fully extended as part of homing

--- klippy/kinematics/rotary_delta.py
+++ klippy/kinematics/rotary_delta.py
@@ -92,8 +92,9 @@ class RotaryDeltaKinematics:
         # All axes are homed simultaneously
         homing_state.set_axes([0, 1, 2])
         forcepos = list(self.home_position)
-        min_angles = [-.5 * math.pi] * 3
-        forcepos[2] = self.calibration.actuator_to_cartesian(min_angles)[2]
+        #min_angles = [-.5 * math.pi] * 3
+        #forcepos[2] = self.calibration.actuator_to_cartesian(min_angles)[2]
+        forcepos[2] = -1.
         homing_state.home_rails(self.rails, forcepos, self.home_position)
     def _motor_off(self, print_time):
         self.limit_xy2 = -1.
