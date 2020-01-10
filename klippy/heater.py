8ed511395cbddbc89dfe1dea4ef6ae81d484cf9b
Arksine <arksine.code@gmail.com>
2020-01-09 19:28:22 -0500

    heater: add support for BME280
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- klippy/heater.py
+++ klippy/heater.py
@@ -259,6 +259,7 @@ class PrinterHeaters:
         self.printer.try_load_module(config, "thermistor")
         self.printer.try_load_module(config, "adc_temperature")
         self.printer.try_load_module(config, "spi_temperature")
+        self.printer.try_load_module(config, "bme280")
         sensor_type = config.get('sensor_type')
         if sensor_type not in self.sensor_factories:
             raise self.printer.config_error(
