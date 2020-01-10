20ba3964661840fe1d5ba65b57014f21c63ed9c4
Arksine <arksine.code@gmail.com>
2020-01-09 19:25:51 -0500

    linux:  implement i2c
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- src/linux/gpio.h
+++ src/linux/gpio.h
@@ -35,4 +35,13 @@ struct gpio_pwm {
 struct gpio_pwm gpio_pwm_setup(uint8_t pin, uint32_t cycle_time, uint16_t val);
 void gpio_pwm_write(struct gpio_pwm g, uint16_t val);
 
+struct i2c_config {
+    int fd;
+};
+
+struct i2c_config i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr);
+void i2c_write(struct i2c_config config, uint8_t write_len, uint8_t *write);
+void i2c_read(struct i2c_config config, uint8_t reg_len, uint8_t *reg
+              , uint8_t read_len, uint8_t *read);
+
 #endif // gpio.h
