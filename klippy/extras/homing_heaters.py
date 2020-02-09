41e73f0becaa0cdffaeb5253cc378673d7954715
FanDjango <FanDjango@users.noreply.github.com>
2020-02-09 15:06:47 +0100

    BLTHS PART1 HEATERS (turn off heaters during homing/probing)

---
+++ klippy/extras/homing_heaters.py
@@ -0,0 +1,54 @@
+# Heater handling on homing moves
+#
+# Copyright (C) 2016-2018  Kevin O'Connor <kevin@koconnor.net>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+import logging
+
+class HomingHeaters:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        self.printer.register_event_handler("homing:move_begin",
+                                            self.handle_homing_move_begin)
+        self.printer.register_event_handler("homing:move_end",
+                                            self.handle_homing_move_end)
+        self.heaters_to_disable = config.get("heaters", "")
+        self.steppers_needing_quiet = config.get("steppers", "")
+        self.last_target = {}
+        self.pheater = self.printer.lookup_object('heater')
+
+    def check_eligible(self, endstops):
+        flaky_steppers = [n.strip()
+                         for n in self.steppers_needing_quiet.split(',')]
+        if flaky_steppers == [""]:
+            return True
+        endstop_instances, dummy = zip(*endstops)
+        steppers_being_homed = list()
+        for n in endstop_instances:
+            steppers_being_homed = steppers_being_homed + n.get_steppers()
+        steppernames_being_homed = list()
+        for n in steppers_being_homed:
+            steppernames_being_homed.append(n.get_name())
+        return any(x in flaky_steppers for x in steppernames_being_homed)
+    def bld_heater_list(self):
+        heaters = [n.strip() for n in self.heaters_to_disable.split(',')]
+        if heaters == [""]:
+            heaters = self.pheater.available_heaters
+        return heaters
+    def handle_homing_move_begin(self, endstops):
+        if not self.check_eligible(endstops):
+            return
+        for heater_name in self.bld_heater_list():
+            heater = self.pheater.lookup_heater(heater_name)
+            self.last_target[heater_name] = heater.get_temp(0)[1]
+            heater.set_temp(0.)
+    def handle_homing_move_end(self, endstops):
+        if not self.check_eligible(endstops):
+            return
+        for heater_name in self.bld_heater_list():
+            heater = self.pheater.lookup_heater(heater_name)
+            heater.set_temp(self.last_target[heater_name])
+
+def load_config(config):
+    return HomingHeaters(config)
