380ba2a618ed3ccacc79ded93f44d80cc93a2894
Arksine <arksine.code@gmail.com>
2020-04-25 14:19:00 -0400

    scripts:  add libusb-1.0 to install scripts
    The hid-flash tool depends on libusb-1.0-0-dev
    
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -20,7 +20,7 @@ install_packages()
     PKGLIST="${PKGLIST} avrdude gcc-avr binutils-avr avr-libc"
     # ARM chip installation and building
     PKGLIST="${PKGLIST} stm32flash libnewlib-arm-none-eabi"
-    PKGLIST="${PKGLIST} gcc-arm-none-eabi binutils-arm-none-eabi"
+    PKGLIST="${PKGLIST} gcc-arm-none-eabi binutils-arm-none-eabi libusb-1.0"
 
     # Update system package info
     report_status "Running apt-get update..."
