40e92961a10bd695df3ae634eea4a19357c458d0
Dushyant Ahuja <dusht.ahuja@gmail.com>
2020-05-04 07:24:35 +0100

    Update to save and load variables from a separate file
    Update to save and load variables from a separate file - needs a config section with the filename of the separate file
    [save_variables]
    filename: variable.cfg
    
    Variables can be saved using SAVE_VARIABLE VARIABLE=temp2 VALUE=23
    and loaded using LOAD_VARIABLE VARIABLE=temp2
    
    Still WIP - as I'm not sure how the variables that are loaded can be used in macros.

--- klippy/extras/save_variables.py
+++ klippy/extras/save_variables.py
@@ -2,26 +2,63 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
-#import math, logging
+import sys,os,logging, ConfigParser
 
-class SaveVariables:
+error = ConfigParser.Error
+
+class LoadSaveVariables:
+    error = ConfigParser.Error
     def __init__(self, config):
         self.printer = config.get_printer()
-	self.name = config.get_name()
         self.gcode = self.printer.lookup_object('gcode')
+        self.name = config.get_name()
+        self.cfilename = config.get('filename')
+        self.filename = os.path.join(os.path.expanduser("~"), self.cfilename)
         self.gcode.register_command(
-            'SAVE_VARIABLE', self.cmd_SAVE_VARIABLE,True,
+            'SAVE_VARIABLE', self.cmd_SAVE_VARIABLE,
             desc=self.cmd_SAVE_VARIABLE_help)
-    cmd_SAVE_VARIABLE_help = "Save arbitrary variables in the config file"
+        self.gcode.register_command(
+            'LOAD_VARIABLE', self.cmd_LOAD_VARIABLE,
+            desc=self.cmd_LOAD_VARIABLE_help)
+    cmd_LOAD_VARIABLE_help = "Load arbitrary variables from disk"
+    cmd_SAVE_VARIABLE_help = "Save arbitrary variables to disk"
+    def cmd_LOAD_VARIABLE(self, params):
+        try:
+            self.variablefile = ConfigParser.ConfigParser()
+            self.variablefile.read(self.filename)
+        except error as e:
+            msg = e.message + "\nUnable to parse existing variable file"
+            logging.exception(msg)
+            raise self.gcode.error(msg)
+        self.variable_name = self.gcode.get_str('VARIABLE', params)
+        try:
+            self.variable_value = self.variablefile.get('Variables',self.variable_name)
+        except error as e:
+            msg = e.message + "\nUnable to parse variables"
+            logging.exception(msg)
+            raise self.gcode.error(msg)
+        #self.gcode.respond_info(self.filename)
+        self.gcode.respond_info(self.variable_name)
+        self.gcode.respond_info(self.variable_value)
+
     def cmd_SAVE_VARIABLE(self, params):
-        variable_name = self.gcode.get_str('VARIABLE', params)
-        variable_value = self.gcode.get_str('VALUE', params)
-        self.gcode.respond_info(
-            "The SAVE_CONFIG command will update the printer config file\n"
-            "with the new variable")
-        # Store results for SAVE_CONFIG
-        configfile = self.printer.lookup_object('configfile')
-        configfile.set(self.name, variable_name, variable_value)
+        self.variable_name = self.gcode.get_str('VARIABLE', params)
+        self.variable_value = self.gcode.get_str('VALUE', params)
+        try:
+            self.variablefile = ConfigParser.ConfigParser()
+            self.variablefile.read(self.filename)
+        except error as e:
+            msg = "Unable to parse existing variable file"
+            logging.exception(msg)
+            raise self.gcode.error(msg)
+        try:
+            self.variablefile.set('Variables',self.variable_name,self.variable_value)
+            self.variablefile.write(open(self.filename, "w"))
+        except error as e:
+            msg = e.message + "\nUnable to save variable"
+            logging.exception(msg)
+            raise self.gcode.error(msg)
+        self.gcode.respond_info("Variable Saved")
 
 def load_config(config):
-    return SaveVariables(config)
+    return LoadSaveVariables(config)
