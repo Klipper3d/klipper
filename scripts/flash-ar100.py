1c50f6782d4fa4bce8876ed5f99357c69f0a44b1
Elias Bakken <elias@iagent.no>
2020-07-10 14:29:22 +0200

    Fixed flasher script

--- scripts/flash-ar100.py
+++ scripts/flash-ar100.py
@@ -4,9 +4,16 @@ import mmap
 import argparse
 import sys
 
-SRAM_A2_BASE    = 0x00050000
-SRAM_A2_LIMIT   = 0x00054000
-SRAM_A2_SIZE    = SRAM_A2_LIMIT - SRAM_A2_BASE
+EXCEPTIONS_BASE     = 0x00040000
+EXCEPTIONS_LIMIT    = 0x00042000
+EXCEPTIONS_SIZE     = EXCEPTIONS_LIMIT - EXCEPTIONS_BASE
+EXCEPTIONS_JUMP     = 0x00010000 # All exceptions reset program
+NR_OF_EXCEPTIONS    = 14
+
+SRAM_A2_BASE        = 0x00050000
+SRAM_A2_LIMIT       = 0x00054000
+SRAM_A2_SIZE        = SRAM_A2_LIMIT - SRAM_A2_BASE
+
 
 R_CPU_CFG_PAGE_BASE  = 0x01F01000
 R_CPU_CFG_PAGE_LIMIT = 0x01F02000
@@ -18,10 +25,17 @@ parser.add_argument('filename', nargs='?', help='binary file to write to memory'
 parser.add_argument('--reset', action='store_true', help='reset the AR100 cpu and exit')
 args = parser.parse_args()
 
-
-#firmware_file = "/home/klipper/klipper/out/ar100.bin"
-
-# TODO: write exception vectors
+def write_exception_vectors():
+    print("Writing exception vectors")
+    with open("/dev/mem", "w+b") as f:
+        exc = mmap.mmap(f.fileno(),
+            length=EXCEPTIONS_SIZE,
+            offset=EXCEPTIONS_BASE)
+        for i in range(NR_OF_EXCEPTIONS):
+            add = i*0x100
+            exc[add:add+4] = ((EXCEPTIONS_JUMP-add)>>2).to_bytes(4, byteorder='little')
+        exc.close()
+    assert_deassert_reset(0)
 
 def assert_deassert_reset(ass):
     with open("/dev/mem", "w+b") as f:
@@ -39,14 +53,13 @@ def write_file(filename):
         data = fw.read()
         if len(data) > SRAM_A2_SIZE:
             print("File does not fit in memory")
+            assert_deassert_reset(0)
             sys.exit(1)
-        assert_deassert_reset(1)
         print("Writing file to SRAM A2")
         with open("/dev/mem", "w+b") as f:
             sram_a2 = mmap.mmap(f.fileno(), length=SRAM_A2_SIZE, offset=SRAM_A2_BASE)
             sram_a2[0:len(data)] = data
             sram_a2.close()
-        assert_deassert_reset(0)
 
 
 if args.reset:
@@ -56,4 +69,7 @@ if args.reset:
     sys.exit(0)
 
 if args.filename:
+    assert_deassert_reset(1)
+    write_exception_vectors()
     write_file(args.filename)
+    assert_deassert_reset(0)
