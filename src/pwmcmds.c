636b6bc838b0f9bafba6e1be82c450551da06549
Pascal Pieper <accounts@pascalpieper.de>
2020-09-11 15:05:10 +0200

    First draft of PWM queueing after stepper example

--- src/pwmcmds.c
+++ src/pwmcmds.c
@@ -6,15 +6,24 @@
 
 #include "basecmd.h" // oid_alloc
 #include "board/gpio.h" // struct gpio_pwm
+#include "board/irq.h" // irq_disable
 #include "command.h" // DECL_COMMAND
 #include "sched.h" // sched_add_timer
+
+struct pwm_value {
+	uint32_t waketime;
+	uint16_t value;
+	struct pwm_value *next;
+};
 
 struct pwm_out_s {
     struct timer timer;
     struct gpio_pwm pin;
     uint32_t max_duration;
-    uint16_t value, default_value;
+    uint16_t value, default_value;		//TODO: Value redundant here?
+    struct pwm_value *first, **plast;
 };
+
 
 static uint_fast8_t
 pwm_end_event(struct timer *timer)
@@ -26,11 +35,31 @@ static uint_fast8_t
 pwm_event(struct timer *timer)
 {
     struct pwm_out_s *p = container_of(timer, struct pwm_out_s, timer);
-    gpio_pwm_write(p->pin, p->value);
-    if (p->value == p->default_value || !p->max_duration)
-        return SF_DONE;
-    p->timer.waketime += p->max_duration;
-    p->timer.func = pwm_end_event;
+    struct pwm_value *v = p->first;
+
+    if(!v)
+    {
+    	// no next pwm value, queue is empty
+    	return SF_DONE;
+    }
+
+    gpio_pwm_write(p->pin, v->value);
+
+    p->first = v->next;
+    pwm_free(v);
+
+    if(p->first){
+    	//next event scheduled
+    	p->timer.waketime += p->first->waketime;
+    } else {
+        if (p->value == p->default_value || !p->max_duration)
+            return SF_DONE;
+
+        //nothing scheduled, so lets start the safety timeout
+		p->timer.waketime += p->max_duration;
+		p->timer.func = pwm_end_event;
+    }
+
     return SF_RESCHEDULE;
 }
 
@@ -43,6 +72,8 @@ command_config_pwm_out(uint32_t *args)
     p->pin = pin;
     p->default_value = args[4];
     p->max_duration = args[5];
+
+    pwm_request_size(sizeof(struct pwm_value));
 }
 DECL_COMMAND(command_config_pwm_out,
              "config_pwm_out oid=%c pin=%u cycle_ticks=%u value=%hu"
@@ -52,11 +83,34 @@ void
 command_schedule_pwm_out(uint32_t *args)
 {
     struct pwm_out_s *p = oid_lookup(args[0], command_config_pwm_out);
-    sched_del_timer(&p->timer);
-    p->timer.func = pwm_event;
-    p->timer.waketime = args[1];
-    p->value = args[2];
-    sched_add_timer(&p->timer);
+    /*
+		sched_del_timer(&p->timer);
+		p->timer.func = pwm_event;
+		p->timer.waketime = args[1];
+		p->value = args[2];
+		sched_add_timer(&p->timer);
+     */
+
+    struct pwm_value* v = pwm_alloc();
+    v->waketime = args[1];		// TODO: is this redundant?
+    v->value = args[2];
+    v->next = NULL;
+
+    irq_disable();
+    if(p->first) {
+    	// there exists an element in queue
+    	*p->plast = v;	//enqueue new v into the last's "next" element
+    }
+    else {
+    	// no first element set
+    	p->first = v;
+    	p->timer.waketime = args[1];
+    	p->timer.func = pwm_event;
+        sched_add_timer(&p->timer);
+    }
+    p->plast = &v->next;	//plast pointing to the new last's "next" element
+    irq_enable();
+
 }
 DECL_COMMAND(command_schedule_pwm_out,
              "schedule_pwm_out oid=%c clock=%u value=%hu");
