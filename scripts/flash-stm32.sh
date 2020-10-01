75da25167c3e86bcca29d8c892b6ada55337dfca
Elias Bakken <elias@iagent.no>
2020-10-01 22:39:16 +0200

    Added install-script for Recore A3, using py2.7

--- scripts/flash-stm32.sh
+++ scripts/flash-stm32.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gpioset 1 197=1
 gpioset 1 196=0
-stm32flash -i -196,196 -w $1 -v -g 0x00 /dev/ttyS3
+stm32flash -i -196,196 -w $1 -v -g 0x00 /dev/ttyS4
 gpioset 1 197=0
-stm32flash -i -196,196 /dev/ttyS3
+stm32flash -i -196,196 /dev/ttyS4
