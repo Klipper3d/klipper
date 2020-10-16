0abb7a0f0d17eb4c458bdbfae81b81921edc3714
Pascal Pieper <accounts@pascalpieper.de>
2020-10-16 08:58:29 +0200

    Testing forced queue flushing
    Signed-off-by: Pascal Pieper <accounts@pascalpieper.de>

--- klippy/mcu.py
+++ klippy/mcu.py
@@ -263,15 +263,14 @@ class MCU_pwm:
         
         # FIXME: Dirty hack to slow down soft-pwm
         if self._hardware_pwm:
-            #print_time may be too near (or in the past!)
-            #now_on_mcu = self._mcu._reactor.
-            curtime = self._mcu.get_printer().get_reactor().monotonic()
-            print_time = max(print_time, self._mcu.estimated_print_time(curtime))
+            print("Hardwarepwm request clock: " + str(clock))
             data = (self._set_cmd_id, self._oid, clock, value)
             ret = self._ffi_lib.pwmchannel_queue_msg(
                 self._pwmqueue, data, len(data), clock)
             if ret:
                 raise error("Internal error in pwm send")
+            #this feels somewhat hardcoded
+            self._mcu.flush_moves(print_time)
         else:
             self._set_cmd_if_soft_pwm.send([self._oid, clock, value],
                                minclock=self._last_clock, reqclock=clock)
