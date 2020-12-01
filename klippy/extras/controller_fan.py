Klipper3d/klipper

commit a7e9050439f539a85d8fc9a4a78e794d6f3620ca

heater_fan: Improve timing of fan commands
Don't use the reactor eventtime to schedule micro-controller commands
as that time may have low accuracy.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/extras/heater_fan.py
@@ -17,22 +17,27 @@ class PrinterHeaterFan:
         self.heaters = []
         self.fan = fan.Fan(config, default_shutdown_speed=1.)
         self.fan_speed = config.getfloat("fan_speed", 1., minval=0., maxval=1.)
+        self.last_speed = 0.
     def handle_ready(self):
         pheaters = self.printer.lookup_object('heaters')
         self.heaters = [pheaters.lookup_heater(n.strip())
                         for n in self.heater_name.split(',')]
         reactor = self.printer.get_reactor()
-        reactor.register_timer(self.callback, reactor.NOW)
+        reactor.register_timer(self.callback, reactor.monotonic()+PIN_MIN_TIME)
     def get_status(self, eventtime):
         return self.fan.get_status(eventtime)
     def callback(self, eventtime):
-        power = 0.
+        speed = 0.
         for heater in self.heaters:
             current_temp, target_temp = heater.get_temp(eventtime)
             if target_temp or current_temp > self.heater_temp:
-                power = self.fan_speed
-        print_time = self.fan.get_mcu().estimated_print_time(eventtime)
-        self.fan.set_speed(print_time + PIN_MIN_TIME, power)
+                speed = self.fan_speed
+        if speed == self.last_speed:
+            return
+        self.last_speed = speed
+        curtime = self.printer.get_reactor().monotonic()
+        print_time = self.fan.get_mcu().estimated_print_time(curtime)
+        self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
         return eventtime + 1.
 
 def load_config_prefix(config):

commit a4f7ad9ffce68aaa84481083b045b29180e1435e

heater_fan: Fix typo causing excessive cpu usage
Commit error introduced in a7e90504 and f261a468.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/extras/controller_fan.py
@@ -49,12 +49,11 @@ class ControllerFan:
         elif self.last_on < self.idle_timeout:
             speed = self.idle_speed
             self.last_on += 1
-        if speed == self.last_speed:
-            return
-        self.last_speed = speed
-        curtime = self.printer.get_reactor().monotonic()
-        print_time = self.fan.get_mcu().estimated_print_time(curtime)
-        self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
+        if speed != self.last_speed:
+            self.last_speed = speed
+            curtime = self.printer.get_reactor().monotonic()
+            print_time = self.fan.get_mcu().estimated_print_time(curtime)
+            self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
         return eventtime + 1.
 
 def load_config_prefix(config):

klippy/extras/heater_fan.py
@@ -32,12 +32,11 @@ class PrinterHeaterFan:
             current_temp, target_temp = heater.get_temp(eventtime)
             if target_temp or current_temp > self.heater_temp:
                 speed = self.fan_speed
-        if speed == self.last_speed:
-            return
-        self.last_speed = speed
-        curtime = self.printer.get_reactor().monotonic()
-        print_time = self.fan.get_mcu().estimated_print_time(curtime)
-        self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
+        if speed != self.last_speed:
+            self.last_speed = speed
+            curtime = self.printer.get_reactor().monotonic()
+            print_time = self.fan.get_mcu().estimated_print_time(curtime)
+            self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
         return eventtime + 1.
 
 def load_config_prefix(config):