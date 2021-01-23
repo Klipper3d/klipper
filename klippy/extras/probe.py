607964f2af0b5d8104a14c530a772f301a6907e9
Stephan3 <stephan.oelze@gmail.com>
2021-01-23 10:09:22 +0000

    Add samples_result option to [probe] last
    Signed-off-by: Stephan3 <stephan.oelze@gmail.com>

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -39,7 +39,7 @@ class PrinterProbe:
         self.sample_count = config.getint('samples', 1, minval=1)
         self.sample_retract_dist = config.getfloat('sample_retract_dist', 2.,
                                                    above=0.)
-        atypes = {'median': 'median', 'average': 'average'}
+        atypes = {'median': 'median', 'average': 'average', 'last': 'last'}
         self.samples_result = config.getchoice('samples_result', atypes,
                                                'average')
         self.samples_tolerance = config.getfloat('samples_tolerance', 0.100,
@@ -180,6 +180,8 @@ class PrinterProbe:
         # Calculate and return result
         if samples_result == 'median':
             return self._calc_median(positions)
+        else if samples_result == 'last':
+            return positions[-1]
         return self._calc_mean(positions)
     cmd_PROBE_help = "Probe Z-height at current XY position"
     def cmd_PROBE(self, gcmd):
