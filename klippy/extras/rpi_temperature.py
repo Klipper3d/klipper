29831a2f69a0724dcd2a63e84fc552c819eb8c3a
Kevin O'Connor <kevin@koconnor.net>
2021-01-24 13:57:58 -0500

    rpi_temperature: Minor formatting changes
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/rpi_temperature.py
+++ klippy/extras/rpi_temperature.py
@@ -19,23 +19,18 @@ class RPiTemperature:
 
         self.printer.add_object("rpi_temperature " + self.name, self)
         self.sample_timer = self.reactor.register_timer(
-            self._sample_pi_temperature
-        )
+            self._sample_pi_temperature)
         try:
             self.file_handle = open(PROC_TEMP_FILE, "r")
         except:
-            raise config.error(
-                "Unable to open temperature file '%s'" % (PROC_TEMP_FILE)
-            )
+            raise config.error("Unable to open temperature file '%s'"
+                               % (PROC_TEMP_FILE,))
 
-        self.printer.register_event_handler(
-            "klippy:connect", self.handle_connect
-        )
-
+        self.printer.register_event_handler("klippy:connect",
+                                            self.handle_connect)
 
     def handle_connect(self):
         self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
-
 
     def setup_minmax(self, min_temp, max_temp):
         self.min_temp = min_temp
@@ -55,14 +50,12 @@ class RPiTemperature:
 
         if self.temp < self.min_temp:
             self.printer.invoke_shutdown(
-                "RPi temperature %0.1f below minimum"
-                " temperature of %0.1f." % (self.temp, self.min_temp,)
-            )
+                "RPi temperature %0.1f below minimum temperature of %0.1f."
+                % (self.temp, self.min_temp,))
         if self.temp > self.max_temp:
             self.printer.invoke_shutdown(
-                "RPi temperature %0.1f above maximum"
-                " temperature of %0.1f." % (self.temp, self.max_temp,)
-            )
+                "RPi temperature %0.1f above maximum temperature of %0.1f."
+                % (self.temp, self.max_temp,))
 
         measured_time = self.reactor.monotonic()
         self._callback(measured_time, self.temp)
@@ -77,6 +70,4 @@ class RPiTemperature:
 def load_config(config):
     # Register sensor
     pheaters = config.get_printer().load_object(config, "heaters")
-    pheaters.add_sensor_factory(
-        "rpi_temperature", RPiTemperature
-    )
+    pheaters.add_sensor_factory("rpi_temperature", RPiTemperature)
