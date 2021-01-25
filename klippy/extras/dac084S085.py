c5fb2c08c67c65f80ba274530418bdb087f23e9a
lorenzing <lorenzo.franco94@gmail.com>
2021-01-25 17:50:42 +0100

    Update dac084S085.py

--- klippy/extras/dac084S085.py
+++ klippy/extras/dac084S085.py
@@ -1,6 +1,6 @@
 # SPI DAC DAC084S085 implementation
 #
-# Copyright (C) 2020  Kevin O'Connor <kevin@koconnor.net>
+# Copyright (C) 2017,2018  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 from . import bus
@@ -8,14 +8,16 @@ import time
 
 class dac084S085:
     def __init__(self, config):
-        self.spi = bus.MCU_SPI_from_config(config, 1, pin_option="enable_pin", default_speed=10000000)
+        self.spi = bus.MCU_SPI_from_config(
+            config, 1, pin_option="enable_pin", default_speed=10000000)
 
 	channel = [0,3,2,1,0 ]
         for i in range(1,5):
-            vref = config.getint('motor%d' % (i,), None, minval=0., maxval=255)
+            vref = config.getint('motor%d' % (i,), None,
+                                  minval=0., maxval=255)
 
             if vref is not None:
-            buff = 0x01 << 12
+                buff = 0x01 << 12
 	    	buff |= (channel[i]) << 14
 	    	buff |= vref << 4
 	    	self.spi.spi_send([buff])
