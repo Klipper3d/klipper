3c2546e2a8e46edcc80f8cc21d942b3668219cdb
Alan Lord <alanslists@gmail.com>
2021-01-30 12:05:37 +0000

    Add configuration description of DS18B20 to Config_Reference and add support for a configurable reporting interval. Signed off by Alan Lord <alanslists@gmail.com>

--- klippy/extras/ds18b20.py
+++ klippy/extras/ds18b20.py
@@ -5,7 +5,10 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging
 
-REPORT_FREQ = 5
+DS18_REPORT_TIME = 3
+# Temperature can be sampled at any time but conversion time is ~750ms so make
+# sure not to read too often.
+DS18_MIN_REPORT_TIME = 1
 
 class DS18B20:
     def __init__(self, config):
@@ -14,6 +17,11 @@ class DS18B20:
         self.sensor_id = config.get("serial_no")
         self.min_temp = config.getfloat('min_temp')
         self.max_temp = config.getfloat('max_temp')
+        self.report_time = config.getfloat(
+            'ds18_report_time',
+            DS18_REPORT_TIME,
+            minval=DS18_MIN_REPORT_TIME
+        )
         #TODO Will need to somehow identify the correct mcu...?
         self.all_mcus = [
             m for n, m in self.printer.lookup_objects(module='mcu')]
@@ -28,7 +36,7 @@ class DS18B20:
             self.sensor_id.encode("hex")))
 
         clock = self.mcu.get_query_slot(self.oid)
-        self._report_clock = self.mcu.seconds_to_clock(REPORT_FREQ)
+        self._report_clock = self.mcu.seconds_to_clock(self.report_time)
         self.mcu.add_config_cmd("query_ds18b20 oid=%d clock=%u rest_ticks=%u"
             " min_value=%d max_value=%d" % (
                 self.oid, clock, self._report_clock,
@@ -49,7 +57,7 @@ class DS18B20:
         self.printer.invoke_async_shutdown(msg)
 
     def get_report_time_delta(self):
-        return REPORT_FREQ
+        return self.report_time
 
     def setup_callback(self, cb):
         self._callback = cb
