decc8d307608ef60ae170f82643c6a676c357eb8
Elias Bakken <elias@iagent.no>
2021-01-31 13:14:11 +0100

    Adding support foor STM32F031x6
    Add support for alt usart 1
    
    Do not select all functions on 16 K mcu
    
    Signed-off-by: Elias Bakken <elias@iagent.no>

--- src/stm32/spi.c
+++ src/stm32/spi.c
@@ -41,10 +41,12 @@ DECL_CONSTANT_STR("BUS_PINS_spi4", "PE13,PE14,PE12");
 #define SPI_FUNCTION GPIO_FUNCTION(CONFIG_MACH_STM32F0 ? 0 : 5)
 
 static const struct spi_info spi_bus[] = {
+  { SPI1, GPIO('A', 6), GPIO('A', 7), GPIO('A', 5), SPI_FUNCTION },
+  { SPI1, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), SPI_FUNCTION },
+#ifdef SPI2
     { SPI2, GPIO('B', 14), GPIO('B', 15), GPIO('B', 13), SPI_FUNCTION },
-    { SPI1, GPIO('A', 6), GPIO('A', 7), GPIO('A', 5), SPI_FUNCTION },
     { SPI2, GPIO('C', 2), GPIO('C', 3), GPIO('B', 10), SPI_FUNCTION },
-    { SPI1, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), SPI_FUNCTION },
+#endif
 #ifdef SPI3
     { SPI3, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), GPIO_FUNCTION(6) },
  #if CONFIG_MACH_STM32F4
--- src/stm32/stm32f0_serial.c
+++ src/stm32/stm32f0_serial.c
@@ -13,9 +13,15 @@
 
 // Select the configured serial port
 #if CONFIG_SERIAL_PORT == 1
-  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA10,PA9");
-  #define GPIO_Rx GPIO('A', 10)
-  #define GPIO_Tx GPIO('A', 9)
+  #if CONFIG_STM32_SERIAL_USART1_ALT
+    DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA15,PA14");
+    #define GPIO_Rx GPIO('A', 15)
+    #define GPIO_Tx GPIO('A', 14)
+  #else
+    DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA10,PA9");
+    #define GPIO_Rx GPIO('A', 10)
+    #define GPIO_Tx GPIO('A', 9)
+  #endif
   #define USARTx USART1
   #define USARTx_IRQn USART1_IRQn
 #elif CONFIG_SERIAL_PORT == 2
