Klipper3d/klipper

commit 73b78af6b226c2826b98197e38cd76a448f9498f

chelper: Rework check for building C code
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/chelper/__init__.py
@@ -178,31 +178,26 @@ defs_all = [
     defs_kin_winch, defs_kin_extruder, defs_kin_shaper,
 ]
 
+# Update filenames to an absolute path
+def get_abs_files(srcdir, filelist):
+    return [os.path.join(srcdir, fname) for fname in filelist]
+
 # Return the list of file modification times
-def get_mtimes(srcdir, filelist):
+def get_mtimes(filelist):
     out = []
     for filename in filelist:
-        pathname = os.path.join(srcdir, filename)
         try:
-            t = os.path.getmtime(pathname)
+            t = os.path.getmtime(filename)
         except os.error:
             continue
         out.append(t)
     return out
 
 # Check if the code needs to be compiled
-def check_build_code(srcdir, target, sources, cmd, other_files=[]):
-    src_times = get_mtimes(srcdir, sources + other_files + [__file__])
-    obj_times = get_mtimes(srcdir, [target])
-    if not obj_times or max(src_times) > min(obj_times):
-        logging.info("Building C code module %s", target)
-        srcfiles = [os.path.join(srcdir, fname) for fname in sources]
-        destlib = os.path.join(srcdir, target)
-        res = os.system(cmd % (destlib, ' '.join(srcfiles)))
-        if res:
-            msg = "Unable to build C code module (error=%s)" % (res,)
-            logging.error(msg)
-            raise Exception(msg)
+def check_build_code(sources, target):
+    src_times = get_mtimes(sources)
+    obj_times = get_mtimes([target])
+    return not obj_times or max(src_times) > min(obj_times)
 
 def check_gcc_option(option):
     cmd = "%s %s -S -o /dev/null -xc /dev/null > /dev/null 2>&1" % (
@@ -219,15 +214,20 @@ def get_ffi():
     global FFI_main, FFI_lib, pyhelper_logging_callback
     if FFI_lib is None:
         srcdir = os.path.dirname(os.path.realpath(__file__))
-        if check_gcc_option(SSE_FLAGS):
-            cmd = "%s %s %s" % (GCC_CMD, SSE_FLAGS, COMPILE_ARGS)
-        else:
-            cmd = "%s %s" % (GCC_CMD, COMPILE_ARGS)
-        check_build_code(srcdir, DEST_LIB, SOURCE_FILES, cmd, OTHER_FILES)
+        srcfiles = get_abs_files(srcdir, SOURCE_FILES)
+        ofiles = get_abs_files(srcdir, OTHER_FILES)
+        destlib = get_abs_files(srcdir, [DEST_LIB])[0]
+        if check_build_code(srcfiles+ofiles+[__file__], destlib):
+            if check_gcc_option(SSE_FLAGS):
+                cmd = "%s %s %s" % (GCC_CMD, SSE_FLAGS, COMPILE_ARGS)
+            else:
+                cmd = "%s %s" % (GCC_CMD, COMPILE_ARGS)
+            logging.info("Building C code module %s", DEST_LIB)
+            os.system(cmd % (destlib, ' '.join(srcfiles)))
         FFI_main = cffi.FFI()
         for d in defs_all:
             FFI_main.cdef(d)
-        FFI_lib = FFI_main.dlopen(os.path.join(srcdir, DEST_LIB))
+        FFI_lib = FFI_main.dlopen(destlib)
         # Setup error logging
         def logging_callback(msg):
             logging.error(FFI_main.string(msg))
@@ -250,7 +250,11 @@ HC_CMD = "sudo %s/hub-ctrl -h 0 -P 2 -p %d"
 def run_hub_ctrl(enable_power):
     srcdir = os.path.dirname(os.path.realpath(__file__))
     hubdir = os.path.join(srcdir, HC_SOURCE_DIR)
-    check_build_code(hubdir, HC_TARGET, HC_SOURCE_FILES, HC_COMPILE_CMD)
+    srcfiles = get_abs_files(hubdir, HC_SOURCE_FILES)
+    destlib = get_abs_files(hubdir, [HC_TARGET])[0]
+    if check_build_code(srcfiles, destlib):
+        logging.info("Building C code module %s", HC_TARGET)
+        os.system(HC_COMPILE_CMD % (destlib, ' '.join(srcfiles)))
     os.system(HC_CMD % (hubdir, enable_power))
 
 

commit 1049282eec1eb1717df6ac49c0e533bb034e1961

chelper: Fix check for failed code build
Commit 73b78af6 inadvertently removed the check for a successful gcc
compilation.  Add the check back in.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

klippy/chelper/__init__.py
@@ -199,11 +199,20 @@ def check_build_code(sources, target):
     obj_times = get_mtimes([target])
     return not obj_times or max(src_times) > min(obj_times)
 
+# Check if the current gcc version supports a particular command-line option
 def check_gcc_option(option):
     cmd = "%s %s -S -o /dev/null -xc /dev/null > /dev/null 2>&1" % (
         GCC_CMD, option)
     res = os.system(cmd)
     return res == 0
+
+# Check if the current gcc version supports a particular command-line option
+def do_build_code(cmd):
+    res = os.system(cmd)
+    if res:
+        msg = "Unable to build C code module (error=%s)" % (res,)
+        logging.error(msg)
+        raise Exception(msg)
 
 FFI_main = None
 FFI_lib = None
@@ -223,7 +232,7 @@ def get_ffi():
             else:
                 cmd = "%s %s" % (GCC_CMD, COMPILE_ARGS)
             logging.info("Building C code module %s", DEST_LIB)
-            os.system(cmd % (destlib, ' '.join(srcfiles)))
+            do_build_code(cmd % (destlib, ' '.join(srcfiles)))
         FFI_main = cffi.FFI()
         for d in defs_all:
             FFI_main.cdef(d)
@@ -254,7 +263,7 @@ def run_hub_ctrl(enable_power):
     destlib = get_abs_files(hubdir, [HC_TARGET])[0]
     if check_build_code(srcfiles, destlib):
         logging.info("Building C code module %s", HC_TARGET)
-        os.system(HC_COMPILE_CMD % (destlib, ' '.join(srcfiles)))
+        do_build_code(HC_COMPILE_CMD % (destlib, ' '.join(srcfiles)))
     os.system(HC_CMD % (hubdir, enable_power))
 
 

scripts/spi_flash/fatfs_lib.py
@@ -73,7 +73,7 @@ def check_fatfs_build(printfunc=lambda o: o):
         else:
             cmd = "%s %s" % (chelper.GCC_CMD, chelper.COMPILE_ARGS)
         printfunc("Building FatFS shared library...")
-        os.system(cmd % (destlib, ' '.join(srcfiles)))
+        chelper.do_build_code(cmd % (destlib, ' '.join(srcfiles)))
         printfunc("Done\n")
     global fatfs_ffi_main, fatfs_ffi_lib
     ffi_main.cdef(FATFS_CDEFS)