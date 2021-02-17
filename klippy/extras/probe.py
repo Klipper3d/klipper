f3bd4e6acffadb4e927f69d3944da92579707a4b
Kevin O'Connor <kevin@koconnor.net>
2021-02-17 17:48:03 -0500

    probe: Call add_stepper() earlier in boot process
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -280,8 +280,9 @@ class ProbeEndstopWrapper:
         pin = config.get('pin')
         pin_params = ppins.lookup_pin(pin, can_invert=True, can_pullup=True)
         mcu = pin_params['chip']
-        mcu.register_config_callback(self._build_config)
         self.mcu_endstop = mcu.setup_pin('endstop', pin_params)
+        self.printer.register_event_handler('klippy:mcu_identify',
+                                            self._handle_mcu_identify)
         # Wrappers
         self.get_mcu = self.mcu_endstop.get_mcu
         self.add_stepper = self.mcu_endstop.add_stepper
@@ -291,7 +292,7 @@ class ProbeEndstopWrapper:
         self.query_endstop = self.mcu_endstop.query_endstop
         # multi probes state
         self.multi = 'OFF'
-    def _build_config(self):
+    def _handle_mcu_identify(self):
         kin = self.printer.lookup_object('toolhead').get_kinematics()
         for stepper in kin.get_steppers():
             if stepper.is_active_axis('z'):
