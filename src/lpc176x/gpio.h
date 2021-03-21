9226b15546634e4de9454f69b83fa11764ac9432
Michael Kurz <michi.kurz@gmail.com>
2021-03-21 19:13:47 +0100

    lpc176x: Add hardware PWM support for LPC176x
    This adds hardware PWM support for LPC176x processors.
    
    Signed-off-by: Michael Kurz <michi.kurz@gmail.com>

--- src/lpc176x/gpio.h
+++ src/lpc176x/gpio.h
@@ -21,6 +21,13 @@ struct gpio_in gpio_in_setup(uint8_t pin, int8_t pull_up);
 void gpio_in_reset(struct gpio_in g, int8_t pull_up);
 uint8_t gpio_in_read(struct gpio_in g);
 
+struct gpio_pwm {
+    void *reg;
+    uint8_t channel;
+};
+struct gpio_pwm gpio_pwm_setup(uint8_t pin, uint32_t cycle_time, uint8_t val);
+void gpio_pwm_write(struct gpio_pwm g, uint32_t val);
+
 struct gpio_adc {
     uint32_t chan;
 };
