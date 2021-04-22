2d1e405a62a53fff16b1dd79d9ce7e667b760777
Matt Baker <baker.matt.j@gmail.com>
2021-04-22 13:44:39 -0700

    stm32l4: add stm32l412 support with adc,i2c,spi,usb
    Signed-off-by: Matt Baker <baker.matt.j@gmail.com>

--- src/stm32/spi.c
+++ src/stm32/spi.c
@@ -79,8 +79,8 @@ spi_setup(uint32_t bus, uint8_t mode, uint32_t rate)
         gpio_peripheral(spi_bus[bus].mosi_pin, spi_bus[bus].function, 0);
         gpio_peripheral(spi_bus[bus].sck_pin, spi_bus[bus].function, 0);
 
-        // Configure CR2 on stm32f0
-#if CONFIG_MACH_STM32F0 || CONFIG_MACH_STM32G0
+        // Configure CR2 on stm32 f0/g0/l4
+#if CONFIG_MACH_STM32F0 || CONFIG_MACH_STM32G0 || CONFIG_MACH_STM32L4
         spi->CR2 = SPI_CR2_FRXTH | (7 << SPI_CR2_DS_Pos);
 #endif
     }
