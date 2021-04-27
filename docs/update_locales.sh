9ca8a4ea09d132b19689bc8aff3baa5d86dc8e30
Yifei Ding <dingyifeiair@gmail.com>
2021-04-28 05:29:49 +0800

    Localization: Add Base Files For Weblate
    Signed-off-by: Yifei Ding <yifeiding@protonmail.com>

--- docs/update_locales.sh
+++ docs/update_locales.sh
@@ -3,16 +3,22 @@
 #in the ./locales/[language]/ folder
 #requirement: pip install mdpo
 
-for dir in ./locales/*/
-do
-    dir=${dir%*/}
+
+for file in *.md; do
+  echo "Converting $file to ${file//.md/.pot}"
+  md2po $file --md-encoding utf-8 --po-encoding utf-8 \
+  -e utf-8 -w 71 -q -s --po-filepath locales/en/${file//.md/.pot}
+done
+
+for dir in ./locales/*/; do
+  dir=${dir%*/}
+  dir=${dir##*/}
+  if [ $dir != "en" ]; then
     echo "Updating ${dir##*/}"
     for file in *.md; do
-      echo "Converting $file to ${file//.md/.po}"
-      md2po $file --md-encoding utf-8 --po-encoding utf-8 \
-      -e utf-8 -w 71 -q -s --po-filepath locales/${dir##*/}/${file//.md/.po}
-      echo "Converting ${file//.md/.po} to $file"
-      po2md $file --md-encoding utf-8 --po-encoding utf-8 \
-      --pofiles locales/${dir##*/}/${file//.md/.po} -q -s locales/${dir##*/}/$file
+    echo "Converting ${file//.md/.po} to $file"
+    po2md $file --md-encoding utf-8 --po-encoding utf-8 \
+    --pofiles locales/$dir/${file//.md/.po} -q -s locales/$dir/$file
     done
+  fi
 done
