9fc8a9bbc312bbcde66fc3159aeff38608c3141e
Yifei Ding <dingyifeiair@gmail.com>
2021-04-28 04:20:51 +0800

    Localization: Relocate local to locales
    Signed-off-by: Yifei Ding <yifeiding@protonmail.com>

--- docs/update_local.sh
+++ docs/update_local.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Automatically generate a set of po and md files for Localization
-#in the ./local/[language]/ folder
+#in the ./locales/[language]/ folder
 #requirement: pip install mdpo
 
-for dir in ./local/*/
+for dir in ./locales/*/
 do
     dir=${dir%*/}
     echo "Updating ${dir##*/}"
     for file in *.md; do
       echo "Converting $file to ${file//.md/.po}"
       md2po $file --md-encoding utf-8 --po-encoding utf-8 \
-      -e utf-8 -w 71 -q -s --po-filepath local/${dir##*/}/${file//.md/.po}
+      -e utf-8 -w 71 -q -s --po-filepath locales/${dir##*/}/${file//.md/.po}
       echo "Converting ${file//.md/.po} to $file"
       po2md $file --md-encoding utf-8 --po-encoding utf-8 \
-      --pofiles local/${dir##*/}/${file//.md/.po} -q -s local/${dir##*/}/$file
+      --pofiles locales/${dir##*/}/${file//.md/.po} -q -s locales/${dir##*/}/$file
     done
 done
