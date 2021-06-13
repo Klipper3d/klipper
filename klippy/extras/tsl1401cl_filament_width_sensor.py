5d889895f255b092952e78e397395d625fe113a9
dawidmpunkt <85826525+dawidmpunkt@users.noreply.github.com>
2021-06-13 16:58:24 +0200

    tsl1401cl_filament_width_sensor: Add functionality
    I added some Functions to the tsl1401cl_filament_width_sensor.py:
    
    Extrusion modifier can be turned on/off
    Filament width logging can be turned on/off (saved in klippy.log)
    Added filament runout/clog switch
    Extrusion modifier range can be set (for imprecise sensors). Extrusion gets modified only above a certain difference in width.
    Possibility to calibrate filament width: y=mx+b, Filament width = adc-value * slope + offset
    Measurement interval can now be set (default=10 mm).
    
    New parameters for config:
    measurement_interval: (default: 10)
    pause_on_runout: (default: True)
    extrude_factor_enable: (default: False)
    logging: (default: False)
    difference_max: (default: 0)
    difference_min: (default: 0)
    clog_width: (default: 5)
    runout_width: (default: 0.1)
    offset_mm: (default: 0) 
    slope: (default: 1)
    
    Old parameters not working anymore:
    max_difference
    
    Signed-off-by: Dawid Murawski <dawid.m@gmx.net>

--- klippy/extras/tsl1401cl_filament_width_sensor.py
+++ klippy/extras/tsl1401cl_filament_width_sensor.py
@@ -3,11 +3,11 @@
 # Copyright (C) 2019  Mustafa YILDIZ <mydiz@hotmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+from . import filament_switch_sensor
 
 ADC_REPORT_TIME = 0.500
 ADC_SAMPLE_TIME = 0.001
 ADC_SAMPLE_COUNT = 8
-MEASUREMENT_INTERVAL_MM = 10
 
 class FilamentWidthSensor:
     def __init__(self, config):
@@ -17,16 +17,31 @@ class FilamentWidthSensor:
         self.nominal_filament_dia = config.getfloat(
             'default_nominal_filament_diameter', above=1.0)
         self.measurement_delay = config.getfloat('measurement_delay', above=0.)
-        self.measurement_max_difference = config.getfloat('max_difference',
-                                                          above=0.)
+        # Logging on/off
+        self.is_log = config.getboolean('logging', False)
+        # Enable extrusion modifier and/or use sensor as Runout switch only
+        self.extrude_factor_enable = config.getboolean('extrude_factor_enable', False)
+        # Set measurement interval
+        self.measurement_interval_mm = config.getint('measurement_interval', 10)
+        # Calibration of sensor: y=mx+b, Filament width = adc_value * slope + offset
+        self.measurement_offset = config.getfloat('offset_mm', 0)
+        self.measurement_slope = config.getfloat('slope', 1)
+        # Filament diameter variation, that does NOT cause the flow rate to be modified (for imprecise filament sensors)
+        self.measurement_difference_max = config.getfloat('difference_max',
+                                                          0)
+        self.measurement_difference_min = config.getfloat('difference_min',
+                                                          0)                                                    
         self.max_diameter = (self.nominal_filament_dia
-                             + self.measurement_max_difference)
+                             + self.measurement_difference_max)
         self.min_diameter = (self.nominal_filament_dia
-                             - self.measurement_max_difference)
+                             - self.measurement_difference_min)
         self.is_active = True
         # filament array [position, filamentWidth]
         self.filament_array = []
         self.lastFilamentWidthReading = 0
+        # Diameter to trigger Filament Runout/Clog in mm
+        self.runout_dia = config.getfloat('runout_width', 0.1)
+        self.clog_dia = config.getfloat('clog_width', 5)
         # printer objects
         self.toolhead = self.ppins = self.mcu_adc = None
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
@@ -47,6 +62,13 @@ class FilamentWidthSensor:
                                     self.cmd_M406)
         self.gcode.register_command('ENABLE_FILAMENT_WIDTH_SENSOR',
                                     self.cmd_M405)
+        # Runout helper                            
+        self.runout_helper = filament_switch_sensor.RunoutHelper(config)
+        # Logging
+        self.gcode.register_command('ENABLE_FILAMENT_WIDTH_LOG',
+                                        self.cmd_log_enable)
+        self.gcode.register_command('DISABLE_FILAMENT_WIDTH_LOG',
+                                        self.cmd_log_disable)
 
     # Initialization
     def handle_ready(self):
@@ -58,8 +80,8 @@ class FilamentWidthSensor:
                                   self.reactor.NOW)
 
     def adc_callback(self, read_time, read_value):
-        # read sensor value
-        self.lastFilamentWidthReading = round(read_value * 5, 2)
+        # read sensor value, Filament width = acd-value * slope + offset, y=mx+b
+        self.lastFilamentWidthReading = round((read_value * self.measurement_slope) + self.measurement_offset, 2)
 
     def update_filament_array(self, last_epos):
         # Fill array
@@ -67,10 +89,14 @@ class FilamentWidthSensor:
             # Get last reading position in array & calculate next
             # reading position
             next_reading_position = (self.filament_array[-1][0]
-                                     + MEASUREMENT_INTERVAL_MM)
+                                     + self.measurement_interval_mm)
             if next_reading_position <= (last_epos + self.measurement_delay):
                 self.filament_array.append([last_epos + self.measurement_delay,
                                             self.lastFilamentWidthReading])
+              # Logging                              
+                if self.is_log:
+                    self.gcode.respond_info("Filament width:%.3f" %
+                                                ( self.diameter ))
         else:
             # add first item to array
             self.filament_array.append([self.measurement_delay + last_epos,
@@ -82,25 +108,33 @@ class FilamentWidthSensor:
         last_epos = pos[3]
         # Update filament array for lastFilamentWidthReading
         self.update_filament_array(last_epos)
-        # Does filament exists
-        if self.lastFilamentWidthReading > 0.5:
-            if len(self.filament_array) > 0:
-                # Get first position in filament array
-                pending_position = self.filament_array[0][0]
-                if pending_position <= last_epos:
-                    # Get first item in filament_array queue
-                    item = self.filament_array.pop(0)
-                    filament_width = item[1]
-                    if ((filament_width <= self.max_diameter)
-                        and (filament_width >= self.min_diameter)):
-                        percentage = round(self.nominal_filament_dia**2
+        # Check runout/clog
+        self.runout_helper.note_filament_present(
+            self.lastFilamentWidthReading > self.runout_dia)
+        self.runout_helper.note_filament_present(
+            self.lastFilamentWidthReading < self.clog_dia)
+        
+        # Is extrusion modifier enabled    
+        if self.extrude_factor_enable:
+            # Does filament exists
+            if self.lastFilamentWidthReading > 0.5:
+                if len(self.filament_array) > 0:
+                    # Get first position in filament array
+                    pending_position = self.filament_array[0][0]
+                    if  pending_position <= last_epos:
+                        # Get first item in filament_array queue
+                        item = self.filament_array.pop(0)
+                        filament_width = item[1]
+                        if ( (filament_width >= self.max_diameter)
+                            or (filament_width <= self.min_diameter)):
+                            percentage = round(self.nominal_filament_dia**2
                                            / filament_width**2 * 100)
-                        self.gcode.run_script("M221 S" + str(percentage))
-                    else:
-                        self.gcode.run_script("M221 S100")
-        else:
-            self.gcode.run_script("M221 S100")
-            self.filament_array = []
+                            self.gcode.run_script("M221 S" + str(percentage))
+                        else:
+                            self.gcode.run_script("M221 S100")
+            else:
+                self.gcode.run_script("M221 S100")
+                self.filament_array = []
 
         if self.is_active:
             return eventtime + 1
@@ -147,6 +181,14 @@ class FilamentWidthSensor:
             # Set extrude multiplier to 100%
             self.gcode.run_script_from_command("M221 S100")
         gcmd.respond_info(response)
+        
+    def cmd_log_enable(self, gcmd):
+        self.is_log = True
+        gcmd.respond_info("Filament width logging Turned On")
 
+    def cmd_log_disable(self, gcmd):
+        self.is_log = False
+        gcmd.respond_info("Filament width logging Turned Off")    
+        
 def load_config(config):
     return FilamentWidthSensor(config)
