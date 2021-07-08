848207cef21ac06f5d978caaab8b33b536dbea4a
brian-edward <87120703+brian-edward@users.noreply.github.com>
2021-07-08 11:01:20 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -266,7 +266,7 @@ class PrinterHeaters:
     def setup_sensor(self, config):
         modules = ["thermistor", "adc_temperature", "spi_temperature",
                    "bme280", "htu21d", "lm75", "temperature_host",
-                   "temperature_mcu", "ds18b20"]
+                   "temperature_mcu", "ds18b20", "sensor_any"]
 
         for module_name in modules:
             self.printer.load_object(config, module_name)
