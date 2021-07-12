7139687cb96189174f572b043e26c751ff7cce47
Curt Meyers <boilerbots@gmail.com>
2021-07-12 14:31:06 -0700

    stm32: Fix compile error for stm32f103
    Verified compile for stm32f103-serial.config
    
    ```
    make clean; make distclean
    cp test/configs/stm32f103-serial.config .config
    make olddefconfig
      Creating symbolic link out/board
    Loaded configuration '/home/cmeyers/projects/klipper/.config'
    Configuration saved to '/home/cmeyers/projects/klipper/.config'
    make
      Creating symbolic link out/board
      Building out/autoconf.h
      Compiling out/src/sched.o
      Compiling out/src/command.o
      Compiling out/src/basecmd.o
      Compiling out/src/debugcmds.o
      Compiling out/src/initial_pins.o
      Compiling out/src/gpiocmds.o
      Compiling out/src/stepper.o
      Compiling out/src/endstop.o
      Compiling out/src/trsync.o
      Compiling out/src/adccmds.o
      Compiling out/src/spicmds.o
      Compiling out/src/thermocouple.o
      Compiling out/src/i2ccmds.o
      Compiling out/src/pwmcmds.o
      Compiling out/src/spi_software.o
      Compiling out/src/sensor_adxl345.o
      Compiling out/src/lcd_st7920.o
      Compiling out/src/lcd_hd44780.o
      Compiling out/src/buttons.o
      Compiling out/src/tmcuart.o
      Compiling out/src/neopixel.o
      Compiling out/src/pulse_counter.o
      Compiling out/src/stm32/watchdog.o
      Compiling out/src/stm32/gpio.o
      Compiling out/src/generic/crc16_ccitt.o
      Compiling out/src/generic/armcm_boot.o
      Compiling out/src/generic/armcm_irq.o
      Compiling out/src/generic/armcm_reset.o
      Compiling out/src/../lib/stm32f1/system_stm32f1xx.o
      Compiling out/src/stm32/stm32f1.o
      Compiling out/src/generic/armcm_timer.o
      Compiling out/src/stm32/adc.o
      Compiling out/src/stm32/i2c.o
      Compiling out/src/stm32/spi.o
      Compiling out/src/stm32/serial.o
      Compiling out/src/generic/serial_irq.o
      Compiling out/src/stm32/hard_pwm.o
      Building out/compile_time_request.o
    Version: v0.9.1-621-g4d99fbc0-dirty-20210712_143059-tesla
      Preprocessing out/src/generic/armcm_link.ld
      Linking out/klipper.elf
      Creating hex file out/klipper.bin
    ```
    
    Signed-off-by: Curt Meyers <cmeyers@boilerbots.com>

--- src/stm32/watchdog.c
+++ src/stm32/watchdog.c
@@ -21,7 +21,7 @@ watchdog_init(void)
     IWDG->PR = 0;
     IWDG->RLR = 0x0FFF; // 410-512ms timeout (depending on stm32 chip)
     IWDG->KR = 0xCCCC;
-#ifndef MACH_STM32F1
+#ifndef CONFIG_MACH_STM32F1
     DBGMCU->APB1FZ |= 0x1000; // stop the watchdog with debug.
 #endif
 }
