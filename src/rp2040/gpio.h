84523f28117b4da64044d60bafe3fde782d71c45
Lasse Dalegaard <dalegaard@gmail.com>
2021-07-15 16:36:42 +0200

    rp2040: implement SPI
    This implements SPI for the rp2040 target. All output groupings of both
    SPI blocks are available for use.
    
    Signed-off-by: Lasse Dalegaard <dalegaard@gmail.com>

--- src/rp2040/gpio.h
+++ src/rp2040/gpio.h
@@ -35,4 +35,13 @@ uint32_t gpio_adc_sample(struct gpio_adc g);
 uint16_t gpio_adc_read(struct gpio_adc g);
 void gpio_adc_cancel_sample(struct gpio_adc g);
 
+struct spi_config {
+    void *spi;
+    uint32_t cr0, cpsr;
+};
+struct spi_config spi_setup(uint32_t bus, uint8_t mode, uint32_t rate);
+void spi_prepare(struct spi_config config);
+void spi_transfer(struct spi_config config, uint8_t receive_data
+                  , uint8_t len, uint8_t *data);
+
 #endif // gpio.h
