7b8b2ff5a5eaead8c5b93a95eb92495f1685a709
Elias Bakken <elias@iagent.no>
2021-08-09 21:07:56 +0200

    add tick timer reset to header

--- src/ar100/timer.h
+++ src/ar100/timer.h
@@ -18,6 +18,7 @@ static inline void mtspr(unsigned long add, unsigned long val){
 uint8_t timer_interrupt_pending(void);
 void timer_set(uint32_t value);
 uint32_t timer_read_time(void);
+void timer_reset(void);
 void timer_clear_interrupt(void);
 void timer_kick(void);
 void timer_init(void);
