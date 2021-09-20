9fdd085f29ac16910ab2ae6d4cb8eb46dad486e4
adelyser <12093019+ad@users.noreply.github.com>
2021-09-20 17:30:48 -0600

    Update switches

--- src/stm32/spi.c
+++ src/stm32/spi.c
@@ -65,10 +65,14 @@ DECL_CONSTANT_STR("BUS_PINS_spi6", "PG12,PG14,PG13");
 #define SPI_FUNCTION GPIO_FUNCTION(CONFIG_MACH_STM32F0 ? 0 : 5)
 
 static const struct spi_info spi_bus[] = {
+#if !CONFIG_STM32_USB_PB14_PB15
     { SPI2, GPIO('B', 14), GPIO('B', 15), GPIO('B', 13), SPI_FUNCTION },
+#endif
     { SPI1, GPIO('A', 6), GPIO('A', 7), GPIO('A', 5), SPI_FUNCTION },
     { SPI1, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), SPI_FUNCTION },
+#if !CONFIG_MACH_STM32F1
     { SPI2, GPIO('C', 2), GPIO('C', 3), GPIO('B', 10), SPI_FUNCTION },
+#endif
 #ifdef SPI3
     { SPI3, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), GPIO_FUNCTION(6) },
 #endif
