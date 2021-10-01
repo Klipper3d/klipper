eef3ee06a2a9fd812e1edd25cf2955188b7831f2
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-10-01 18:36:33 +0100

    wled: Use reactor for background processing
    Signed-off-by: Richard Mitchell <richardjm+klipper@gmail.com>

--- klippy/extras/wled.py
+++ klippy/extras/wled.py
@@ -22,29 +22,30 @@ class WLED:
                                     self.cmd_WLED_ON, desc=self.cmd_WLED_ON_help)
         self.gcode.register_mux_command('WLED_OFF', 'WLED', name,
                             self.cmd_WLED_OFF, desc=self.cmd_WLED_OFF_help)
-                            
+    def _wled_send(self, json):
+        def post_bg(json):
+            try:
+                if self.verbose:
+                    self.gcode.respond_info("WLED: Sending - " + json)
+                requests.post(self.url, json=eval(json))
+            except:
+                raise self.gcode.error('Incorrect json syntax')
+        
+        reactor = self.printer.get_reactor()
+        reactor.register_callback(lambda et: post_bg(json))
+        
     cmd_WLED_JSON_help = 'Send json data to WLED'
     def cmd_WLED_JSON(self, gcmd):
         json = gcmd.get('JSON')
-        
-        try:
-            if self.verbose:
-                self.gcode.respond_info("WLED: Sending - " + json)
-            requests.post(self.url, json=eval(json))
-        except:
-            raise self.gcode.error('Incorrect json syntax')
-        
+        self._wled_send(json)
+
     cmd_WLED_ON_help = 'Turn on WLED'
     def cmd_WLED_ON(self, gcmd):
-        if self.verbose:
-            self.gcode.respond_info("WLED: Turning on - " + self.json_on)
-        requests.post(self.url, json=eval(self.json_on))
+        self._wled_send(self.json_on)
 
     cmd_WLED_OFF_help = 'Turn off WLED'
     def cmd_WLED_OFF(self, gcmd):
-        if self.verbose:
-            self.gcode.respond_info("WLED: Turning off " + self.json_off)
-        requests.post(self.url, json=eval(self.json_off))
+        self._wled_send(self.json_off)
 
 def load_config_prefix(config):
     return WLED(config)
