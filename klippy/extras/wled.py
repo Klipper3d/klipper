4eebafc36de3c25af891bdb3bf3c8f142288b20d
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-10-01 18:27:59 +0100

    wled: Some basic wled functionality
    Signed-off-by: Richard Mitchell <richardjm+klipper@gmail.com>

--- klippy/extras/wled.py
+++ klippy/extras/wled.py
@@ -1,33 +1,50 @@
-# Helper script to adjust bed screws
+# Helper script to control WLED via JSON API
 #
 # Copyright (C) 2019-2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import requests
 
-class BedScrews:
+class WLED:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.state = None
-        self.current_screw = 0
-        self.accepted_screws = 0
-        self.number_of_screws = 0
+        name = config.get_name().split()[1]
         # Read config
+        self.verbose = config.get('verbose', False)
         self.url = config.get('url')
-        # Register command
+        self.json_on = config.get('on', {'on': True})
+        self.json_off = config.get('off', {'on': False})
+        # Register commands
         self.gcode = self.printer.lookup_object('gcode')
-        self.gcode.register_command("WLED_ON",
-                                    self.cmd_WLED_ON,
-                                    desc=self.cmd_WLED_ON_help)
-        self.gcode.register_command("WLED_OFF",
-                            self.cmd_WLED_OFF,
-                            desc=self.cmd_WLED_OFF_help)                            
-    cmd_WLED_ON_help = "Turn on WLED"
+        self.gcode.register_mux_command('WLED_JSON', 'WLED', name,
+                            self.cmd_WLED_JSON, desc=self.cmd_WLED_JSON_help)
+        self.gcode.register_mux_command('WLED_ON', 'WLED', name,
+                                    self.cmd_WLED_ON, desc=self.cmd_WLED_ON_help)
+        self.gcode.register_mux_command('WLED_OFF', 'WLED', name,
+                            self.cmd_WLED_OFF, desc=self.cmd_WLED_OFF_help)
+                            
+    cmd_WLED_JSON_help = 'Send json data to WLED'
+    def cmd_WLED_JSON(self, gcmd):
+        json = gcmd.get('JSON')
+        
+        try:
+            if self.verbose:
+                self.gcode.respond_info("WLED: Sending - " + json)
+            requests.post(self.url, json=eval(json))
+        except:
+            raise self.gcode.error('Incorrect json syntax')
+        
+    cmd_WLED_ON_help = 'Turn on WLED'
     def cmd_WLED_ON(self, gcmd):
-        requests.get(self.url + "&T=1")
-    cmd_WLED_OFF_help = "Turn off WLED"
-    def cmd_WLED_ON(self, gcmd):
-        requests.get(self.url + "&T=0")
+        if self.verbose:
+            self.gcode.respond_info("WLED: Turning on - " + self.json_on)
+        requests.post(self.url, json=eval(self.json_on))
 
-def load_config(config):
-    return BedScrews(config)
+    cmd_WLED_OFF_help = 'Turn off WLED'
+    def cmd_WLED_OFF(self, gcmd):
+        if self.verbose:
+            self.gcode.respond_info("WLED: Turning off " + self.json_off)
+        requests.post(self.url, json=eval(self.json_off))
+
+def load_config_prefix(config):
+    return WLED(config)
