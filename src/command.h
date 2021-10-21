823fdc9cb47b0117f34366b1c7d583070803a93c
Kevin O'Connor <kevin@koconnor.net>
2021-10-20 20:58:12 -0400

    command: Make command_add_frame() and command_encodef() static
    The only users of these two functions are in command.c so they can be
    declared local to that code.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/command.h
+++ src/command.h
@@ -74,9 +74,6 @@ enum {
 void *command_decode_ptr(uint32_t v);
 uint8_t *command_parsef(uint8_t *p, uint8_t *maxend
                         , const struct command_parser *cp, uint32_t *args);
-uint_fast8_t command_encodef(uint8_t *buf, const struct command_encoder *ce
-                             , va_list args);
-void command_add_frame(uint8_t *buf, uint_fast8_t msglen);
 uint_fast8_t command_encode_and_frame(
     uint8_t *buf, const struct command_encoder *ce, va_list args);
 void command_sendf(const struct command_encoder *ce, ...);
