dcdc66cd33a07fd4dafa26443ba93ed1b5ff1256
adelyser <12093019+ad@users.noreply.github.com>
2021-10-23 21:28:27 -0600

    Breakout stm32h spi

--- src/stm32/stm32h7_spi.c
+++ src/stm32/stm32h7_spi.c
@@ -1,4 +1,4 @@
-// SPI functions on STM32
+// SPI functions on STM32H7
 //
 // Copyright (C) 2019  Kevin O'Connor <kevin@koconnor.net>
 //
@@ -15,8 +15,10 @@ struct spi_info {
     uint8_t miso_pin, mosi_pin, sck_pin, function;
 };
 
+#if !CONFIG_STM32_USB_PB14_PB15
 DECL_ENUMERATION("spi_bus", "spi2", __COUNTER__);
 DECL_CONSTANT_STR("BUS_PINS_spi2", "PB14,PB15,PB13");
+#endif
 
 DECL_ENUMERATION("spi_bus", "spi1", __COUNTER__);
 DECL_CONSTANT_STR("BUS_PINS_spi1", "PA6,PA7,PA5");
@@ -31,9 +33,7 @@ DECL_CONSTANT_STR("BUS_PINS_spi2a", "PC2,PC3,PB10");
 #ifdef SPI3
 DECL_ENUMERATION("spi_bus", "spi3", __COUNTER__);
 DECL_CONSTANT_STR("BUS_PINS_spi3", "PB4,PB5,PB3");
-#endif
 
-#if CONFIG_MACH_STM32F4
 DECL_ENUMERATION("spi_bus", "spi3a", __COUNTER__);
 DECL_CONSTANT_STR("BUS_PINS_spi3a", "PC11,PC12,PC10");
 #endif
@@ -60,30 +60,27 @@ DECL_ENUMERATION("spi_bus", "spi6", __COUNTER__);
 DECL_CONSTANT_STR("BUS_PINS_spi6", "PG12,PG14,PG13");
 #endif
 
-#define SPI_FUNCTION GPIO_FUNCTION(CONFIG_MACH_STM32F0 ? 0 : 5)
 
 static const struct spi_info spi_bus[] = {
 #if !CONFIG_STM32_USB_PB14_PB15
-    { SPI2, GPIO('B', 14), GPIO('B', 15), GPIO('B', 13), SPI_FUNCTION },
+    { SPI2, GPIO('B', 14), GPIO('B', 15), GPIO('B', 13), GPIO_FUNCTION(5) },
 #endif
-    { SPI1, GPIO('A', 6), GPIO('A', 7), GPIO('A', 5), SPI_FUNCTION },
-    { SPI1, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), SPI_FUNCTION },
+    { SPI1, GPIO('A', 6), GPIO('A', 7), GPIO('A', 5), GPIO_FUNCTION(5) },
+    { SPI1, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), GPIO_FUNCTION(5) },
 #if !CONFIG_MACH_STM32F1
-    { SPI2, GPIO('C', 2), GPIO('C', 3), GPIO('B', 10), SPI_FUNCTION },
+    { SPI2, GPIO('C', 2), GPIO('C', 3), GPIO('B', 10), GPIO_FUNCTION(5) },
 #endif
 #ifdef SPI3
     { SPI3, GPIO('B', 4), GPIO('B', 5), GPIO('B', 3), GPIO_FUNCTION(6) },
-#endif
-#if CONFIG_MACH_STM32F4
     { SPI3, GPIO('C', 11), GPIO('C', 12), GPIO('C', 10), GPIO_FUNCTION(6) },
 #endif
 #ifdef SPI4
-    { SPI4, GPIO('E', 13), GPIO('E', 14), GPIO('E', 12), SPI_FUNCTION },
+    { SPI4, GPIO('E', 13), GPIO('E', 14), GPIO('E', 12), GPIO_FUNCTION(5) },
 #endif
-    { SPI2, GPIO('I', 2), GPIO('I', 3), GPIO('I', 1), SPI_FUNCTION },
+    { SPI2, GPIO('I', 2), GPIO('I', 3), GPIO('I', 1), GPIO_FUNCTION(5) },
 #ifdef SPI5
-    { SPI5, GPIO('F', 8), GPIO('F', 9), GPIO('F', 7), SPI_FUNCTION },
-    { SPI5, GPIO('H', 7), GPIO('F', 11), GPIO('H', 6), SPI_FUNCTION },
+    { SPI5, GPIO('F', 8), GPIO('F', 9), GPIO('F', 7), GPIO_FUNCTION(5) },
+    { SPI5, GPIO('H', 7), GPIO('F', 11), GPIO('H', 6), GPIO_FUNCTION(5) },
 #endif
 #ifdef SPI6
     { SPI6, GPIO('G', 12), GPIO('G', 14), GPIO('G', 13), GPIO_FUNCTION(5)},
@@ -103,11 +100,6 @@ spi_setup(uint32_t bus, uint8_t mode, uint32_t rate)
         gpio_peripheral(spi_bus[bus].miso_pin, spi_bus[bus].function, 1);
         gpio_peripheral(spi_bus[bus].mosi_pin, spi_bus[bus].function, 0);
         gpio_peripheral(spi_bus[bus].sck_pin, spi_bus[bus].function, 0);
-
-        // Configure CR2 on stm32f0
-#if CONFIG_MACH_STM32F0
-        spi->CR2 = SPI_CR2_FRXTH | (7 << SPI_CR2_DS_Pos);
-#endif
     }
 
     // Calculate CR1 register
@@ -116,16 +108,19 @@ spi_setup(uint32_t bus, uint8_t mode, uint32_t rate)
     while ((pclk >> (div + 1)) > rate && div < 7)
         div++;
 
-    uint32_t cr1 = ((mode << SPI_CR1_CPHA_Pos) | (div << SPI_CR1_BR_Pos)
-                    | SPI_CR1_SPE | SPI_CR1_MSTR | SPI_CR1_SSM | SPI_CR1_SSI);
+    uint32_t cr1 = SPI_CR1_SPE;
+    spi->CFG1 |= (div << SPI_CFG1_MBR_Pos) | (7 << SPI_CFG1_DSIZE_Pos);
+    CLEAR_BIT(spi->CFG1, SPI_CFG1_CRCSIZE);
+    spi->CFG2 |= ((mode << SPI_CFG2_CPHA_Pos) | SPI_CFG2_MASTER | SPI_CFG2_SSM
+                   | SPI_CFG2_AFCNTR | SPI_CFG2_SSOE);
+    spi->CR1 |= SPI_CR1_SSI;
+
     return (struct spi_config){ .spi = spi, .spi_cr1 = cr1 };
 }
 
 void
 spi_prepare(struct spi_config config)
 {
-    SPI_TypeDef *spi = config.spi;
-    spi->CR1 = config.spi_cr1;
 }
 
 void
@@ -134,13 +129,26 @@ spi_transfer(struct spi_config config, uint8_t receive_data,
 {
     uint8_t rdata = 0;
     SPI_TypeDef *spi = config.spi;
+
+    MODIFY_REG(spi->CR2, SPI_CR2_TSIZE, len);
+    // Enable SPI and start transfer, these MUST be set in this sequence
+    SET_BIT(spi->CR1, SPI_CR1_SPE);
+    SET_BIT(spi->CR1, SPI_CR1_CSTART);
+
     while (len--) {
-        writeb((void *)&spi->DR, *data);
-        while (!(spi->SR & SPI_SR_RXNE))
-            ;
-        uint8_t rdata = readb((void *)&spi->DR);
-        if (receive_data)
+        writeb((void *)&spi->TXDR, *data);
+        while((spi->SR & (SPI_SR_RXWNE | SPI_SR_RXPLVL)) == 0);
+        rdata = readb((void *)&spi->RXDR);
+
+        if (receive_data) {
             *data = rdata;
+        }
         data++;
     }
+
+    while ((spi->SR & SPI_SR_EOT) == 0);
+
+    // Clear flags and disable SPI
+    SET_BIT(spi->IFCR, 0xFFFFFFFF);
+    CLEAR_BIT(spi->CR1, SPI_CR1_SPE);
 }
