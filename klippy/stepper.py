9efdc85fe7c930f24a1af6649bd6f83443d2c6f9
Andrei Ignat <andrei@ignat.se>
2021-11-06 18:58:17 +0100

    Lines now less than 80 characters
    Signed-off-by: Andrei Ignat andrei@ignat.se

--- klippy/stepper.py
+++ klippy/stepper.py
@@ -394,8 +394,10 @@ class PrinterRail:
             stepper.set_position(coord)
 
 # Wrapper for dual stepper motor support
-def LookupMultiRail(config, need_position_minmax=True, default_position_endstop=None, units_in_radians=False):
-    rail = PrinterRail(config, need_position_minmax=True, default_position_endstop=None, units_in_radians=False)
+def LookupMultiRail(config, need_position_minmax=True,
+                 default_position_endstop=None, units_in_radians=False):
+    rail = PrinterRail(config, need_position_minmax=True, 
+                       default_position_endstop=None, units_in_radians=False)
     for i in range(1, 99):
         if not config.has_section(config.get_name() + str(i)):
             break
