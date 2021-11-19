e2d16b4df50b57dd4115bcd9dc520481bad7f04e
Elias Bakken <elias@iagent.no>
2021-11-19 17:13:19 +0100

    Use board aliases instead of from the pins file

--- klippy/extras/recore.py
+++ klippy/extras/recore.py
@@ -20,7 +20,7 @@ class recore:
         'A6': 'A6'}
     config.getchoice('revision', revisions)
     # Setup enable pin
-    enable_pin = config.get('enable_pin', 'ar100:PG1')
+    enable_pin = config.get('enable_pin', 'ar100:OC_RESET ')
     self.mcu_power_enable = ppins.setup_pin('digital_out', enable_pin)
     self.mcu_power_enable.setup_start_value(
         start_value=1., shutdown_value=0., is_static=False)
