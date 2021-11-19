1b1e0f6ae84444db1726a278a330b79cf27036e9
Elias Bakken <elias@iagent.no>
2021-11-19 23:59:55 +0100

    Move gain and pull-ups into recore section

--- klippy/extras/recore.py
+++ klippy/extras/recore.py
@@ -11,30 +11,37 @@ class recore:
     printer = config.get_printer()
     ppins = printer.lookup_object('pins')
     ppins.register_chip('recore', self)
-    revisions = {
-        'A1': 'A1',
-        'A2': 'A2',
-        'A3': 'A5',
-        'A4': 'A5',
-        'A5': 'A5',
-        'A6': 'A6'}
-    config.getchoice('revision', revisions)
+    revisions = {'A'+str(i): 'A'+str(i) for i in range(7)}
+    self.revision = config.getchoice('revision', revisions)
     # Setup enable pin
-    enable_pin = config.get('enable_pin', 'ar100:OC_RESET ')
+    enable_pin = config.get('enable_pin', 'ar100:EN_HP')
     self.mcu_power_enable = ppins.setup_pin('digital_out', enable_pin)
     self.mcu_power_enable.setup_start_value(
-        start_value=1., shutdown_value=0., is_static=False)
+        start_value=0., shutdown_value=1., is_static=False)
     self.mcu_power_enable.setup_max_duration(0.)
-    self.mcu_power_start_value = self.mcu_power_shutdown_value = False
-    # Setup stepper config
-    self.last_stepper_time = 0.
 
-  def setup_pin(self, pin_type, pin_params):
-    pin = pin_params['pin']
-    if pin in self.pins:
-      pclass, channel = self.pins[pin]
-      return pclass(self, channel, pin_type, pin_params)
-    raise pins.error("Unknown recore pin %s" % (pin, ))
+    if self.revision in ['A3', 'A4', 'A5', 'A6']:
+        for idx in range(4):
+            gain = config.get('gain_t'+str(idx), '1')
+            if gain not in ['1', '100']:
+                raise error("Gain not 1 or 100")
+            pin_name = "ar100:GAIN_ENABLE_T"+str(idx)
+            if gain == '1':
+                pin = ppins.setup_pin('endstop', pin_name)
+            else:
+                pin = ppins.setup_pin('digital_out', pin_name)
+                pin.setup_start_value(start_value=0., shutdown_value=0., is_static=True)
+
+            pullup = config.get('pullup_t'+str(idx), '1')
+            if pullup not in ['0', '1']:
+                raise error("Pullup not 0 or 1")
+            pin_name = "ar100:PU_ENABLE_T"+str(idx)
+            if pullup == '0':
+                pin = ppins.setup_pin('endstop', pin_name)
+            else:
+                pin = ppins.setup_pin('digital_out', pin_name)
+                pin.setup_start_value(start_value=1., shutdown_value=1., is_static=True)
+
 
 def load_config(config):
   return recore(config)
