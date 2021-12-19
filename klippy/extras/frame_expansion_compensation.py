bc3f358dd0f52fa1d354ba515b95043a5ba330c6
alchemyEngine <robertp@norbital.com>
2021-12-19 11:29:21 -0500

    frame_expansion_compensation: deprecate branch
    Warn about move to Moonraker plug-in for this feature.
    
    Signed-off-by: Robert Pazdzior <robertp@norbital.com>

--- klippy/extras/frame_expansion_compensation.py
+++ klippy/extras/frame_expansion_compensation.py
@@ -69,6 +69,14 @@ class FrameExpansionCompensator:
         'Called after all printer objects are instantiated'
         self.toolhead = self.printer.lookup_object('toolhead')
         gcode_move = self.printer.lookup_object('gcode_move')
+        
+        _warn_msg = '''This Klipper branch is deprecated and will no longer be
+        maintained.
+        Please use the Moonraker plug-in version available at
+        https://github.com/alchemyEngine/klipper_frame_expansion_comp
+        '''
+        self.config.deprecate('frame_expansion_compensation', 'temp_coeff',
+                              msg=_warn_msg)
 
         # Detect old temp sensor config section
         try:
