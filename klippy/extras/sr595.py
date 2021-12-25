ba88a75761ee47c4faabf16d810d667d6c055a26
Heino Mortensen <hm@dansknet.dk>
2021-12-25 11:47:17 +0100

    Fixed long line

--- klippy/extras/sr595.py
+++ klippy/extras/sr595.py
@@ -1,32 +1,15 @@
 # Code to create virtual pins for shift registers
 # Tested with SN74HC595
 #
-# [sr595 sr]
-# data_pin:
-# clock_pin:
-# latch_pin:
-#
-# Turn on Qd / Q3 pin on the first shift register
-# SET_SR_PIN SR=sr NUMBER=4 VALUE=1
-#
-# Turn off Qb / Q2 pin on the second shift register
-# SET_SR_PIN SR=sr NUMBER=10 VALUE=0
-#
-# Toggle the value of Qb / Q2 pin on the second shift register
-# TOGGLE_SR_PIN SR=sr NUMBER=10
-#
-# Set the value of all pins on all connected registers
-# SR2 7-0   SR1 7-0
-# 10101010  01010101    = 43605
-# SET_SR_PINS SR=sr VALUE=43605
-#
 # Copyright (C) 2021  Heino Mortensen <heino@no-mercy.dk>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 from . import bus
 
+
 class error(Exception):
     pass
+
 
 class _SrFromConfig:
     def __init__(self, config):
@@ -64,11 +47,13 @@ class _SrFromConfig:
     cmd_SET_SR_PINS_help = "Set the value of all output pins"
 
     def cmd_SET_SR_PINS(self, gcmd):
-        value = gcmd.get_int('VALUE', minval=0, maxval=(256 ** self.chip_count) - 1)
+        value = gcmd.get_int('VALUE', minval=0,
+                             maxval=(256 ** self.chip_count) - 1)
         self.sr.set_value(value)
 
     def get_status(self, eventtime):
         return self.sr.get_status(eventtime)
+
 
 class Sr:
     def __init__(self, printer, chip_count, data_pin, clock_pin, latch_pin):
@@ -159,8 +144,8 @@ class Sr:
 
     def _is_valid_pin(self, pin):
         if pin < 0 or pin >= 8 * self.chip_count:
-                raise error("Invalid pin number %d - Must be between 0 and %d" %
-                            (pin, (8 * self.chip_count) - 1))
+            raise error("Invalid pin number %d - Must be between 0 and %d" %
+                        (pin, (8 * self.chip_count) - 1))
 
     def _set_bit(self, value, offset):
         mask = 1 << offset
