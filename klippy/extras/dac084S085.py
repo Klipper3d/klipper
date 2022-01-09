ea1d915e014bac87dfbb7b655c85e7c377a7a314
lorenzing <lorenzo.franco94@gmail.com>
2022-01-09 21:03:22 +0100

    Add dac file
    Alligator board Digital/Analog converter setup

---
+++ klippy/extras/dac084S085.py
@@ -0,0 +1,28 @@
+# SPI DAC DAC084S085 implementation for Alligator Board rev.2
+#
+# Copyright (C) 2021  Lorenzo Franco <lorenzo.franco@lorenzing.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+from . import bus
+import time
+
+class dac084S085:
+	def __init__(self, config):
+		self.spi = bus.MCU_SPI_from_config(
+			config, 1, pin_option="enable_pin", default_speed=10000000)
+
+		channel = [0,3,2,1,0 ]
+
+		for i in range(5):
+			vref = config.getint('motor%d' % (i,), None, 
+						minval=0., maxval=255)
+			if vref is not None:
+				buff = 0x01 << 12
+				buff |= (channel[i]) << 14
+				buff |= vref << 4
+				self.spi.spi_send([buff])
+				time.sleep(0.05)
+
+def load_config_prefix(config):
+	return dac084S085(config)
