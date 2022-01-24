0c24796016ef9b70e6f4d19a4f9d9b401f4abde1
Claudio <lord.strambo@gmail.com>
2022-01-24 18:20:49 +0100

    Update filament_motion_sensor.py
    added option: consecutive_count (default 3)
    added gcode: FILAMENT_MOTION_SENSOR VERBOSE=[1/0] in order to enable the verbose
    Changed the check _extruder_pos_update_event in order set filament not present when the filament difference beetwen the runnout sensor and the extruder position is greater than the detection_length for consecutive_count times.
    
    In my test with this mode i can use the detection_lenght to 4mm (my sensor have 3mm + some margin seen with verbose) and trigger the runout early in case of clogs

--- klippy/extras/filament_motion_sensor.py
+++ klippy/extras/filament_motion_sensor.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2021 Joshua Wherrett <thejoshw.code@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
+import logging, collections
 from . import filament_switch_sensor
 
 CHECK_RUNOUT_TIMEOUT = .250
@@ -16,6 +16,8 @@ class EncoderSensor:
         self.extruder_name = config.get('extruder')
         self.detection_length = config.getfloat(
                 'detection_length', 7., above=0.)
+        self.consecutive_count = config.getint('consecutive_count', 3, minval=1)
+        self.current_count = 0
         # Configure pins
         buttons = self.printer.load_object(config, 'buttons')
         buttons.register_buttons([switch_pin], self.encoder_event)
@@ -27,6 +29,8 @@ class EncoderSensor:
         self.estimated_print_time = None
         # Initialise internal state
         self.filament_runout_pos = None
+        self.verbose = False
+        self.verbose_gcmd = None
         # Register commands and event handlers
         self.printer.register_event_handler('klippy:ready',
                 self._handle_ready)
@@ -36,12 +40,31 @@ class EncoderSensor:
                 self._handle_not_printing)
         self.printer.register_event_handler('idle_timeout:idle',
                 self._handle_not_printing)
+        # Register GCODE commands
+        self.gcode = self.printer.lookup_object('gcode')
+        self.gcode.register_command(
+            'FILAMENT_MOTION_SENSOR', self.cmd_FILAMENT_MOTION_SENSOR,
+            desc=self.cmd_FILAMENT_MOTION_SENSOR_help)
+    cmd_FILAMENT_MOTION_SENSOR_help = "Enable debug logging for the encoder event"
+    def cmd_FILAMENT_MOTION_SENSOR(self, gcmd):
+        options = collections.OrderedDict({
+            'VERBOSE': None
+        })
+        for key in options:
+            name = gcmd.get(key,None)
+            if name is not None:
+                name = name.lower()
+                if name == "1" or name == "on" or name == "true":
+                    self.verbose = True
+                    self.verbose_gcmd = gcmd
+                else:
+                    self.verbose = False
+                    self.verbose_gcmd = None
+            gcmd.respond_info("Filament Motion Verbose is %s" % ("On" if self.verbose else "Off"))
     def _update_filament_runout_pos(self, eventtime=None):
         if eventtime is None:
             eventtime = self.reactor.monotonic()
-        self.filament_runout_pos = (
-                self._get_extruder_pos(eventtime) +
-                self.detection_length)
+        self.filament_runout_pos = self._get_extruder_pos(eventtime)
     def _handle_ready(self):
         self.extruder = self.printer.lookup_object(self.extruder_name)
         self.estimated_print_time = (
@@ -50,6 +73,7 @@ class EncoderSensor:
         self._extruder_pos_update_timer = self.reactor.register_timer(
                 self._extruder_pos_update_event)
     def _handle_printing(self, print_time):
+        self.current_count = 0
         self.reactor.update_timer(self._extruder_pos_update_timer,
                 self.reactor.NOW)
     def _handle_not_printing(self, print_time):
@@ -63,8 +87,18 @@ class EncoderSensor:
     def _extruder_pos_update_event(self, eventtime):
         extruder_pos = self._get_extruder_pos(eventtime)
         # Check for filament runout
-        self.runout_helper.note_filament_present(
-                extruder_pos < self.filament_runout_pos)
+        diff_pos = abs(extruder_pos - self.filament_runout_pos)
+        it_fail = diff_pos > self.detection_length
+        is_filament_present = True
+        if it_fail:
+            self.current_count += 1
+            if self.current_count >= self.consecutive_count:
+                is_filament_present = False
+        self.runout_helper.note_filament_present(is_filament_present)
+        if self.verbose:
+            self.verbose_gcmd.respond_info(
+                "Filament Motion: Update Event pos %s present %s" %
+                diff_pos, "true" if is_filament_present else "false")
         return eventtime + CHECK_RUNOUT_TIMEOUT
     def encoder_event(self, eventtime, state):
         if self.extruder is not None:
@@ -72,6 +106,7 @@ class EncoderSensor:
             # Check for filament insertion
             # Filament is always assumed to be present on an encoder event
             self.runout_helper.note_filament_present(True)
+            self.current_count = 0
 
 def load_config_prefix(config):
     return EncoderSensor(config)
