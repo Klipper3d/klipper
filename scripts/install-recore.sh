db5f9b578d72d61202266eb8847b23156a66e57f
Elias Bakken <elias@iagent.no>
2022-01-27 01:30:47 +0100

    Update klipper service

--- scripts/install-recore.sh
+++ scripts/install-recore.sh
@@ -65,7 +65,7 @@ ExecStartPre=/usr/bin/gpioset 1 196=0
 ExecStartPre=/usr/bin/gpioget 1 196
 ExecStartPre=${SRCDIR}/scripts/flash-ar100.py /opt/firmware/ar100.bin
 ExecStart=${PYTHONDIR}/bin/python ${SRCDIR}/klippy/klippy.py ${HOME}/printer.cfg -l ${KLIPPER_LOG}
-ExecStopPost=${SRCDIR}/scripts/flash-ar100.py --halt
+ExecStopPost=${SRCDIR}/scripts/flash-ar100.py --bl31 --halt /opt/firmware/bl31.bin
 EOF
 # Use systemctl to enable the klipper systemd service script
     sudo systemctl enable klipper.service
