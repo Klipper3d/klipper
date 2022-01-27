82e9849bdba614fb6d2d54e7077b2477af84dc8c
Yifei Ding <dingyifeiair@gmail.com>
2022-01-26 22:21:43 -0800

    scripts: Change pre-mkdocs.sh to work with only one language
    Signed-off-by: Yifei Ding<yifeiding@protonmail.com>

--- scripts/pre-mkdocs.sh
+++ scripts/pre-mkdocs.sh
@@ -2,18 +2,12 @@
 # Modify the file structure before running mkdocs
 # This is a make shift script before the current structure of
 # Klipper-translations can be directly utilized by mkdocs
+# Usage: pre-mkdocs.sh <lang_directory_name> <target_lang_name>
 
 git submodule add --force https://github.com/Klipper3d/klipper-translations
-
-for dir in klipper-translations/docs/locales/*/; do
-
-  dir=${dir%*/}
-  lang=${dir##*/}
-  if [ $lang = "zh-hans" ]; then
-    echo "Moving $lang"
-    for file in klipper-translations/docs/locales/$lang/*.md; do
-      mdfilename=${file/$dir\//}
-      mv $file "./docs/${mdfilename//.md/.zh.md}"
-    done
-  fi
+dir=klipper-translations/docs/locales/$1
+echo "Moving $1"
+for file in $dir/*.md; do
+  mdfilename=${file/$dir\//}
+  mv $file "./docs/${mdfilename//.md/.$2.md}"
 done
