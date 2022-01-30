3408519c0c5088f1fda81240f85a97430400156b
Elias Bakken <elias@iagent.no>
2022-01-30 21:30:13 +0100

    Add author to file

--- klippy/extras/recore.py
+++ klippy/extras/recore.py
@@ -1,6 +1,7 @@
 # Config for Recore,
 #
 # Copyright (C) 2017-2019  Kevin O'Connor <kevin@koconnor.net>
+# Copyright (C) 2019-2022  Elias Bakken <elias@iagent.no>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging, os
