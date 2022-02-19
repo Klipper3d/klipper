4f3d3c37531f57e6c706ecfb49e1b06bca64e4a0
Lorenzo <lorenzo.franco94@gmail.com>
2022-02-19 13:05:55 +0100

    Update dac084S085.py
    Add channel scaled value in amps.

--- klippy/extras/dac084S085.py
+++ klippy/extras/dac084S085.py
@@ -10,14 +10,15 @@ class dac084S085:
     def __init__(self, config):
         self.spi = bus.MCU_SPI_from_config(
             config, 1, pin_option="enable_pin", default_speed=10000000)
-        channel = [0,3,2,1,0 ]
+	scale = config.getfloat('scale', 1., above=0.)
+        channel = [0,3,2,1,0]
         for i in range(5):
-            vref = config.getint('channel%d' % (i,), None,
-                                 minval=0., maxval=255)
+            vref = config.getint('channel_%d’ % (i,), None,
+                                 minval=0., maxval= scale)
             if vref is not None:
                 buff = 0x01 << 12
                 buff |= (channel[i]) << 14
-                buff |= vref << 4
+                buff |= int(val * 255. / scale) << 4
                 self.spi.spi_send([buff])
 
 def load_config_prefix(config):
