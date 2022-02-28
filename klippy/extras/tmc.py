84d8b4c9422de642741bcce9dde156f8e5b94163
camerony <camerony@users.noreply.github.com>
2022-02-28 07:29:35 -0800

    Update tmc.py

--- klippy/extras/tmc.py
+++ klippy/extras/tmc.py
@@ -413,7 +413,6 @@ class TMCCommandHelper:
             if self.read_translate is not None:
                 reg_name, val = self.read_translate(reg_name, val)
             gcmd.respond_info(self.fields.pretty_format(reg_name, val))
-            
 
 
 ######################################################################
@@ -530,11 +529,10 @@ def TMCStealthchopHelper(config, mcu_tmc, tmc_freq):
         threshold = int(tmc_freq * step_dist_256 / velocity + .5)
         fields.set_field("tpwmthrs", max(0, min(0xfffff, threshold)))
         en_pwm_mode = True
-        logging.info("TMC stepper_name", stepper_name)
-        logging.info("TMC sconfig", sconfig)
-        logging.info("TMC step_dist", step_dist)
-        logging.info("TMC threshold", threshold)
-        logging.info("TMC tpwmthrs", max(0, min(0xfffff, threshold)))
+        logging.info("TMC step_dist '%s'", step_dist)
+        logging.info("TMC step_dist_256 '%s'", step_dist_256)
+        logging.info("TMC threshold '%s'", threshold)
+        logging.info("TMC tpwmthrs '%s'", max(0, min(0xfffff, threshold)))
     reg = fields.lookup_register("en_pwm_mode", None)
     if reg is not None:
         fields.set_field("en_pwm_mode", en_pwm_mode)
