Klipper3d/klipper

commit 80492432210f1cf7817b7808245d196f3420021e

atsam: Add support for SAM E70
Signed-off-by: Alex Maclean <monkeh@monkeh.net>

src/atsam/sam4e_afec.c
@@ -7,15 +7,19 @@
 #include "autoconf.h" // CONFIG_CLOCK_FREQ
 #include "command.h" // shutdown
 #include "gpio.h" // gpio_adc_setup
-#include "internal.h" // GPIO
-#include "sam4e.h" // AFEC0
+#include "internal.h" // GPIO, AFEC0
 #include "sched.h" // sched_shutdown
+
+#if CONFIG_MACH_SAME70
+#include "same70_afec.h" // Fixes for upstream header changes
+#endif
 
 #define ADC_TEMPERATURE_PIN 0xfe
 DECL_ENUMERATION("pin", "ADC_TEMPERATURE", ADC_TEMPERATURE_PIN);
 
 static const uint8_t afec_pins[] = {
     //remove first channel, since it offsets the channel number: GPIO('A', 8),
+    #if CONFIG_MACH_SAM4E
     GPIO('A', 17), GPIO('A', 18), GPIO('A', 19),
     GPIO('A', 20), GPIO('B', 0),  GPIO('B', 1), GPIO('C', 13),
     GPIO('C', 15), GPIO('C', 12), GPIO('C', 29), GPIO('C', 30),
@@ -24,9 +28,40 @@ static const uint8_t afec_pins[] = {
     // AFEC1
     GPIO('B', 2), GPIO('B', 3), GPIO('A', 21), GPIO('A', 22),
     GPIO('C', 1), GPIO('C', 2), GPIO('C', 3), GPIO('C', 4),
+    #elif CONFIG_MACH_SAME70
+    GPIO('D', 30), GPIO('A', 21), GPIO('B', 3),  GPIO('E', 5),
+    GPIO('E', 4),  GPIO('B', 2),  GPIO('A', 17), GPIO('A', 18),
+    GPIO('A', 19), GPIO('A', 20), GPIO('B', 0),
+    ADC_TEMPERATURE_PIN,
+    // AFEC1
+    GPIO('B', 1),  GPIO('C', 13), GPIO('C', 15), GPIO('C', 12),
+    GPIO('C', 29), GPIO('C', 30), GPIO('C', 31), GPIO('C', 26),
+    GPIO('C', 27), GPIO('C', 0),  GPIO('E', 3),  GPIO('E', 0),
+    #endif
 };
 
+#if CONFIG_MACH_SAM4E
 #define AFEC1_START 16 // The first 16 pins are on afec0
+#define CFG_AFE_MR (AFE_MR_ANACH_ALLOWED | \
+                    AFE_MR_PRESCAL(pclk / (2 * ADC_FREQ_MAX) - 1) | \
+                    AFE_MR_SETTLING_AST3 | \
+                    AFE_MR_TRACKTIM(2) | \
+                    AFE_MR_TRANSFER(1) | \
+                    AFE_MR_STARTUP_SUT64)
+#define CFG_AFE_ACR AFE_ACR_IBCTL(1)
+#define CFG_AFE_IDR 0xDF00FFFF
+#define CFG_AFE_COCR (0x800 & AFE_COCR_AOFF_Msk)
+
+#elif CONFIG_MACH_SAME70
+#define AFEC1_START 12 // The first 12 pins are on afec0
+#define CFG_AFE_MR (AFEC_MR_ONE | \
+                    AFE_MR_PRESCAL (pclk / (ADC_FREQ_MAX) -1) | \
+                    AFE_MR_TRANSFER(2) | \
+                    AFE_MR_STARTUP_SUT64)
+#define CFG_AFE_ACR (AFE_ACR_IBCTL(1) | AFEC_ACR_PGA0EN | AFEC_ACR_PGA1EN)
+#define CFG_AFE_IDR 0x47000FFF
+#define CFG_AFE_COCR (0x200 & AFE_COCR_AOFF_Msk)
+#endif
 
 static inline struct gpio_adc
 pin_to_gpio_adc(uint8_t pin)
@@ -73,19 +108,14 @@ init_afec(Afec* afec) {
 
     // Configure afec
     uint32_t pclk = get_pclock_frequency(afec == AFEC0 ? ID_AFEC0 : ID_AFEC1);
-    afec->AFE_MR = AFE_MR_ANACH_ALLOWED | \
-                    AFE_MR_PRESCAL(pclk / (2 * ADC_FREQ_MAX) - 1) | \
-                    AFE_MR_SETTLING_AST3 | \
-                    AFE_MR_TRACKTIM(2) | \
-                    AFE_MR_TRANSFER(1) | \
-                    AFE_MR_STARTUP_SUT64;
+    afec->AFE_MR = CFG_AFE_MR;
     afec->AFE_EMR = AFE_EMR_TAG | \
                      AFE_EMR_RES_NO_AVERAGE | \
                      AFE_EMR_STM;
-    afec->AFE_ACR = AFE_ACR_IBCTL(1);
+    afec->AFE_ACR = CFG_AFE_MR;
 
     // Disable interrupts
-    afec->AFE_IDR = 0xDF00803F;
+    afec->AFE_IDR = CFG_AFE_IDR;
 
     // Disable SW triggering
     uint32_t mr = afec->AFE_MR;
@@ -123,23 +153,25 @@ gpio_adc_setup(uint8_t pin)
     afec->AFE_DIFFR = reg;
     reg = afec->AFE_CGR;
     reg &= ~(0x03u << (2 * afec_chan));
-    reg |= 1 << (2 * afec_chan);
     afec->AFE_CGR = reg;
 
     // Configure channel
     // afec_ch_get_config_defaults(&ch_cfg);
     // afec_ch_set_config(afec, afec_chan, &ch_cfg);
     // Remove default internal offset from channel
-    // See Atmel Appnote AT03078 Section 1.5
+    // See Atmel Appnote AT03078 Section 1.5 for SAM4E,
+    // datasheet section 52.6.11 for SAME70
     afec->AFE_CSELR = afec_chan;
-    afec->AFE_COCR = (0x800 & AFE_COCR_AOFF_Msk);
+    afec->AFE_COCR = CFG_AFE_COCR;
 
     // Enable and calibrate Channel
     afec->AFE_CHER = 1 << afec_chan;
 
+    #if CONFIG_MACH_SAM4E
     reg = afec->AFE_CHSR;
     afec->AFE_CDOR = reg;
     afec->AFE_CR = AFE_CR_AUTOCAL;
+    #endif
 
     return adc_pin;
 }

klippy/extras/temperature_mcu.py
@@ -64,6 +64,7 @@ class PrinterTemperatureMCU:
         cfg_funcs = [
             ('rp2040', self.config_rp2040),
             ('sam3', self.config_sam3), ('sam4', self.config_sam4),
+            ('same70', self.config_same70),
             ('samd21', self.config_samd21), ('samd51', self.config_samd51),
             ('stm32f1', self.config_stm32f1), ('stm32f2', self.config_stm32f2),
             ('stm32f4', self.config_stm32f4),
@@ -100,6 +101,9 @@ class PrinterTemperatureMCU:
     def config_sam4(self):
         self.slope = 3.3 / .004700
         self.base_temperature = self.calc_base(27., 1.44 / 3.3)
+    def config_same70(self):
+        self.slope = 3.3 / .002330
+        self.base_temperature = self.calc_base(25., 0.72 / 3.3)
     def config_samd21(self, addr=0x00806030):
         def get1v(val):
             if val & 0x80:

scripts/flash_usb.py
@@ -286,7 +286,7 @@ def flash_rp2040(options, binfile):
 
 MCUTYPES = {
     'sam3': flash_atsam3, 'sam4': flash_atsam4, 'samd': flash_atsamd,
-    'lpc176': flash_lpc176x, 'stm32f103': flash_stm32f1,
+    'same70': flash_atsam4, 'lpc176': flash_lpc176x, 'stm32f103': flash_stm32f1,
     'stm32f4': flash_stm32f4, 'stm32f042': flash_stm32f4,
     'stm32f072': flash_stm32f4, 'rp2040': flash_rp2040
 }

src/Kconfig
@@ -14,7 +14,7 @@ choice
     config MACH_AVR
         bool "Atmega AVR"
     config MACH_ATSAM
-        bool "SAM3/SAM4 (Due and Duet)"
+        bool "SAM3/SAM4/SAM E70 (Due and Duet)"
     config MACH_ATSAMD
         bool "SAMD21/SAMD51"
     config MACH_LPC176X

src/atsam/Kconfig
@@ -9,7 +9,7 @@ config ATSAM_SELECT
     select HAVE_GPIO_ADC
     select HAVE_GPIO_I2C
     select HAVE_GPIO_SPI
-    select HAVE_GPIO_HARD_PWM
+    select HAVE_GPIO_HARD_PWM if !MACH_SAME70
     select HAVE_GPIO_BITBANGING
     select HAVE_STRICT_TIMING
     select HAVE_CHIPID
@@ -33,6 +33,9 @@ choice
     config MACH_SAM4E8E
         bool "SAM4e8e (Duet Wifi/Eth)"
         select MACH_SAM4E
+    config MACH_SAME70Q20B
+        bool "SAME70Q20B (Duet 3 6HC)"
+        select MACH_SAME70
 endchoice
 
 config MACH_SAM3X
@@ -45,6 +48,8 @@ config MACH_SAM4S
 config MACH_SAM4E
     bool
     select MACH_SAM4
+config MACH_SAME70
+    bool
 
 config MCU
     string
@@ -52,15 +57,17 @@ config MCU
     default "sam3x8c" if MACH_SAM3X8C
     default "sam4s8c" if MACH_SAM4S8C
     default "sam4e8e" if MACH_SAM4E8E
+    default "same70q20b" if MACH_SAME70Q20B
 
 config CLOCK_FREQ
     int
     default 84000000 if MACH_SAM3X
     default 120000000 if MACH_SAM4
+    default 300000000 if MACH_SAME70
 
 config FLASH_START
     hex
-    default 0x400000 if MACH_SAM4
+    default 0x400000 if MACH_SAM4 || MACH_SAME70
     default 0x80000
 
 config FLASH_SIZE
@@ -69,12 +76,14 @@ config FLASH_SIZE
 
 config RAM_START
     hex
+    default 0x20400000 if MACH_SAME70
     default 0x20000000
 
 config RAM_SIZE
     hex
     default 0x18000 if MACH_SAM3X
     default 0x20000 if MACH_SAM4
+    default 0x40000 if MACH_SAME70
 
 config STACK_SIZE
     int

src/atsam/Makefile
@@ -7,14 +7,17 @@ dirs-y += src/atsam src/generic
 dirs-$(CONFIG_MACH_SAM3X) += lib/sam3x/gcc
 dirs-$(CONFIG_MACH_SAM4S) += lib/sam4s/gcc
 dirs-$(CONFIG_MACH_SAM4E) += lib/sam4e/gcc
+dirs-$(CONFIG_MACH_SAME70) += lib/same70b/gcc
 
 MCU := $(shell echo $(CONFIG_MCU) | tr a-z A-Z)
 
 CFLAGS-$(CONFIG_MACH_SAM3X) += -mcpu=cortex-m3 -falign-loops=16
 CFLAGS-$(CONFIG_MACH_SAM4) += -mcpu=cortex-m4
+CFLAGS-$(CONFIG_MACH_SAME70) += -mcpu=cortex-m7
 CFLAGS-$(CONFIG_MACH_SAM3X) += -Ilib/sam3x/include
 CFLAGS-$(CONFIG_MACH_SAM4S) += -Ilib/sam4s/include
 CFLAGS-$(CONFIG_MACH_SAM4E) += -Ilib/sam4e/include
+CFLAGS-$(CONFIG_MACH_SAME70) += -Ilib/same70b/include
 CFLAGS += $(CFLAGS-y) -D__$(MCU)__ -mthumb -Ilib/cmsis-core
 
 CFLAGS_klipper.elf += --specs=nano.specs --specs=nosys.specs
@@ -22,19 +25,22 @@ CFLAGS_klipper.elf += -T $(OUT)src/generic/armcm_link.ld
 $(OUT)klipper.elf: $(OUT)src/generic/armcm_link.ld
 
 # Add source files
-src-y += atsam/main.c atsam/gpio.c atsam/i2c.c atsam/spi.c atsam/hard_pwm.c
+src-y += atsam/main.c atsam/gpio.c atsam/i2c.c atsam/spi.c
 src-y += generic/armcm_boot.c generic/armcm_irq.c generic/armcm_timer.c
 src-y += generic/crc16_ccitt.c
 usb-src-$(CONFIG_MACH_SAM3X) := atsam/sam3_usb.c
 usb-src-$(CONFIG_MACH_SAM4) := atsam/sam4_usb.c
+usb-src-$(CONFIG_MACH_SAME70) := atsam/sam3_usb.c
 src-$(CONFIG_USBSERIAL) += $(usb-src-y) atsam/chipid.c generic/usb_cdc.c
 src-$(CONFIG_SERIAL) += atsam/serial.c generic/serial_irq.c
-src-$(CONFIG_MACH_SAM3X) += atsam/adc.c
+src-$(CONFIG_MACH_SAM3X) += atsam/adc.c atsam/hard_pwm.c
+src-$(CONFIG_MACH_SAM4)  += atsam/hard_pwm.c
 src-$(CONFIG_MACH_SAM4S) += atsam/adc.c
 src-$(CONFIG_MACH_SAM4E) += atsam/sam4e_afec.c atsam/sam4_cache.c
 src-$(CONFIG_MACH_SAM3X) += ../lib/sam3x/gcc/system_sam3xa.c
 src-$(CONFIG_MACH_SAM4S) += atsam/sam4s_sysinit.c
 src-$(CONFIG_MACH_SAM4E) += ../lib/sam4e/gcc/system_sam4e.c
+src-$(CONFIG_MACH_SAME70) += atsam/same70_sysinit.c atsam/sam4e_afec.c
 
 # Build the additional bin output file
 target-y += $(OUT)klipper.bin

src/atsam/gpio.c
@@ -11,6 +11,11 @@
 #include "internal.h" // gpio_peripheral
 #include "sched.h" // sched_shutdown
 
+struct gpio_info {
+    void *dev;
+    uint8_t dev_id;
+};
+
 DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 32);
 DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 32);
 #ifdef PIOC
@@ -23,16 +28,17 @@ DECL_ENUMERATION_RANGE("pin", "PD0", GPIO('D', 0), 32);
 DECL_ENUMERATION_RANGE("pin", "PE0", GPIO('E', 0), 32);
 #endif
 
-static Pio * const digital_regs[] = {
-    PIOA, PIOB,
+static const struct gpio_info digital_regs[] = {
+    { PIOA, ID_PIOA },
+    { PIOB, ID_PIOB },
 #ifdef PIOC
-    PIOC,
+    { PIOC, ID_PIOC },
 #endif
 #ifdef PIOD
-    PIOD,
+    { PIOD, ID_PIOD },
 #endif
 #ifdef PIOE
-    PIOE,
+    { PIOE, ID_PIOE },
 #endif
 };
 
@@ -65,6 +71,9 @@ set_pull_up(Pio *regs, uint32_t bit, int32_t pull_up)
     // Check if this pin is a "system IO pin" and disable if so
     if (regs == PIOB && (bit & 0x1cf0))
         MATRIX->CCFG_SYSIO |= bit;
+#elif CONFIG_MACH_SAME70
+    if (regs == PIOB && (bit & 0x10f0))
+        MATRIX->CCFG_SYSIO |= bit;
 #endif
 }
 
@@ -72,7 +81,7 @@ void
 gpio_peripheral(uint32_t gpio, char ptype, int32_t pull_up)
 {
     uint32_t bank = GPIO2PORT(gpio), bit = GPIO2BIT(gpio), pt = ptype - 'A';
-    Pio *regs = digital_regs[bank];
+    Pio *regs = digital_regs[bank].dev;
 
 #if CONFIG_MACH_SAM3X
     regs->PIO_ABSR = (regs->PIO_ABSR & ~bit) | (pt & 0x01 ? bit : 0);
@@ -94,7 +103,7 @@ gpio_out_setup(uint8_t pin, uint8_t val)
 {
     if (GPIO2PORT(pin) >= ARRAY_SIZE(digital_regs))
         goto fail;
-    Pio *regs = digital_regs[GPIO2PORT(pin)];
+    Pio *regs = digital_regs[GPIO2PORT(pin)].dev;
     struct gpio_out g = { .regs=regs, .bit=GPIO2BIT(pin) };
     gpio_out_reset(g, val);
     return g;
@@ -152,8 +161,8 @@ gpio_in_setup(uint8_t pin, int8_t pull_up)
     if (CONFIG_MACH_SAM3X && pull_up < 0)
         goto fail;
     uint32_t port = GPIO2PORT(pin);
-    enable_pclock(ID_PIOA + port);
-    struct gpio_in g = { .regs=digital_regs[port], .bit=GPIO2BIT(pin) };
+    enable_pclock(digital_regs[port].dev_id);
+    struct gpio_in g = { .regs=digital_regs[port].dev, .bit=GPIO2BIT(pin) };
     gpio_in_reset(g, pull_up);
     return g;
 fail:

src/atsam/i2c.c
@@ -11,35 +11,62 @@
 #include "internal.h" // gpio_peripheral
 #include "sched.h" // sched_shutdown
 
-// I2C pin definitions
-DECL_ENUMERATION_RANGE("i2c_bus", "twi0", 0, 2);
-#if CONFIG_MACH_SAM3X
-#define TWI0_SCL_GPIO GPIO('A', 18)
-#define TWI0_SDA_GPIO GPIO('A', 17)
-#define TWI1_SCL_GPIO GPIO('B', 13)
-#define TWI1_SDA_GPIO GPIO('B', 12)
-DECL_CONSTANT_STR("BUS_PINS_twi0", "PA18,PA17");
-DECL_CONSTANT_STR("BUS_PINS_twi1", "PB13,PB12");
-#elif CONFIG_MACH_SAM4
-#define TWI0_SCL_GPIO GPIO('A', 4)
-#define TWI0_SDA_GPIO GPIO('A', 3)
-#define TWI1_SCL_GPIO GPIO('B', 5)
-#define TWI1_SDA_GPIO GPIO('B', 4)
-DECL_CONSTANT_STR("BUS_PINS_twi0", "PA4,PA3");
-DECL_CONSTANT_STR("BUS_PINS_twi1", "PB5,PB4");
+#if CONFIG_MACH_SAME70
+#include "same70_i2c.h" // Fixes for upstream header changes
 #endif
 
+struct twi_info {
+    void *dev;
+    uint32_t dev_id;
+    uint8_t scl_pin, sda_pin, periph;
+};
+
+// I2C pin definitions
+#if CONFIG_MACH_SAM3X
+DECL_ENUMERATION_RANGE("i2c_bus", "twi0", 0, 2);
+DECL_CONSTANT_STR("BUS_PINS_twi0", "PA18,PA17");
+DECL_CONSTANT_STR("BUS_PINS_twi1", "PB13,PB12");
+#define PRD_CALC_NUM 4
+#elif CONFIG_MACH_SAM4
+DECL_ENUMERATION_RANGE("i2c_bus", "twi0", 0, 2);
+DECL_CONSTANT_STR("BUS_PINS_twi0", "PA4,PA3");
+DECL_CONSTANT_STR("BUS_PINS_twi1", "PB5,PB4");
+#define PRD_CALC_NUM 4
+#elif CONFIG_MACH_SAME70
+DECL_ENUMERATION_RANGE("i2c_bus", "twihs0", 0,3);
+DECL_CONSTANT_STR("BUS_PINS_twihs0", "PA4,PA3");
+DECL_CONSTANT_STR("BUS_PINS_twihs1", "PB5,PB4");
+DECL_CONSTANT_STR("BUS_PINS_twihs2", "PD28,PD27");
+#define PRD_CALC_NUM 3
+#endif
+
+static const struct twi_info twi_bus[] = {
+#if CONFIG_MACH_SAM3X
+    { TWI0, ID_TWI0, GPIO('A', 18), GPIO('A', 17), 'A'},
+    { TWI1, ID_TWI1, GPIO('B', 13), GPIO('B', 12), 'A'},
+#elif CONFIG_MACH_SAM4
+    { TWI0, ID_TWI0, GPIO('A', 4), GPIO('A', 3), 'A'},
+    { TWI1, ID_TWI1, GPIO('B', 5), GPIO('B', 4), 'A'},
+#elif CONFIG_MACH_SAME70
+    { TWIHS0, ID_TWIHS0, GPIO('A', 4), GPIO('A', 3), 'A'},
+    { TWIHS1, ID_TWIHS1, GPIO('B', 5), GPIO('B', 4), 'A'},
+    { TWIHS2, ID_TWIHS2, GPIO('D', 28), GPIO('D', 27), 'C'},
+#endif
+};
+
 static void
-i2c_init(Twi *p_twi, uint32_t rate)
+init_pins(uint32_t bus)
 {
-    enable_pclock(p_twi == TWI0 ? ID_TWI0 : ID_TWI1);
-    if (p_twi == TWI0) {
-        gpio_peripheral(TWI0_SCL_GPIO, 'A', 1);
-        gpio_peripheral(TWI0_SDA_GPIO, 'A', 1);
-    } else {
-        gpio_peripheral(TWI1_SCL_GPIO, 'A', 1);
-        gpio_peripheral(TWI1_SDA_GPIO, 'A', 1);
-    }
+    const struct twi_info *si = &twi_bus[bus];
+    gpio_peripheral(si->scl_pin, si->periph, 1);
+    gpio_peripheral(si->sda_pin, si->periph, 1);
+    enable_pclock(si->dev_id);
+}
+
+static void
+i2c_init(uint32_t bus, uint32_t rate)
+{
+    Twi *p_twi = twi_bus[bus].dev;
     p_twi->TWI_IDR = 0xFFFFFFFF;
     (void)p_twi->TWI_SR;
     p_twi->TWI_CR = TWI_CR_SWRST;
@@ -48,9 +75,9 @@ i2c_init(Twi *p_twi, uint32_t rate)
     p_twi->TWI_CR = TWI_CR_SVDIS;
     p_twi->TWI_CR = TWI_CR_MSEN;
 
-    uint32_t pclk = get_pclock_frequency(p_twi == TWI0 ? ID_TWI0 : ID_TWI1);
+    uint32_t pclk = get_pclock_frequency(twi_bus[bus].dev_id);
     uint32_t cldiv = 0, chdiv = 0, ckdiv = 0;
-    cldiv = pclk / ((rate > 384000 ? 384000 : rate) * 2) - 4;
+    cldiv = pclk / ((rate > 384000 ? 384000 : rate) * 2) - PRD_CALC_NUM;
 
     while ((cldiv > 255) && (ckdiv < 7)) {
         ckdiv++;
@@ -58,7 +85,7 @@ i2c_init(Twi *p_twi, uint32_t rate)
     }
 
     if (rate > 348000) {
-        chdiv = pclk / ((2 * rate - 384000) * 2) - 4;
+        chdiv = pclk / ((2 * rate - 384000) * 2) - PRD_CALC_NUM;
         while ((chdiv > 255) && (ckdiv < 7)) {
             ckdiv++;
             chdiv /= 2;
@@ -91,10 +118,11 @@ addr_to_u32(uint8_t addr_len, uint8_t *addr)
 struct i2c_config
 i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr)
 {
-    if (bus > 1 || rate > 400000)
+    if (bus >= ARRAY_SIZE(twi_bus)  || rate > 400000)
         shutdown("Invalid i2c_setup parameters!");
-    Twi *p_twi = (bus == 0) ? TWI0 : TWI1;
-    i2c_init(p_twi, rate);
+    Twi *p_twi = twi_bus[bus].dev;
+    init_pins(bus);
+    i2c_init(bus, rate);
     return (struct i2c_config){ .twi=p_twi, .addr=addr};
 }
 

commit fcf8c5ea98ea8ee6d7797cf37bede3dc8b058525

atsam: Fix typo in sam4e_afec
Accidentally introduced in 80492432210f1cf7817b7808245d196f3420021e,
renders the AFE non-functional on SAME70. Should have had little impact
on SAM4E.

Signed-off-by: Alex Maclean <monkeh@monkeh.net>

src/atsam/sam4e_afec.c
@@ -112,7 +112,7 @@ init_afec(Afec* afec) {
     afec->AFE_EMR = AFE_EMR_TAG | \
                      AFE_EMR_RES_NO_AVERAGE | \
                      AFE_EMR_STM;
-    afec->AFE_ACR = CFG_AFE_MR;
+    afec->AFE_ACR = CFG_AFE_ACR;
 
     // Disable interrupts
     afec->AFE_IDR = CFG_AFE_IDR;