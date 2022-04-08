f98b06f0edca64aa3d6501a8d1fa1a88526ba8a2
Elias Bakken <elias@iagent.no>
2022-04-08 23:23:42 +0200

    Use IV curves on input instead of fixed current

--- klippy/extras/recore_thermistor.py
+++ klippy/extras/recore_thermistor.py
@@ -16,6 +16,13 @@ class RecoreThermistor(thermistor.Thermistor):
          self.adc_ref = adc_ref
          self.pullup_ref = pullup_ref
          self.vo_ref = vo_ref
+         self.a = 1.84633422e-07
+         self.b = -3.15418589e-07
+         self.c = 3.16706799e-07
+         self.d = -5.08889891e-08
+
+    def smf5a_iv_curve(self, v):
+        return self.a*v**3 + self.b*v**2 + self.c*v + self.d
 
     def calc_temp(self, adc):
         # Calculate temperature from adc
@@ -24,14 +31,16 @@ class RecoreThermistor(thermistor.Thermistor):
         R2 = 100000
         R1 = 1000
 
-        I1 = (self.vo_ref-Vo)/R2
+        I1 = (Vo-self.vo_ref)/R2 # current through offset resistor
         VR1 = I1*R1
-        V3 = Vo-VR1
+        V3 = Vo+VR1 # Voltage at the divider.
+
+        ID =  self.smf5a_iv_curve(V3) # TVS diode current
         I5 = (self.pullup_ref-V3)/self.pullup
-        I3 = (I5+I1)
+        I3 = (I5-I1-ID)
         r = V3/I3
 
-        ln_r = math.log(r - self.inline_resistor)
+        ln_r = math.log(r)
         inv_t = self.c1 + self.c2 * ln_r + self.c3 * ln_r**3
         return 1.0/inv_t + KELVIN_TO_CELSIUS
 
@@ -39,9 +48,9 @@ class RecoreThermistor(thermistor.Thermistor):
 def RecorePrinterThermistor(config, params):
     pullup = config.getfloat('pullup_resistor', 4700., above=0.)
     inline_resistor = config.getfloat('inline_resistor', 0., minval=0.)
-    adc_ref = config.getfloat('adc_ref', 3.3)
-    pullup_ref = config.getfloat('pullup_ref', 3.2)
-    vo_ref = config.getfloat('vo_ref', 0)
+    adc_ref = config.getfloat('adc_voltage', 3.3)
+    pullup_ref = config.getfloat('pullup_voltage', 3.3)
+    vo_ref = config.getfloat('offset_voltage', 0.05)
     thermistor = RecoreThermistor(pullup, inline_resistor, adc_ref,
                                   pullup_ref, vo_ref)
 
