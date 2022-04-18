260fd7d367b78a53170dde80fb53dac12df7576e
Kevin O'Connor <kevin@koconnor.net>
2022-04-18 11:06:40 -0400

    pca9632: Fix color_order assignment
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/pca9632.py
+++ klippy/extras/pca9632.py
@@ -32,7 +32,7 @@ class PCA9632:
         color_order = config.get("color_order", "RGBW")
         if sorted(color_order) != sorted("RGBW"):
             raise config.error("Invalid color_order '%s'" % (color_order,))
-        self.color_map = [color_order.index(c) for c in "RGBW"]
+        self.color_map = ["RGBW".index(c) for c in color_order]
         self.prev_regs = {}
         pled = printer.load_object(config, "led")
         self.led_helper = pled.setup_helper(config, self.update_leds, 1)
