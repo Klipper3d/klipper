Klipper3d/klipper

commit 7ef7bf608a2e784505439e50c443f24e03f2f3cf

gcode_macro: Add "rawparams" pseudo-variable
Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

klippy/gcode.py
@@ -26,6 +26,22 @@ class GCodeCommand:
         return self._commandline
     def get_command_parameters(self):
         return self._params
+    def get_raw_command_parameters(self):
+        rawparams = self._commandline
+        command = self._command
+        urawparams = rawparams.upper()
+        if not urawparams.startswith(command):
+            start = urawparams.find(command)
+            end = rawparams.rfind('*')
+            if end >= 0:
+                rawparams = rawparams[:end]
+            rawparams = rawparams[start:]
+        commandlen = len(command) + 1
+        if len(rawparams) > commandlen:
+            rawparams = rawparams[commandlen:]
+        else:
+            rawparams = ''
+        return rawparams
     def ack(self, msg=None):
         if not self._need_ack:
             return False

klippy/extras/display_status.py
@@ -35,19 +35,8 @@ class DisplayStatus:
         curtime = self.printer.get_reactor().monotonic()
         self.expire_progress = curtime + M73_TIMEOUT
     def cmd_M117(self, gcmd):
-        msg = gcmd.get_commandline()
-        umsg = msg.upper()
-        if not umsg.startswith('M117'):
-            # Parse out additional info if M117 recd during a print
-            start = umsg.find('M117')
-            end = msg.rfind('*')
-            if end >= 0:
-                msg = msg[:end]
-            msg = msg[start:]
-        if len(msg) > 5:
-            self.message = msg[5:]
-        else:
-            self.message = None
+        msg = gcmd.get_raw_command_parameters() or None
+        self.message = msg
 
 def load_config(config):
     return DisplayStatus(config)

klippy/extras/gcode_macro.py
@@ -180,6 +180,7 @@ class GCodeMacro:
         kwparams = dict(self.variables)
         kwparams.update(self.template.create_template_context())
         kwparams['params'] = gcmd.get_command_parameters()
+        kwparams['rawparams'] = gcmd.get_raw_command_parameters()
         self.in_script = True
         try:
             self.template.run_gcode_from_command(kwparams)

klippy/extras/respond.py
@@ -22,17 +22,7 @@ class HostResponder:
         gcode.register_command('RESPOND', self.cmd_RESPOND, True,
                                desc=self.cmd_RESPOND_help)
     def cmd_M118(self, gcmd):
-        msg = gcmd.get_commandline()
-        umsg = msg.upper()
-        if not umsg.startswith('M118'):
-            # Parse out additional info if M118 recd during a print
-            start = umsg.find('M118')
-            end = msg.rfind('*')
-            msg = msg[start:end]
-        if len(msg) > 5:
-            msg = msg[5:]
-        else:
-            msg = ''
+        msg = gcmd.get_raw_command_parameters()
         gcmd.respond_raw("%s %s" % (self.default_prefix, msg))
     cmd_RESPOND_help = ("Echo the message prepended with a prefix")
     def cmd_RESPOND(self, gcmd):

commit 07e4455c54c2beffbf0f6b7dc314b7be546883c3

gcode: handle renamed M117/118  starting with special characters
f97fd7c6e392e376bd1552afdd39a2150d3e21d0 partially fixed the regression
from 7ef7bf608a2e784505439e50c443f24e03f2f3cf for the base gocde
commands, but not for renamed versions such as `M117.1`. This adds
support for the renamed gcode calls to the fix.

Signed-off-by: Owen O'Connor <tux2603@gmail.com>

klippy/gcode.py
@@ -30,6 +30,11 @@ class GCodeCommand:
         command = self._command
         if command.startswith("M117 ") or command.startswith("M118 "):
             command = command[:4]
+        elif command.startswith("M117.") or command.startswith("M118."):
+            # If there's a space, it means that there was a special character
+            space_index = command.find(" ")
+            if space_index >= 0:
+                command = command[:space_index]
         rawparams = self._commandline
         urawparams = rawparams.upper()
         if not urawparams.startswith(command):
@@ -282,6 +287,15 @@ class GCodeDispatch:
             if handler is not None:
                 handler(gcmd)
                 return
+        elif cmd.startswith("M117.") or cmd.startswith("M118."):
+            # Handle renamed M117/M118 gcode with numeric and special characters
+            space_index = cmd.find(' ')
+            if space_index >= 0:
+                cmd = cmd[:space_index]
+            handler = self.gcode_handlers.get(cmd, None)
+            if handler is not None:
+                handler(gcmd)
+                return
         elif cmd in ['M140', 'M104'] and not gcmd.get_float('S', 0.):
             # Don't warn about requests to turn off heaters when not present
             return
@@ -351,7 +365,6 @@ class GCodeDispatch:
             if cmd in self.gcode_help:
                 cmdhelp.append("%-10s: %s" % (cmd, self.gcode_help[cmd]))
         gcmd.respond_info("\n".join(cmdhelp), log=False)
-
 # Support reading gcode from a pseudo-tty interface
 class GCodeIO:
     def __init__(self, printer):