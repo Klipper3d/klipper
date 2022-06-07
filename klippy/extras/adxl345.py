478bc0b36e66d9c3fd0e54463ed025abc050345c
bluesforte <harry3b9@gmail.com>
2022-06-07 16:01:15 -0700

    fix import rename class for clarity

--- klippy/extras/adxl345.py
+++ klippy/extras/adxl345.py
@@ -1,12 +1,12 @@
 import logging
-import motion_sensor.adxl345
+from .motion_sensor import adxl345
 
 def load_config(config):
-    return ADXL345(config)
+    return ADXL345Legacy(config)
 
-class ADXL345 (motion_sensor.adxl345.ADXL345):
+class ADXL345Legacy (adxl345.ADXL345):
     def __init__(self, config):
-        super(ADXL345, self).__init__(config)
+        super(ADXL345Legacy, self).__init__(config)
         logging.warn(
             """The config section [adxl345] has been replaced by
              [motion_sensor] with 'chip: adxl345'.  Please update your
