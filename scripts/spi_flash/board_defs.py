6af931c4e1c5bf6f9590d0d6ad13d76e2594de77
Andreas Fröhlke <67454162+BOAndy1985@users.noreply.github.com>
2022-06-18 19:08:30 +0200

    spi_flash: add mks_monster8,robin_v3 (#5568)
    Signed-off-by Andreas Fröhlke <boandy1985@googlemail.com>

--- scripts/spi_flash/board_defs.py
+++ scripts/spi_flash/board_defs.py
@@ -65,6 +65,11 @@ BOARD_DEFS = {
         'mcu': 'stm32h743xx',
         'spi_bus': 'spi3a',
         'cs_pin': 'PA15'
+    },
+    'monster8': {
+        'mcu': "stm32f407xx",
+        'spi_bus': "spi3a",
+        "cs_pin": "PC9"
     }
 }
 
@@ -99,7 +104,9 @@ BOARD_ALIASES = {
     'mks-robin-e3d': BOARD_DEFS['mks-robin-e3'],
     'fysetc-spider-v1': BOARD_DEFS['fysetc-spider'],
     'fysetc-s6-v1.2': BOARD_DEFS['fysetc-spider'],
-    'fysetc-s6-v2': BOARD_DEFS['fysetc-spider']
+    'fysetc-s6-v2': BOARD_DEFS['fysetc-spider'],
+    'monster8': BOARD_DEFS['monster8'],
+    'robin_v3': BOARD_DEFS['monster8']
 }
 
 def list_boards():
