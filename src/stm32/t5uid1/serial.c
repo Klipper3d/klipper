140dca1f68a46e5ee86f1ce4f99a8bc39e12b342
Desuuuu <contact@desuuuu.com>
2022-10-09 19:11:29 +0200

    t5uid1: Improve STM32 support

--- src/stm32/t5uid1/serial.c
+++ src/stm32/t5uid1/serial.c
@@ -10,60 +10,94 @@
 #include "command.h" // DECL_CONSTANT_STR
 #include "../internal.h" // enable_pclock
 
-#if CONFIG_SERIAL && CONFIG_T5UID1_SERIAL_PORT == CONFIG_SERIAL_PORT
-  #error "The serial port selected for the T5UID1 screen is already used"
+#if CONFIG_MACH_STM32F0
+  #include "stm32f0_serial.h"
+#elif CONFIG_MACH_STM32G0
+  #include "stm32g0_serial.h"
+#elif CONFIG_MACH_STM32H7
+  #include "stm32h7_serial.h"
+#else
+  #include "stm32_serial.h"
 #endif
 
-// Select the configured serial port
-#if CONFIG_T5UID1_SERIAL_PORT == 1
-  DECL_CONSTANT_STR("RESERVE_PINS_t5uid1", "PA10,PA9");
+#if CONFIG_STM32_T5UID1_SERIAL_USART1_PA10_PA9
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA10,PA9");
   #define GPIO_Rx GPIO('A', 10)
   #define GPIO_Tx GPIO('A', 9)
   #define USARTx USART1
   #define USARTx_IRQn USART1_IRQn
-#elif CONFIG_T5UID1_SERIAL_PORT == 2
-  DECL_CONSTANT_STR("RESERVE_PINS_t5uid1", "PA3,PA2");
+  #define USARTx_FUNCTION USART1_PA10_PA9_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART1_PB7_PB6
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PB7,PB6");
+  #define GPIO_Rx GPIO('B', 7)
+  #define GPIO_Tx GPIO('B', 6)
+  #define USARTx USART1
+  #define USARTx_IRQn USART1_IRQn
+  #define USARTx_FUNCTION USART1_PB7_PB6_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART2_PA3_PA2
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA3,PA2");
   #define GPIO_Rx GPIO('A', 3)
   #define GPIO_Tx GPIO('A', 2)
   #define USARTx USART2
   #define USARTx_IRQn USART2_IRQn
-#elif CONFIG_T5UID1_SERIAL_PORT == 3
-  #if CONFIG_T5UID1_STM32_SERIAL_USART3_ALT
-    DECL_CONSTANT_STR("RESERVE_PINS_t5uid1", "PD9,PD8");
-    #define GPIO_Rx GPIO('D', 9)
-    #define GPIO_Tx GPIO('D', 8)
-  #else
-    DECL_CONSTANT_STR("RESERVE_PINS_t5uid1", "PB11,PB10");
-    #define GPIO_Rx GPIO('B', 11)
-    #define GPIO_Tx GPIO('B', 10)
-  #endif
+  #define USARTx_FUNCTION USART2_PA3_PA2_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART2_PA15_PA14
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA15,PA14");
+  #define GPIO_Rx GPIO('A', 15)
+  #define GPIO_Tx GPIO('A', 14)
+  #define USARTx USART2
+  #define USARTx_IRQn USART2_IRQn
+  #define USARTx_FUNCTION USART2_PA15_PA14_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART2_PD6_PD5
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD6,PD5");
+  #define GPIO_Rx GPIO('D', 6)
+  #define GPIO_Tx GPIO('D', 5)
+  #define USARTx USART2
+  #define USARTx_IRQn USART2_IRQn
+  #define USARTx_FUNCTION USART2_PD6_PD5_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART3_PB11_PB10
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PB11,PB10");
+  #define GPIO_Rx GPIO('B', 11)
+  #define GPIO_Tx GPIO('B', 10)
   #define USARTx USART3
   #define USARTx_IRQn USART3_IRQn
+  #define USARTx_FUNCTION USART3_PB11_PB10_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_USART3_PD9_PD8
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PD9,PD8");
+  #define GPIO_Rx GPIO('D', 9)
+  #define GPIO_Tx GPIO('D', 8)
+  #define USARTx USART3
+  #define USARTx_IRQn USART3_IRQn
+  #define USARTx_FUNCTION USART3_PD9_PD8_FUNCTION
+#elif CONFIG_STM32_T5UID1_SERIAL_UART4_PA1_PA0
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA1,PA0");
+  #define GPIO_Rx GPIO('A', 1)
+  #define GPIO_Tx GPIO('A', 0)
+  #define USARTx UART4
+  #define USARTx_IRQn UART4_IRQn
+  #define USARTx_FUNCTION UART4_PA1_PA0_FUNCTION
 #endif
-
-#define CR1_FLAGS (USART_CR1_UE | USART_CR1_RE | USART_CR1_TE   \
-                   | USART_CR1_RXNEIE)
 
 void
 t5uid1_USARTx_IRQHandler(void)
 {
-    uint32_t sr = USARTx->SR;
-    if (sr & (USART_SR_RXNE | USART_SR_ORE))
-        t5uid1_rx_byte(USARTx->DR);
-    if (sr & USART_SR_TXE && USARTx->CR1 & USART_CR1_TXEIE) {
+    uint32_t isr = USART_ISR(USARTx);
+    if (isr & ISR_RX)
+        t5uid1_rx_byte(USART_RDR(USARTx));
+    if (isr & ISR_TXEN && USART_CR1(USARTx) & CR1_TXEN) {
         uint8_t data;
         int ret = t5uid1_get_tx_byte(&data);
         if (ret)
-            USARTx->CR1 = CR1_FLAGS;
+            USART_CR1(USARTx) = CR1_BASE;
         else
-            USARTx->DR = data;
+            USART_TDR(USARTx) = data;
     }
 }
 
 void
 t5uid1_enable_tx_irq(void)
 {
-    USARTx->CR1 = CR1_FLAGS | USART_CR1_TXEIE;
+    USART_CR1(USARTx) = CR1_BASE | CR1_TXEN;
 }
 
 void
@@ -73,11 +107,10 @@ t5uid1_init(uint32_t baud)
 
     uint32_t pclk = get_pclock_frequency((uint32_t)USARTx);
     uint32_t div = DIV_ROUND_CLOSEST(pclk, baud);
-    USARTx->BRR = (((div / 16) << USART_BRR_DIV_Mantissa_Pos)
-                   | ((div % 16) << USART_BRR_DIV_Fraction_Pos));
-    USARTx->CR1 = CR1_FLAGS;
+    USART_BRR(USARTx) = BRR_VAL(div);
+    USART_CR1(USARTx) = CR1_BASE;
     armcm_enable_irq(t5uid1_USARTx_IRQHandler, USARTx_IRQn, 0);
 
-    gpio_peripheral(GPIO_Rx, GPIO_FUNCTION(7), 1);
-    gpio_peripheral(GPIO_Tx, GPIO_FUNCTION(7), 0);
+    gpio_peripheral(GPIO_Rx, USARTx_FUNCTION, 1);
+    gpio_peripheral(GPIO_Tx, USARTx_FUNCTION, 0);
 }
---
+++ src/stm32/t5uid1/stm32h7_serial.h
@@ -0,0 +1,31 @@
+#ifndef __STM32_T5UID1_STM32H7_SERIAL_H
+#define __STM32_T5UID1_STM32H7_SERIAL_H
+
+#include "../internal.h"
+
+#define USART_ISR(usart) (usart)->ISR
+#define USART_RDR(usart) (usart)->RDR
+#define USART_TDR(usart) (usart)->TDR
+#define USART_CR1(usart) (usart)->CR1
+#define USART_BRR(usart) (usart)->BRR
+
+#define ISR_RX   (USART_ISR_RXNE_RXFNE | USART_ISR_ORE)
+#define ISR_TXEN USART_ISR_TXE_TXFNF
+
+#define CR1_BASE (USART_CR1_UE | USART_CR1_RE | USART_CR1_TE \
+                  | USART_CR1_RXNEIE)
+#define CR1_TXEN USART_CR1_TXEIE
+
+#define BRR_VAL(div) ((((div) / 16) << USART_BRR_DIV_MANTISSA_Pos) \
+                      | (((div) % 16) << USART_BRR_DIV_FRACTION_Pos))
+
+#define USART1_PA10_PA9_FUNCTION  GPIO_FUNCTION(7)
+#define USART1_PB7_PB6_FUNCTION   GPIO_FUNCTION(7)
+#define USART2_PA3_PA2_FUNCTION   GPIO_FUNCTION(7)
+#define USART2_PA15_PA14_FUNCTION GPIO_FUNCTION(7)
+#define USART2_PD6_PD5_FUNCTION   GPIO_FUNCTION(7)
+#define USART3_PB11_PB10_FUNCTION GPIO_FUNCTION(7)
+#define USART3_PD9_PD8_FUNCTION   GPIO_FUNCTION(7)
+#define UART4_PA1_PA0_FUNCTION    GPIO_FUNCTION(8)
+
+#endif // stm32h7_serial.h
