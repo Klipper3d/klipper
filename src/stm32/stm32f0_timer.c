44a65f9e9649ea898fe88c20c5c7e8966b1c5212
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2022-10-25 11:06:44 +0200

    stm32f0/g0: use systick timer for timer interrupts
    Instead of relying on a TIMx channel for generating a compare match
    interrupt, use the SYSTICK timer countdown just like on the other
    stm32 families. The TIMx is still the timebase source, but only for the
    tick counter. As a result, the TIMx compare channels can be used for
    other purposes such as very accurately timed events in hardware.
    
    Signed-off-by: Alex Voinea <voinea.dragos.alexandru@gmail.com>

--- src/stm32/stm32f0_timer.c
+++ src/stm32/stm32f0_timer.c
@@ -1,19 +1,20 @@
-// STM32F0 timer support
+// Timer based on ARM Cortex-M3/M4 SysTick and a STM32 TIM
 //
-// Copyright (C) 2019  Kevin O'Connor <kevin@koconnor.net>
+// Copyright (C) 2017-2019  Kevin O'Connor <kevin@koconnor.net>
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include "board/armcm_boot.h" // armcm_enable_irq
-#include "board/armcm_timer.h" // udelay
-#include "board/internal.h" // TIM3
+#include "autoconf.h" // CONFIG_CLOCK_FREQ
+#include "board/armcm_boot.h" // DECL_ARMCM_IRQ
+#include "board/internal.h" // SysTick
 #include "board/io.h" // readl
 #include "board/irq.h" // irq_disable
-#include "board/misc.h" // timer_read_time
-#include "sched.h" // DECL_INIT
-#include "command.h" // DECL_SHUTDOWN
-#include "board/timer_irq.h" // timer_dispatch_many
+#include "board/misc.h" // timer_from_us
+#include "command.h" // shutdown
+#include "sched.h" // sched_timer_dispatch
 
+
+DECL_CONSTANT("CLOCK_FREQ", CONFIG_CLOCK_FREQ);
 
 /****************************************************************
  * Low level timer code
@@ -41,20 +42,23 @@ timer_get(void)
     return TIMx->CNT;
 }
 
-static inline void
-timer_set(uint32_t next)
+// Set the next irq time
+static void
+timer_set_diff(uint32_t value)
 {
-    TIMx->CCR1 = next;
-    TIMx->SR = 0;
+    SysTick->LOAD = value;
+    SysTick->VAL = 0;
+    SysTick->LOAD = 0;
 }
 
 // Activate timer dispatch as soon as possible
 void
 timer_kick(void)
 {
-    timer_set(timer_get() + 50);
+    SysTick->LOAD = 0;
+    SysTick->VAL = 0;
+    SCB->ICSR = SCB_ICSR_PENDSTSET_Msk;
 }
-
 
 /****************************************************************
  * 16bit hardware timer to 32bit conversion
@@ -78,52 +82,143 @@ timer_read_time(void)
 
 // Update timer_high every 0x8000 clock ticks
 static uint_fast8_t
-timer_event(struct timer *t)
+timx_event(struct timer *t)
 {
     timer_high += 0x8000;
     t->waketime = timer_high + 0x8000;
     return SF_RESCHEDULE;
 }
-static struct timer wrap_timer = {
-    .func = timer_event,
+static struct timer timx_wrap_timer = {
+    .func = timx_event,
     .waketime = 0x8000,
 };
-
-void
-timer_reset(void)
-{
-    if (!HAVE_TIMER_32BIT)
-        sched_add_timer(&wrap_timer);
-}
-DECL_SHUTDOWN(timer_reset);
-
 
 /****************************************************************
  * Setup and irqs
  ****************************************************************/
 
-// Hardware timer IRQ handler - dispatch software timers
-void __aligned(16)
-TIMx_IRQHandler(void)
+// Return the number of clock ticks for a given number of microseconds
+uint32_t
+timer_from_us(uint32_t us)
 {
-    irq_disable();
-    uint32_t next = timer_dispatch_many();
-    timer_set(next);
-    irq_enable();
+    return us * (CONFIG_CLOCK_FREQ / 1000000);
 }
+
+// Return true if time1 is before time2.  Always use this function to
+// compare times as regular C comparisons can fail if the counter
+// rolls over.
+uint8_t
+timer_is_before(uint32_t time1, uint32_t time2)
+{
+    return (int32_t)(time1 - time2) < 0;
+}
+
+// Dummy timer to avoid scheduling a SysTick irq greater than 0xffffff
+static uint_fast8_t
+timer_wrap_event(struct timer *t)
+{
+    t->waketime += 0xffffff;
+    return SF_RESCHEDULE;
+}
+static struct timer wrap_timer = {
+    .func = timer_wrap_event,
+    .waketime = 0xffffff,
+};
+void
+timer_reset(void)
+{
+    if (!HAVE_TIMER_32BIT)
+        sched_add_timer(&timx_wrap_timer);
+    else if (timer_from_us(100000) > 0xffffff)
+        sched_add_timer(&wrap_timer);
+    else
+        // Timer in sched.c already ensures timers wont overflow
+        return;
+}
+DECL_SHUTDOWN(timer_reset);
 
 void
 timer_init(void)
 {
     irqstatus_t flag = irq_save();
+
     enable_pclock((uint32_t)TIMx);
     TIMx->CNT = 0;
-    TIMx->DIER = TIM_DIER_CC1IE;
-    TIMx->CCER = TIM_CCER_CC1E;
-    armcm_enable_irq(TIMx_IRQHandler, TIMx_IRQn, 2);
-    timer_kick();
-    timer_reset();
     TIMx->CR1 = TIM_CR1_CEN;
+
+    // Schedule a recurring timer on fast cpus
+    timer_reset();
+
+    // Enable SysTick
+    NVIC_SetPriority(SysTick_IRQn, 2);
+    SysTick->CTRL = (SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk
+                     | SysTick_CTRL_ENABLE_Msk);
+    timer_kick();
+
     irq_restore(flag);
 }
 DECL_INIT(timer_init);
+
+static uint32_t timer_repeat_until;
+#define TIMER_IDLE_REPEAT_TICKS timer_from_us(500)
+#define TIMER_REPEAT_TICKS timer_from_us(100)
+
+#define TIMER_MIN_TRY_TICKS timer_from_us(2)
+#define TIMER_DEFER_REPEAT_TICKS timer_from_us(5)
+
+// Invoke timers
+static uint32_t
+timer_dispatch_many(void)
+{
+    uint32_t tru = timer_repeat_until;
+    for (;;) {
+        // Run the next software timer
+        uint32_t next = sched_timer_dispatch();
+
+        uint32_t now = timer_read_time();
+        int32_t diff = next - now;
+        if (diff > (int32_t)TIMER_MIN_TRY_TICKS)
+            // Schedule next timer normally.
+            return diff;
+
+        if (unlikely(timer_is_before(tru, now))) {
+            // Check if there are too many repeat timers
+            if (diff < (int32_t)(-timer_from_us(1000)))
+                try_shutdown("Rescheduled timer in the past");
+            if (sched_tasks_busy()) {
+                timer_repeat_until = now + TIMER_REPEAT_TICKS;
+                return TIMER_DEFER_REPEAT_TICKS;
+            }
+            timer_repeat_until = tru = now + TIMER_IDLE_REPEAT_TICKS;
+        }
+
+        // Next timer in the past or near future - wait for it to be ready
+        irq_enable();
+        while (unlikely(diff > 0))
+            diff = next - timer_read_time();
+        irq_disable();
+    }
+}
+
+// IRQ handler
+void __visible __aligned(16) // aligning helps stabilize perf benchmarks
+SysTick_Handler(void)
+{
+    irq_disable();
+    uint32_t diff = timer_dispatch_many();
+    timer_set_diff(diff);
+    irq_enable();
+}
+DECL_ARMCM_IRQ(SysTick_Handler, SysTick_IRQn);
+
+// Make sure timer_repeat_until doesn't wrap 32bit comparisons
+void
+timer_task(void)
+{
+    uint32_t now = timer_read_time();
+    irq_disable();
+    if (timer_is_before(timer_repeat_until, now))
+        timer_repeat_until = now;
+    irq_enable();
+}
+DECL_TASK(timer_task);
