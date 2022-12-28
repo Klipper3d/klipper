4a44046c512edb56959223b057777ddf70670909
Daniel <dans98@users.noreply.github.com>
2022-12-28 15:40:24 -0600

    heaters: fixed the m104/109 bug related to velocity PID
    Signed-off-by: Daniel Sherman <blogmail79@dans-hobbies.com>

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -278,7 +278,7 @@ class ControlVelocityPID:
     def check_busy(self, eventtime, smoothed_temp, target_temp):
         temp_diff = target_temp - smoothed_temp
         return (abs(temp_diff) > PID_SETTLE_DELTA
-                or abs(self.prev_der) > PID_SETTLE_SLOPE)
+                or abs(self.d1) > PID_SETTLE_SLOPE)
 
 
 ######################################################################
