54d6c7228476fc86fa35d73d6d6ccf451fc425a3
Ben Eastep <shifting@shifting.ca>
2023-01-01 04:02:14 +0000

    cleanup

--- klippy/extras/bed_mesh.py
+++ klippy/extras/bed_mesh.py
@@ -295,7 +295,7 @@ class BedMeshCalibrate:
         self.mesh_config = collections.OrderedDict()
         self._init_mesh_config(config)
         self._generate_points(config.error)
-        if self.relative_reference_index >= len(self.points):    
+        if self.relative_reference_index >= len(self.points):
             raise config.error("Invalid relative_reference_index")
         self._profile_name = None
         self.orig_points = self.points
