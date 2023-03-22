93111716a01508694dc2cc5620142f2850340059
Mark <niujl123@sina.com>
2023-03-22 22:54:39 +0800

    Update probe.py

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -392,7 +392,6 @@ class ProbePointsHelper:
                                                 parser=float, count=2)
         def_move_z = config.getfloat('horizontal_move_z', 5.)
         self.default_horizontal_move_z = def_move_z
-        self.horizontal_move_z=def_move_z
         self.speed = config.getfloat('speed', 50., above=0.)
         self.use_offsets = False
         # Internal probing state
