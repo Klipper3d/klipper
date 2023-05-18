4b2643e38925abe45c0c8150ccd8554dc6a94325
LastZeanon <127127685+LastZeanon@users.noreply.github.com>
2023-05-18 11:46:46 +0200

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -167,7 +167,7 @@ class ControlBangBang:
             self.heater.set_pwm(read_time, 0.)
     def check_busy(self, eventtime, smoothed_temp, target_temp):
         return smoothed_temp < target_temp-self.max_delta
-    def get_name(self):
+    def get_type(self):
         return 'watermark'
 
 
@@ -231,7 +231,7 @@ class ControlPID:
         temp_diff = target_temp - smoothed_temp
         return (abs(temp_diff) > PID_SETTLE_DELTA
                 or abs(self.prev_der) > PID_SETTLE_SLOPE)
-    def get_name(self):
+    def get_type(self):
         return 'pid'
 
 
@@ -283,7 +283,7 @@ class ControlVelocityPID:
         temp_diff = target_temp - smoothed_temp
         return (abs(temp_diff) > PID_SETTLE_DELTA
                 or abs(self.d1) > PID_SETTLE_SLOPE)
-    def get_name(self):
+    def get_type(self):
         return 'pid_v'
 
 
