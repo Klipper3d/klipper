bfb8cd3fb273c12e96759f9cccf27cfefa9f1f2b
Michael Jäger <emjay276@gmail.com>
2023-05-28 23:26:02 +0200

    Update real sensor temperatur_callback

--- klippy/extras/temperature_combined.py
+++ klippy/extras/temperature_combined.py
@@ -10,21 +10,31 @@ class PrinterSensorCombined:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.name = config.get_name().split()[-1]
+        # get sensor names
         self.sensor_names = config.getlist("sensors", None)
+        # get type of algorithm to handle the different sensor values with
         algos = {'min': min, 'max': max, 'mean': mean}
         self.mode = config.getchoice('type', algos)
+        # get heater
         pheaters = self.printer.load_object(config, 'heaters')
-        self.sensor = pheaters.setup_sensor(config)
+        # set min / max temp
         self.min_temp = config.getfloat('min_temp', KELVIN_TO_CELSIUS,
                                         minval=KELVIN_TO_CELSIUS)
         self.max_temp = config.getfloat('max_temp', 99999999.9,
                                         above=self.min_temp)
-        self.sensor.setup_minmax(self.min_temp, self.max_temp)
-        self.sensor.setup_callback(self.temperature_callback)
+        # register sensor
         pheaters.register_sensor(config, self)
+        # set default values
         self.last_temp = 0.
         self.measured_min = 99999999.
         self.measured_max = 0.
+        # update original sensor callbacks
+        for sensor_name in self.sensor_names:
+            sensor = self.printer.lookup_object(sensor_name)
+            def new_temperature_callback(read_time, temp):
+                sensor.temperature_callback(read_time, temp)
+                self.temperature_callback(read_time, temp)
+            sensor.setup_callback(new_temperature_callback)
     def temperature_callback(self, read_time, temp):
         values = []
         for sensor_name in self.sensor_names:
