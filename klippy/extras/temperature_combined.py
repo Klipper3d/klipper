f3f90d3c59218a9f01e0aaaabf8bd09b53f5c3a8
Michael Jäger <emjay276@gmail.com>
2023-05-29 01:13:36 +0200

    Add update handler

--- klippy/extras/temperature_combined.py
+++ klippy/extras/temperature_combined.py
@@ -5,10 +5,12 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
 KELVIN_TO_CELSIUS = -273.15
+UPDATE_TEMPERATURE_INTERVALL = 1
 
 class PrinterSensorCombined:
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
         self.name = config.get_name().split()[-1]
         # get sensor names
         self.sensor_names = config.getlist("sensors", None)
@@ -16,32 +18,23 @@ class PrinterSensorCombined:
         algos = {'min': min, 'max': max, 'mean': mean}
         self.mode = config.getchoice('type', algos)
         # get heater
-        self.pheaters = self.printer.lookup_object('heaters')
+        pheaters = self.printer.lookup_object('heaters')
         # set min / max temp
         self.min_temp = config.getfloat('min_temp', KELVIN_TO_CELSIUS,
                                         minval=KELVIN_TO_CELSIUS)
         self.max_temp = config.getfloat('max_temp', 99999999.9,
                                         above=self.min_temp)
         # register sensor
-        self.pheaters.register_sensor(config, self)
+        pheaters.register_sensor(config, self)
         # set default values
         self.last_temp = 0.
         self.measured_min = 99999999.
         self.measured_max = 0.
-        # update original sensor callbacks
-        for sensor_name in self.sensor_names:
-            if sensor_name not in self.pheaters.available_sensors:
-                return
-            sensor = self.pheaters.heaters[sensor_name]
-            def new_temperature_callback(read_time, temp):
-                sensor.temperature_callback(read_time, temp)
-                self.temperature_callback(read_time, temp)
-            sensor.setup_callback(new_temperature_callback)
-    def temperature_callback(self, read_time, temp):
+    def update_temp(self, eventtime):
         values = []
         for sensor_name in self.sensor_names:
             sensor = self.pheaters.heaters[sensor_name]
-            temp_sensor, _ = sensor.get_temp(read_time)
+            temp_sensor, _ = sensor.get_temp(eventtime)
             values.append(temp_sensor)
         temp = self.mode(values)
         if temp:
@@ -58,6 +51,14 @@ class PrinterSensorCombined:
             'measured_min_temp': round(self.measured_min, 2),
             'measured_max_temp': round(self.measured_max, 2)
         }
+    def _handle_ready(self):
+        self.pheaters = self.printer.lookup_object('heaters')
+        # Start temperature update timer
+        self.reactor.update_timer(self._temperature_update_event)
+    def _temperature_update_event(self, eventtime):
+        # update sensor value
+        self.update_temp(eventtime)
+        return eventtime + UPDATE_TEMPERATURE_INTERVALL
 
 def load_config_prefix(config):
     return PrinterSensorCombined(config)
