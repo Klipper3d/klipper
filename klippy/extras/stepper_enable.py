5bd29513b224671fb595481d0fe7fdf881885093
Evgenii Shavrin <shavrin0591@gmail.com>
2023-06-12 13:37:33 +0300

    Fix whitespace...
    Signed-off-by: Evgenii Shavrin <shavrin0591@gmail.com>

--- klippy/extras/stepper_enable.py
+++ klippy/extras/stepper_enable.py
@@ -106,7 +106,7 @@ class PrinterStepperEnable:
                 el.motor_enable(print_time)
             else:
                 el.motor_disable(print_time)
-        logging.info("%s have been manually %s", steppers, 
+        logging.info("%s have been manually %s", steppers,
                      "enabled" if enable else "disabled")
         toolhead.dwell(DISABLE_STALL_TIME)
     def get_status(self, eventtime):
