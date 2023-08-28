aaf02f3c8af81d2e757f4128ae5a009cea1d6a55
Dennis Marttinen <twelho@welho.tech>
2023-08-28 15:58:32 +0200

    kinematics: implement `clear_homing_state`
    Signed-off-by: Dennis Marttinen <twelho@welho.tech>

--- klippy/kinematics/hybrid_corexz.py
+++ klippy/kinematics/hybrid_corexz.py
@@ -74,9 +74,10 @@ class HybridCoreXZKinematics:
             rail.set_position(newpos)
             if i in homing_axes:
                 self.limits[i] = rail.get_range()
-    def note_z_not_homed(self):
-        # Helper for Safe Z Home
-        self.limits[2] = (1.0, -1.0)
+    def clear_homing_state(self, axes):
+        for i, _ in enumerate(self.limits):
+            if i in axes:
+                self.limits[i] = (1.0, -1.0)
     def home_axis(self, homing_state, axis, rail):
         position_min, position_max = rail.get_range()
         hi = rail.get_homing_info()
@@ -96,7 +97,7 @@ class HybridCoreXZKinematics:
             else:
                 self.home_axis(homing_state, axis, self.rails[axis])
     def _motor_off(self, print_time):
-        self.limits = [(1.0, -1.0)] * 3
+        self.clear_homing_state((0, 1, 2))
     def _check_endstops(self, move):
         end_pos = move.end_pos
         for i in (0, 1, 2):
