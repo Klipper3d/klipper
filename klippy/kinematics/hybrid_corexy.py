d03d0a3731c4275d25349022d38398ab143f293f
HelgeKeck <helgekeck@hotmail.com>
2023-09-27 19:43:55 +0200

    Update hybrid_corexy.py

--- klippy/kinematics/hybrid_corexy.py
+++ klippy/kinematics/hybrid_corexy.py
@@ -69,18 +69,16 @@ class HybridCoreXYKinematics:
         if (self.dc_module is not None and 'PRIMARY' == \
                     self.dc_module.get_status()['carriage_1']):
             if self.inverted == False:
-                return [stepper_positions['dual_carriage'] \
-                        - pos[1], pos[1], pos[2]]
+                return stepper_positions['dual_carriage'] \
+                        - pos[1], pos[1], pos[2]
             else:
-                return [stepper_positions['dual_carriage']  \
-                        + pos[1], pos[1], pos[2]]
+                return stepper_positions['dual_carriage']  \
+                        + pos[1], pos[1], pos[2]
         else:
             if self.inverted == False:
-                return [stepper_positions['stepper_x'] \
-                        + pos[1], pos[1], pos[2]]
+                return pos[0] + pos[1], pos[1], pos[2]
             else:
-                return [stepper_positions['stepper_x'] \
-                        - pos[1], pos[1], pos[2]]
+                return pos[0] - pos[1], pos[1], pos[2]
     def update_limits(self, i, range):
         l, h = self.limits[i]
         # Only update limits if this axis was already homed,
