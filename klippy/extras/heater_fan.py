8b60f7586e8d690ed9a7ca10c1536c5ff550293d
David Sun <cneshi@gmail.com>
2023-10-09 23:02:04 +0800

    heater_fan: Add Heater Fan Self Check Logic Adds min_rpm config to [heater_fan] module, which allows the fan to monitor its own RPM speed and shutdown Klipper if speed is abnormal.
    Signed-off-by: David Sun <cneshi@gmail.com>

--- klippy/extras/heater_fan.py
+++ klippy/extras/heater_fan.py
@@ -6,6 +6,7 @@
 from . import fan
 
 PIN_MIN_TIME = 0.100
+SAFETY_CHECK_INIT_TIME = 3.
 
 class PrinterHeaterFan:
     def __init__(self, config):
@@ -18,11 +19,21 @@ class PrinterHeaterFan:
         self.fan = fan.Fan(config, default_shutdown_speed=1.)
         self.fan_speed = config.getfloat("fan_speed", 1., minval=0., maxval=1.)
         self.last_speed = 0.
+        self.name = config.get_name().split()[-1]
+        self.num_err = 0
+        self.max_err = 3
+        self.min_rpm = config.getint("min_rpm", 0, minval=0)
+        if self.min_rpm > 0 and self.fan.tachometer._freq_counter is None:
+            raise config.error(
+                "'tachometer_pin' must be specified before enabling `min_rpm`")
     def handle_ready(self):
         pheaters = self.printer.lookup_object('heaters')
         self.heaters = [pheaters.lookup_heater(n) for n in self.heater_names]
         reactor = self.printer.get_reactor()
         reactor.register_timer(self.callback, reactor.monotonic()+PIN_MIN_TIME)
+        if self.min_rpm > 0:
+            reactor.register_timer(
+                self.fan_check, reactor.monotonic()+SAFETY_CHECK_INIT_TIME)
     def get_status(self, eventtime):
         return self.fan.get_status(eventtime)
     def callback(self, eventtime):
@@ -37,6 +48,25 @@ class PrinterHeaterFan:
             print_time = self.fan.get_mcu().estimated_print_time(curtime)
             self.fan.set_speed(print_time + PIN_MIN_TIME, speed)
         return eventtime + 1.
+    def fan_check(self, eventtime):
+        rpm = self.fan.tachometer.get_status(eventtime)['rpm']
+        has_err = False
+        if rpm is not None:
+            for heater in self.heaters:
+                current_temp, target_temp = heater.get_temp(eventtime)
+                if current_temp > self.heater_temp and rpm < self.min_rpm:
+                    has_err = True
+        if has_err:
+            self.num_err += 1
+            if self.num_err > self.max_err:
+                msg = "'%s' spinning below minimum safe speed of %d rev/min"
+                    % (self.name, self.min_rpm)
+                logging.error(msg)
+                self.printer.invoke_shutdown(msg)
+                return self.printer.get_reactor().NEVER
+        else:
+            self.num_err = 0
+        return eventtime + 1.5
 
 def load_config_prefix(config):
     return PrinterHeaterFan(config)
