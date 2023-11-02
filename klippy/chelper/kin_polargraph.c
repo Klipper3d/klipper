e98990284caf43b2d5d4133c2998f54c6f333d2c
Tsogtgerel <ts.r.madrid@gmail.com>
2023-11-02 22:56:28 +0800

    kinematics: add polargraph kinematics for drawbot Signed-off-by: Tsogtgerel Amar <ts.r.madrid@gmail.com>

---
+++ klippy/chelper/kin_polargraph.c
@@ -0,0 +1,55 @@
+// Polargraph kinematics stepper pulse time generation
+//
+// Copyright (C) 2022  Tsogtgerel Amar <ts.r.madrid@gmail.com>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include <math.h> // sqrt
+#include <stddef.h> // offsetof
+#include <stdlib.h> // malloc
+#include <string.h> // memset
+#include "compiler.h" // __visible
+#include "itersolve.h" // struct stepper_kinematics
+#include "pyhelper.h" // errorf
+#include "trapq.h" // move_get_coord
+
+struct polargraph_stepper {
+    struct stepper_kinematics sk;
+    double width;
+};
+static double
+polargraph_stepper_left_calc_position(struct stepper_kinematics *sk
+                             , struct move *m
+                             , double move_time)
+{
+    struct polargraph_stepper *ps = container_of(sk, struct polargraph_stepper
+                             , sk);
+    struct coord c = move_get_coord(m, move_time);
+    return sqrt((ps->width - c.x) * (ps->width - c.x) + c.y*c.y);
+}
+
+static double
+polargraph_stepper_right_calc_position(struct stepper_kinematics *sk
+                             , struct move *m
+                             , double move_time)
+{
+    struct polargraph_stepper *ps = container_of(sk, struct polargraph_stepper
+                             , sk);
+    struct coord c = move_get_coord(m, move_time);
+    return sqrt(c.y * c.y +  c.x * c.x);
+}
+
+struct stepper_kinematics * __visible
+polargraph_stepper_alloc(double width, char side)
+{
+    struct polargraph_stepper *ps = malloc(sizeof(*ps));
+    memset(ps, 0, sizeof(*ps));
+    if (side == 'l') {
+        ps->sk.calc_position_cb = polargraph_stepper_left_calc_position;
+    } else if (side == 'r') {
+        ps->sk.calc_position_cb = polargraph_stepper_right_calc_position;
+    }
+    ps->sk.active_flags = AF_X | AF_Y;
+    ps->width = width;
+    return &ps->sk;
+}
---
+++ klippy/kinematics/polargraph.py
@@ -0,0 +1,137 @@
+# Code for handling the kinematics of corexy robots
+#
+# Copyright (C) 2022  Tsogtgerel Amar <ts.r.madrid@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import stepper
+
+
+class PolargraphKinematics:
+    def __init__(self, toolhead, config):
+        self.rails = [
+            stepper.LookupMultiRail(config.getsection("stepper_" + n)) for n in "lrz"
+        ]
+        for s in self.rails[1].get_steppers():
+            self.rails[0].get_endstops()[0][0].add_stepper(s)
+        for s in self.rails[0].get_steppers():
+            self.rails[1].get_endstops()[0][0].add_stepper(s)
+        polargraph_config = config.getsection("drawbot")
+        self.width = polargraph_config.getfloat("width", above=0.0)
+        self.rails[0].setup_itersolve("polargraph_stepper_alloc", self.width, b"l")
+        self.rails[1].setup_itersolve("polargraph_stepper_alloc", self.width, b"r")
+        self.rails[2].setup_itersolve("cartesian_stepper_alloc", b"z")
+        for s in self.get_steppers():
+            s.set_trapq(toolhead.get_trapq())
+            toolhead.register_step_generator(s.generate_steps)
+        config.get_printer().register_event_handler(
+            "stepper_enable:motor_off", self._motor_off
+        )
+        # Setup boundary checks
+        max_velocity, max_accel = toolhead.get_max_velocity()
+        self.max_z_velocity = config.getfloat(
+            "max_z_velocity", max_velocity, above=0.0, maxval=max_velocity
+        )
+        self.max_z_accel = config.getfloat(
+            "max_z_accel", max_accel, above=0.0, maxval=max_accel
+        )
+        self.limits = [(1.0, -1.0)] * 3
+        ranges = [r.get_range() for r in self.rails]
+        self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.0)
+        self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.0)
+
+    def get_steppers(self):
+        return [s for rail in self.rails for s in rail.get_steppers()]
+
+    def calc_position(self, stepper_positions):
+        x = self.width - (
+            stepper_positions["l"] ** 2 - stepper_positions["r"] ** 2 + self.width**2
+        ) / (2 * self.width)
+        return [
+            x,
+            (stepper_positions["r"] ** 2 - x**2) ** 0.5,
+            stepper_positions["z"],
+        ]
+
+    def set_position(self, newpos, homing_axes):
+        for i, rail in enumerate(self.rails):
+            rail.set_position(newpos)
+            if i in homing_axes:
+                self.limits[i] = rail.get_range()
+
+    def note_z_not_homed(self):
+        # Helper for Safe Z Home
+        self.limits[2] = (1.0, -1.0)
+
+    def _home_axis(self, homing_state, axis, rail):
+        # Determine movement
+        position_min, position_max = rail.get_range()
+        hi = rail.get_homing_info()
+        homepos = [None, None, None, None]
+        homepos[axis] = hi.position_endstop
+        forcepos = list(homepos)
+        if hi.positive_dir:
+            forcepos[axis] -= 1.5 * (hi.position_endstop - position_min)
+        else:
+            forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
+        # Perform homing
+        homing_state.home_rails([rail], forcepos, homepos)
+
+    def home(self, homing_state):
+        # Each axis is homed independently and in order
+        for axis in homing_state.get_axes():
+            rail = self.rails[axis]
+            # Determine movement
+            position_min, position_max = rail.get_range()
+            hi = rail.get_homing_info()
+            homepos = [None, None, None, None]
+            homepos[axis] = hi.position_endstop
+            forcepos = list(homepos)
+            if hi.positive_dir:
+                forcepos[axis] -= 1.5 * (hi.position_endstop - position_min)
+            else:
+                forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
+            # Perform homing
+            homing_state.home_rails([rail], forcepos, homepos)
+
+    def _motor_off(self, print_time):
+        self.limits = [(1.0, -1.0)] * 3
+
+    def _check_endstops(self, move):
+        end_pos = move.end_pos
+        for i in (0, 1, 2):
+            if move.axes_d[i] and (
+                end_pos[i] < self.limits[i][0] or end_pos[i] > self.limits[i][1]
+            ):
+                if self.limits[i][0] > self.limits[i][1]:
+                    raise move.move_error("Must home axis first")
+                raise move.move_error()
+
+    def check_move(self, move):
+        limits = self.limits
+        xpos, ypos = move.end_pos[:2]
+        if (
+            xpos < limits[0][0]
+            or xpos > limits[0][1]
+            or ypos < limits[1][0]
+            or ypos > limits[1][1]
+        ):
+            self._check_endstops(move)
+        if not move.axes_d[2]:
+            # Normal XY move - use defaults
+            return
+        # Move with Z - update velocity and accel for slower Z axis
+        self._check_endstops(move)
+        z_ratio = move.move_d / abs(move.axes_d[2])
+        move.limit_speed(self.max_z_velocity * z_ratio, self.max_z_accel * z_ratio)
+
+    def get_status(self, eventtime):
+        axes = [a for a, (l, h) in zip("xyz", self.limits) if l <= h]
+        return {
+            "homed_axes": "".join(axes),
+            "axis_minimum": self.axes_min,
+            "axis_maximum": self.axes_max,
+        }
+
+
+def load_kinematics(toolhead, config):
+    return PolargraphKinematics(toolhead, config)
