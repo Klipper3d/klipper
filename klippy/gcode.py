3950fe8ff236d15916fce0e8387e0f56d2b77bfc
Pedro Lamas <pedrolamas@gmail.com>
2023-11-06 15:06:36 +0000

    gcode: expose status with available commands
    Signed-off-by: Pedro Lamas <pedrolamas@gmail.com>

--- klippy/gcode.py
+++ klippy/gcode.py
@@ -155,6 +155,7 @@ class GCodeDispatch:
         self.ready_gcode_handlers = {}
         self.mux_commands = {}
         self.gcode_help = {}
+        self.status_commands = {}
         # Register commands needed before config file is loaded
         handlers = [
             "M110",
@@ -195,6 +196,7 @@ class GCodeDispatch:
                 del self.ready_gcode_handlers[cmd]
             if cmd in self.base_gcode_handlers:
                 del self.base_gcode_handlers[cmd]
+            self._build_status_commands()
             return old_cmd
         if cmd in self.ready_gcode_handlers:
             raise self.printer.config_error(
@@ -208,6 +210,7 @@ class GCodeDispatch:
             self.base_gcode_handlers[cmd] = func
         if desc is not None:
             self.gcode_help[cmd] = desc
+        self._build_status_commands()
 
     def register_mux_command(self, cmd, key, value, func, desc=None):
         prev = self.mux_commands.get(cmd)
@@ -236,6 +239,16 @@ class GCodeDispatch:
     def get_command_help(self):
         return dict(self.gcode_help)
 
+    def get_status(self, eventtime):
+        return {'commands': self.status_commands}
+
+    def _build_status_commands(self):
+        commands = {cmd: {} for cmd in self.gcode_handlers}
+        for cmd in self.gcode_help:
+            if cmd in commands:
+                commands[cmd]['help'] = self.gcode_help[cmd]
+        self.status_commands = commands
+
     def register_output_handler(self, cb):
         self.output_callbacks.append(cb)
 
@@ -244,6 +257,7 @@ class GCodeDispatch:
             return
         self.is_printer_ready = False
         self.gcode_handlers = self.base_gcode_handlers
+        self._build_status_commands()
         self._respond_state("Shutdown")
 
     def _handle_disconnect(self):
@@ -252,6 +266,7 @@ class GCodeDispatch:
     def _handle_ready(self):
         self.is_printer_ready = True
         self.gcode_handlers = self.ready_gcode_handlers
+        self._build_status_commands()
         self._respond_state("Ready")
 
     # Parse input into commands
