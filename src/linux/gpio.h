e381b6c6322855005ca53e26957669d87a90eaed
Francois Chagnon <fc@francoischagnon.net>
2023-11-17 13:12:20 -0500

    Remove trailing whitespace

--- src/linux/gpio.h
+++ src/linux/gpio.h
@@ -49,7 +49,7 @@ struct i2c_config {
     uint16_t timeout;
 };
 
-struct i2c_config i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr, 
+struct i2c_config i2c_setup(uint32_t bus, uint32_t rate, uint8_t addr,
                             uint16_t timeout);
 void i2c_write(struct i2c_config config, uint8_t write_len, uint8_t *write);
 void i2c_read(struct i2c_config config, uint8_t reg_len, uint8_t *reg
