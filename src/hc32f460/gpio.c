58f7dde1f5bd77a5d7c3b6f8100d7e32adfdacd1
ggiraudon <ggiraudon@BLADE.localdomain>
2024-02-01 20:09:39 -0500

    Added package selection for HC32F460 (100 pin package used on Tenlog 32bit board)

--- src/hc32f460/gpio.c
+++ src/hc32f460/gpio.c
@@ -18,7 +18,12 @@
 DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PC0", GPIO('C', 0), 16);
+#if CONFIG_HC32F460_PINS_100
+DECL_ENUMERATION_RANGE("pin", "PD0", GPIO('D', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PE0", GPIO('E', 0), 16);
+#else
 DECL_ENUMERATION_RANGE("pin", "PD2", GPIO('D', 2), 1);
+#endif
 DECL_ENUMERATION_RANGE("pin", "PH2", PortH * 16 + 2, 1);    // H: special case
 
 
