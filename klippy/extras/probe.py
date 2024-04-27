7b490f3ec1d3f0477b90c74d96f51ba1fb457a46
Kevin O'Connor <kevin@koconnor.net>
2024-04-27 11:10:01 -0400

    probe: Fix typo in activate/deactive error messages
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -330,14 +330,14 @@ class ProbeEndstopWrapper:
         self.deactivate_gcode.run_gcode_from_command()
         if toolhead.get_position()[:3] != start_pos[:3]:
             raise self.printer.command_error(
-                "Toolhead moved during probe activate_gcode script")
+                "Toolhead moved during probe deactivate_gcode script")
     def _lower_probe(self):
         toolhead = self.printer.lookup_object('toolhead')
         start_pos = toolhead.get_position()
         self.activate_gcode.run_gcode_from_command()
         if toolhead.get_position()[:3] != start_pos[:3]:
             raise self.printer.command_error(
-                "Toolhead moved during probe deactivate_gcode script")
+                "Toolhead moved during probe activate_gcode script")
     def multi_probe_begin(self):
         if self.stow_on_each_sample:
             return
