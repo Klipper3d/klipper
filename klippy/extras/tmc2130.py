f01c8853ca5c4f7d89ea1ae9a33779fe9c0446b1
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-05-13 22:31:39 +0200

    tmc: Do not pass the frequency directly to the helpers
    Use the new get_tmc_frequency() instead.
    
    Signed-off-by: Alex Voinea <voinea.dragos.alexandru@gmail.com>

--- klippy/extras/tmc2130.py
+++ klippy/extras/tmc2130.py
@@ -296,7 +296,7 @@ class TMC2130:
         self.get_status = cmdhelper.get_status
         # Setup basic register values
         tmc.TMCWaveTableHelper(config, self.mcu_tmc)
-        tmc.TMCStealthchopHelper(config, self.mcu_tmc, TMC_FREQUENCY)
+        tmc.TMCStealthchopHelper(config, self.mcu_tmc)
         # Allow other registers to be set from the config
         set_config_field = self.fields.set_config_field
         # CHOPCONF
