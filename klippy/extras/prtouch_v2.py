f4be1bb10b7107d43ace3ef22d329e9b53632ada
LazyCoderOZ <LazyCoderOZ@users.noreply.github.com>
2024-10-13 07:43:11 +1030

    Update prtouch_v2.py
    Remove additional \n at end of file causing build fail

--- klippy/extras/prtouch_v2.py
+++ klippy/extras/prtouch_v2.py
@@ -1581,11 +1581,9 @@ def load_config(config):
     vrt = PRTouchEndstopWrapper(config)
     # config.get_printer().add_object('probe', probe.PrinterProbe(config, vrt))
     return vrt
-
-
+    
 # /home/cc/moonraker-env/bin/python3.10 /home/cc/moonraker/moonraker/moonraker.py -d /home/cc/printer_data
 # sudo service klipper stop
 # /home/cc/klippy-env/bin/python3.10 /home/cc/klipper/klippy/klippy.py /home/cc/printer_data/config/printer.cfg -a /home/cc/printer_data/comms/klippy.sock -l /home/cc/printer_data/logs/klippy.log
 # ./micropython /home/cc/klipper/klippy/klippy.py /home/cc/printer_data/config/printer.cfg -a /home/cc/printer_data/comms/klippy.sock -l /home/cc/printer_data/logs/klippy.log
 # /home/cc/klippy-env/bin/python3.10 /home/cc/micropython_test/klipper/klippy/klippy.py /home/cc/printer_data/config/printer.cfg -a /home/cc/printer_data/comms/klippy.sock -l /home/cc/printer_data/logs/klippy.log
-
