efca1bfba9784ec559c757b4de10d7b20684ebcc
Aaron Haun <aaron@haun.guru>
2024-11-17 22:56:45 -0500

    linux: Add options for setting group and mode
    Signed-off-by: Aaron B. Haun <aaron@haun.guru>

--- src/linux/main.c
+++ src/linux/main.c
@@ -4,9 +4,13 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include <sched.h> // sched_setscheduler sched_get_priority_max
+#include <errno.h> // errno
+#include </usr/include/sched.h> // sched_setscheduler sched_get_priority_max
 #include <stdio.h> // fprintf
 #include <string.h> // memset
+#include <stdlib.h> // strtoul
+#include <sys/types.h> // getgrent struct
+#include <grp.h> // getgrnam
 #include <unistd.h> // getopt
 #include <sys/mman.h> // mlockall MCL_CURRENT MCL_FUTURE
 #include "board/misc.h" // console_sendf
@@ -70,7 +74,9 @@ main(int argc, char **argv)
     orig_argv = argv;
     int opt, watchdog = 0, realtime = 0;
     char *serial = "/tmp/klipper_host_mcu";
-    while ((opt = getopt(argc, argv, "wrI:")) != -1) {
+    char *pty_group_name = NULL;
+    mode_t pty_mode = 0660;
+    while ((opt = getopt(argc, argv, "wrI:g:m:")) != -1) {
         switch (opt) {
         case 'w':
             watchdog = 1;
@@ -81,8 +87,28 @@ main(int argc, char **argv)
         case 'I':
             serial = optarg;
             break;
+        case 'g':
+            pty_group_name = optarg;
+            break;
+        case 'm':
+            errno = 0; // see man 2 strtoul
+            unsigned long int input_pty_mode = strtoul(optarg, NULL, 8);
+            if (errno) {
+                fprintf(stderr, "unable to parse pty mode: %s", optarg);
+                return -1;
+            }
+            if (input_pty_mode >  0777ul) {
+                fprintf(stderr,
+                        "mode %s invalid (setuid/gid/sticky not supported)",
+                        optarg);
+                return -1;
+            }
+            pty_mode = input_pty_mode;
+            break;
         default:
-            fprintf(stderr, "Usage: %s [-w] [-r] [-I path]\n", argv[0]);
+            fprintf(stderr,
+                    "Usage: %s [-w] [-r] [-I path] [-g group] [-m filemode]\n",
+                    argv[0]);
             return -1;
         }
     }
@@ -93,7 +119,17 @@ main(int argc, char **argv)
         if (ret)
             return ret;
     }
-    int ret = console_setup(serial);
+    gid_t pty_group_id = -1;
+    if (pty_group_name) {
+        struct group * pty_group = getgrnam(pty_group_name);
+        if (!pty_group) {
+            fprintf(stderr, "Unable to find group \"%s\"\n", pty_group_name);
+            return -1;
+        }
+        pty_group_id = pty_group->gr_gid;
+    }
+
+    int ret = console_setup(serial, pty_mode, pty_group_id);
     if (ret)
         return -1;
     if (watchdog) {
