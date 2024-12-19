5017d909a2373e6227788f639e9c11044dfa4766
Thomas-Toucan3d <thomas.seidl@toucan3d.eu>
2024-12-19 15:03:36 +0100

    -

--- klippy/extras/concentricity_tolerance_compensation.py
+++ klippy/extras/concentricity_tolerance_compensation.py
@@ -41,7 +41,7 @@ class ConcentricityToleranceCompansation:
         
     def move_transformer(self):
         # Register transform
-        gcode_move = self.printer.load_object('gcode_move')
+        gcode_move = self.printer.lookup_object('gcode_move')
         gcode_move.add_move_transformer(self)
         
     def calc_xy_adj(self, a_pos):
