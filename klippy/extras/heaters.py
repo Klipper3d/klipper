39ee605ad100495c86a2f9d5056df4883b8a0ab9
Timofey Titovets <nefelim4ag@gmail.com>
2025-02-03 04:18:39 +0100

    pwm: limit max duration for support fast MCUs
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -11,10 +11,10 @@ import os, logging, threading
 ######################################################################
 
 KELVIN_TO_CELSIUS = -273.15
-MAX_HEAT_TIME = 5.0
+MAX_HEAT_TIME = 3.5
 AMBIENT_TEMP = 25.
 PID_PARAM_BASE = 255.
-MAX_MAINTHREAD_TIME = 5.0
+MAX_MAINTHREAD_TIME = 3.5
 
 class Heater:
     def __init__(self, config, sensor):
