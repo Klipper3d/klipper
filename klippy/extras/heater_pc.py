0b12759c3373ba32adeeec55f4550e66467fd45d
Timofey Titovets <nefelim4ag@gmail.com>
2025-02-27 23:17:18 +0100

    heater_pc: use display_template
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- klippy/extras/heater_pc.py
+++ klippy/extras/heater_pc.py
@@ -4,67 +4,35 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
-import logging
-from .gcode_macro import TemplateWrapper, PrinterGCodeMacro
+from .gcode_macro import PrinterGCodeMacro
+from .display import display
 
 class HeaterPredictControl:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.config = config
-        self.name = config.get_name().split()[-1]
-        script = config.get("func")
-        self.env = PrinterGCodeMacro(config).env
-        self.env.globals.update({
-            'log': self._log,
-            'set_var': self._set_var,
-            'get_var': self._get_var,
-        })
-        self.variables = {}
-        self.load_variables()
-        self.template = TemplateWrapper(self.printer, self.env,
-                                        config.get_name(), script)
+        name_parts = config.get_name().split()
+        if len(name_parts) != 2:
+            raise config.error("Section name '%s' is not valid"
+                               % (config.get_name(),))
+        template_name = config.get("macro_template")
+        templates = display.lookup_display_templates(config)
+        display_templates = templates.get_display_templates()
+        self.create_context = PrinterGCodeMacro(config).create_template_context
+        self.template = display_templates.get(template_name)
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
     def handle_connect(self):
         pheaters = self.printer.load_object(self.config, 'heaters')
-        heater = pheaters.heaters.get(self.name)
+        heater_name = self.config.get_name().split()[-1]
+        heater = pheaters.heaters.get(heater_name)
         if heater is None:
-            self.config.error("Heater %s is not registered" % (self.name))
+            self.config.error("Heater %s is not registered" % (heater_name))
         def callback():
-            return float(self.template.render())
+            context = self.create_context()
+            res = self.template.render(context)
+            return float(res)
         heater.set_pc_callback(callback)
-
-    def load_variables(self):
-        import ast
-        import json
-        prefix = 'variable_'
-        for option in self.config.get_prefix_options(prefix):
-            try:
-                literal = ast.literal_eval(self.config.get(option))
-                json.dumps(literal, separators=(',', ':'))
-                self.variables[option[len(prefix):]] = literal
-            except (SyntaxError, TypeError, ValueError) as e:
-                raise self.config.error(
-                    "Option '%s' in section '%s' is not a valid literal: %s" % (
-                        option, self.config.get_name(), e))
-
-    def _log(self, output):
-        logging.info(output)
-        # Make it callable in a block section
-        return ""
-
-    def _get_var(self, name):
-        v = self.variables.get(name)
-        if v is None:
-            self.printer.invoke_shutdown(
-                "variable_%s is not defined" % (name)
-            )
-        return v
-
-    def _set_var(self, name, value):
-        v = self._get_var(name)
-        self.variables[name] = value
-        return v
 
 def load_config_prefix(config):
     return HeaterPredictControl(config)
