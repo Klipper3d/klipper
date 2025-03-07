b50d7405429cbdbac7de8442f9ce31a032c3c584
Timofey Titovets <nefelim4ag@gmail.com>
2025-03-08 00:32:31 +0100

    gcode_macro: Expand template syntax errors (#6839)
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- klippy/extras/gcode_macro.py
+++ klippy/extras/gcode_macro.py
@@ -49,6 +49,12 @@ class TemplateWrapper:
         self.create_template_context = gcode_macro.create_template_context
         try:
             self.template = env.from_string(script)
+        except jinja2.exceptions.TemplateSyntaxError as e:
+            lines = script.splitlines()
+            msg = "Error loading template '%s'\nline %s: %s # %s" % (
+                name, e.lineno, lines[e.lineno-1], e.message)
+            logging.exception(msg)
+            raise self.gcode.error(msg)
         except Exception as e:
             msg = "Error loading template '%s': %s" % (
                  name, traceback.format_exception_only(type(e), e)[-1])
