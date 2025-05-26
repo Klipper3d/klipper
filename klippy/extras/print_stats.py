fb65de57b1ea619a8a94ed02e7eb7cbde611c6c4
Ingo Donasch <ingo@donasch.net>
2025-05-26 11:24:42 +0200

    removed leftover loggin import

--- klippy/extras/print_stats.py
+++ klippy/extras/print_stats.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2020  Eric Callahan <arksine.code@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
 
 class PrintStats:
     def __init__(self, config):
