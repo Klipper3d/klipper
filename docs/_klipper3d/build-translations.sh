4b188832eef82c8ab08f4f162342bca214fd6d21
Timofey Titovets <nefelim4ag@gmail.com>
2025-05-30 03:31:49 +0200

    docs: avoid push of pycache
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- docs/_klipper3d/build-translations.sh
+++ docs/_klipper3d/build-translations.sh
@@ -82,3 +82,6 @@ while IFS="," read dirname langsite langdesc langsearch; do
   ln -sf "${PWD}/site/${langsite}/" "${WORK_DIR}lang/${langsite}/site"
   mkdocs build -f "${new_mkdocs_file}"
 done < <(egrep -v '^ *(#|$)' ${TRANS_FILE})
+
+# Avoid push of pycache
+find "${PWD}/site/" -type f -name "*.pyc" -delete
