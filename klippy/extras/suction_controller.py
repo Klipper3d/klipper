4884b7837e9a557d1b534fa6f9e30b77951828fd
mmokhtar761 <mmokhtar761@gmail.com>
2025-06-27 23:31:58 -0400

    PPS: added a suction control module based on the standard fan module

---
+++ klippy/extras/suction_controller.py
@@ -0,0 +1,107 @@
+#################################################################
+# Klippy Suction control Module @ Albatross
+# Description: This module provides klippy interface for suction
+#               pump control with i2c-based pressure sensor (XGZP6847D)
+# Date: 2025-06-26
+# Version: 0.1
+# Owner: Mo
+#################################################################
+from . import  output_pin
+from XGZP6847D_sensor import PumpPressureSensor
+
+class Pump:
+    def __init__(self, config, default_shutdown_pressure=0.):
+        self.printer = config.get_printer()
+        self.last_fan_value = self.last_req_value = 0.
+        # Read config
+        self.max_power = config.getfloat('max_power', 1., above=0., maxval=1.)
+        self.kick_start_time = config.getfloat('kick_start_time', 0.1,
+                                               minval=0.)
+        self.off_below = config.getfloat('off_below', default=0.,
+                                         minval=0., maxval=1.)
+        cycle_time = config.getfloat('cycle_time', 0.010, above=0.)
+        hardware_pwm = config.getboolean('hardware_pwm', False)
+        shutdown_pressure = config.getfloat(
+            'shutdown_pressure', default_shutdown_pressure, minval=0., maxval=1.)
+        # Setup pwm object
+        ppins = self.printer.lookup_object('pins')
+        self.mcu_fan = ppins.setup_pin('pwm', config.get('pin'))
+        self.mcu_fan.setup_max_duration(0.)
+        self.mcu_fan.setup_cycle_time(cycle_time, hardware_pwm)
+        shutdown_power = max(0., min(self.max_power, shutdown_pressure))
+        self.mcu_fan.setup_start_value(0., shutdown_power)
+
+        self.enable_pin = None
+        enable_pin = config.get('enable_pin', None)
+        if enable_pin is not None:
+            self.enable_pin = ppins.setup_pin('digital_out', enable_pin)
+            self.enable_pin.setup_max_duration(0.)
+
+        # Create gcode request queue
+        self.gcrq = output_pin.GCodeRequestQueue(config, self.mcu_fan.get_mcu(),
+                                                 self._apply_pressure)
+
+        # Setup Pressure sensor
+        self.PressureSensor = PumpPressureSensor(config)
+
+        # Register callbacks
+        self.printer.register_event_handler("gcode:request_restart",
+                                            self._handle_request_restart)
+
+    def get_mcu(self):
+        return self.mcu_fan.get_mcu()
+    def _apply_pressure(self, print_time, value):
+        if value < self.off_below:
+            value = 0.
+        value = max(0., min(self.max_power, value * self.max_power))
+        if value == self.last_fan_value:
+            return "discard", 0.
+        if self.enable_pin:
+            if value > 0 and self.last_fan_value == 0:
+                self.enable_pin.set_digital(print_time, 1)
+            elif value == 0 and self.last_fan_value > 0:
+                self.enable_pin.set_digital(print_time, 0)
+        if (value and self.kick_start_time
+            and (not self.last_fan_value or value - self.last_fan_value > .5)):
+            # Run fan at full pressure for specified kick_start_time
+            self.last_req_value = value
+            self.last_fan_value = self.max_power
+            self.mcu_fan.set_pwm(print_time, self.max_power)
+            return "delay", self.kick_start_time
+        self.last_fan_value = self.last_req_value = value
+        self.mcu_fan.set_pwm(print_time, value)
+    def set_pressure(self, value, print_time=None):
+        self.gcrq.send_async_request(value, print_time)
+    def set_pressure_from_command(self, value):
+        self.gcrq.queue_gcode_request(value)
+    def _handle_request_restart(self, print_time):
+        self.set_pressure(0., print_time)
+
+    def get_status(self, eventtime):
+        PumpPressureSensor_status = self.PumpPressureSensor.get_status(eventtime)
+        return {
+            'Pump Power': self.last_req_value,
+            'Actual Pressure': PumpPressureSensor_status['pressure'],
+        }
+
+
+
+class PrinterPump:
+    def __init__(self, config):
+        self.pump = Pump(config)
+        # Register commands
+        gcode = config.get_printer().lookup_object('gcode')
+        gcode.register_command("M106", self.cmd_M106)
+        gcode.register_command("M107", self.cmd_M107)
+    def get_status(self, eventtime):
+        return self.pump.get_status(eventtime)
+    def cmd_M106(self, gcmd):
+        # Set fan pressure
+        value = gcmd.get_float('P', 255., minval=0.) / 255.
+        self.pump.set_pressure_from_command(value)
+    def cmd_M107(self, gcmd):
+        # Turn fan off
+        self.pump.set_pressure_from_command(0.)
+
+def load_config(config):
+    return PrinterPump(config)
