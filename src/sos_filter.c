f02c41fe7d6e1b23289da3ee24663a984c7c8bd1
Findlay Feng <i@fengch.me>
2025-07-08 17:29:51 +0800

    sos_filter.c: fix overflows_int32
    Modify the inline function overflows_int32 to static inline
    Inline functions cannot be debugged in -O mode
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49653
    
    Signed-off-by: Findlay Feng <i@fengch.me>

--- src/sos_filter.c
+++ src/sos_filter.c
@@ -29,7 +29,7 @@ struct sos_filter {
     struct sos_filter_section filter[0];
 };
 
-inline uint8_t
+static inline uint8_t
 overflows_int32(int64_t value) {
     return value > (int64_t)INT32_MAX || value < (int64_t)INT32_MIN;
 }
