71e63a5717545019be7dbbb2beb459f26f04c785
minicx <minicx@disroot.org>
2025-08-10 13:41:29 +0300

    stm32: Fix N32G45x ADC pin mapping and eliminate lookup loop
    Replace linear search with direct array indexing by inverting the
    adc_pins array structure. Now GPIO pin ID serves as array index
    and contains {adc_num, channel} info.
    
    This fixes PA0 (GPIO 0) incorrectly mapping to ADC1_IN0 due to
    collision with placeholder zeros, and improves performance from
    O(n) to O(1) pin lookup.
    
    Changes:
    - Invert adc_pin_map: index=GPIO_ID, value={adc_num, channel}
    - Remove linear search loop in gpio_adc_setup()
    - Add conditional compilation for N32G455-only ADC3/4 channels
    - Use struct adc_pin_info for type safety
    
    Signed-off-by: minicx <minicx@disroot.org>
    
    Co-authored-by: Alexander Simonov <me@darksimpson.com>

--- src/stm32/n32g45x_adc.c
+++ src/stm32/n32g45x_adc.c
@@ -19,45 +19,65 @@ DECL_CONSTANT("ADC_MAX", 4095);
 #define ADC_TEMPERATURE_PIN 0xfe
 DECL_ENUMERATION("pin", "ADC_TEMPERATURE", ADC_TEMPERATURE_PIN);
 
-static const uint8_t adc_pins[] = {
-    // ADC1
-    0, GPIO('A', 0), GPIO('A', 1), GPIO('A', 6),
-    GPIO('A', 3), GPIO('F', 4), 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    ADC_TEMPERATURE_PIN, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    // ADC2
-    0, GPIO('A', 4), GPIO('A', 5), GPIO('B', 1),
-    GPIO('A', 7), GPIO('C', 4), GPIO('C', 0), GPIO('C', 1),
-    GPIO('C', 2), GPIO('C', 3), GPIO('F', 2), GPIO('A', 2),
-    GPIO('C', 5), GPIO('B', 2), 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-#if CONFIG_MACH_N32G455 // ADC3/4 for G455 only
-    // ADC3
-    0, GPIO('B', 11), GPIO('E', 9), GPIO('E', 13),
-    GPIO('E', 12), GPIO('B', 13), GPIO('E', 8), GPIO('D', 10),
-    GPIO('D', 11), GPIO('D', 12), GPIO('D', 13), GPIO('D', 14),
-    GPIO('B', 0), GPIO('E', 7), GPIO('E', 10), GPIO('E', 11),
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    // ADC4
-    0, GPIO('E', 14), GPIO('E', 15), GPIO('B', 12),
-    GPIO('B', 14), GPIO('B', 15), 0, 0,
-    0, 0, 0, 0,
-    GPIO('D', 8), GPIO('D', 9), 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
+struct adc_pin_info {
+    uint8_t adc_num;
+    uint8_t channel;
+};
+
+static const struct adc_pin_info adc_pin_map[] = {
+    [GPIO('A', 0)] = {1, 1},   // PA0 -> ADC1_IN1
+    [GPIO('A', 1)] = {1, 2},   // PA1 -> ADC1_IN2
+    [GPIO('A', 2)] = {2, 11},  // PA2 -> ADC2_IN11
+    [GPIO('A', 3)] = {1, 4},   // PA3 -> ADC1_IN4
+    [GPIO('A', 4)] = {2, 1},   // PA4 -> ADC2_IN1
+    [GPIO('A', 5)] = {2, 2},   // PA5 -> ADC2_IN2
+    [GPIO('A', 6)] = {1, 3},   // PA6 -> ADC1_IN3
+    [GPIO('A', 7)] = {2, 4},   // PA7 -> ADC2_IN4
+
+#if CONFIG_MACH_N32G455
+    [GPIO('B', 0)] = {3, 12},  // PB0 -> ADC3_IN12
 #endif
+    [GPIO('B', 1)] = {2, 3},   // PB1 -> ADC2_IN3
+    [GPIO('B', 2)] = {2, 13},  // PB2 -> ADC2_IN13
+#if CONFIG_MACH_N32G455
+    [GPIO('B', 11)] = {3, 1},  // PB11 -> ADC3_IN1
+    [GPIO('B', 12)] = {4, 3},  // PB12 -> ADC4_IN3
+    [GPIO('B', 13)] = {3, 5},  // PB13 -> ADC3_IN5
+    [GPIO('B', 14)] = {4, 4},  // PB14 -> ADC4_IN4
+    [GPIO('B', 15)] = {4, 5},  // PB15 -> ADC4_IN5
+#endif
+
+    [GPIO('C', 0)] = {2, 6},   // PC0 -> ADC2_IN6
+    [GPIO('C', 1)] = {2, 7},   // PC1 -> ADC2_IN7
+    [GPIO('C', 2)] = {2, 8},   // PC2 -> ADC2_IN8
+    [GPIO('C', 3)] = {2, 9},   // PC3 -> ADC2_IN9
+    [GPIO('C', 4)] = {2, 5},   // PC4 -> ADC2_IN5
+    [GPIO('C', 5)] = {2, 12},  // PC5 -> ADC2_IN12
+
+#if CONFIG_MACH_N32G455
+    [GPIO('D', 8)] = {4, 12},  // PD8 -> ADC4_IN12
+    [GPIO('D', 9)] = {4, 13},  // PD9 -> ADC4_IN13
+    [GPIO('D', 10)] = {3, 7},  // PD10 -> ADC3_IN7
+    [GPIO('D', 11)] = {3, 8},  // PD11 -> ADC3_IN8
+    [GPIO('D', 12)] = {3, 9},  // PD12 -> ADC3_IN9
+    [GPIO('D', 13)] = {3, 10}, // PD13 -> ADC3_IN10
+    [GPIO('D', 14)] = {3, 11}, // PD14 -> ADC3_IN11
+
+    [GPIO('E', 7)] = {3, 13},  // PE7 -> ADC3_IN13
+    [GPIO('E', 8)] = {3, 6},   // PE8 -> ADC3_IN6
+    [GPIO('E', 9)] = {3, 2},   // PE9 -> ADC3_IN2
+    [GPIO('E', 10)] = {3, 14}, // PE10 -> ADC3_IN14
+    [GPIO('E', 11)] = {3, 15}, // PE11 -> ADC3_IN15
+    [GPIO('E', 12)] = {3, 4},  // PE12 -> ADC3_IN4
+    [GPIO('E', 13)] = {3, 3},  // PE13 -> ADC3_IN3
+    [GPIO('E', 14)] = {4, 1},  // PE14 -> ADC4_IN1
+    [GPIO('E', 15)] = {4, 2},  // PE15 -> ADC4_IN2
+#endif
+
+    [GPIO('F', 2)] = {2, 10},  // PF2 -> ADC2_IN10
+    [GPIO('F', 4)] = {1, 5},   // PF4 -> ADC1_IN5
+
+    [ADC_TEMPERATURE_PIN] = {1, 16},
 };
 
 // Perform calibration
@@ -77,32 +97,36 @@ adc_calibrate(ADC_Module *adc)
 struct gpio_adc
 gpio_adc_setup(uint32_t pin)
 {
-    // Find pin in adc_pins table
-    int chan;
-    for (chan=0; ; chan++) {
-        if (chan >= ARRAY_SIZE(adc_pins))
-            shutdown("Not a valid ADC pin");
-        if (adc_pins[chan] == pin)
-            break;
+    if (pin >= ARRAY_SIZE(adc_pin_map)) {
+        shutdown("ADC pin out of range");
     }
 
-    // Determine which ADC block to use
+    struct adc_pin_info pin_info = adc_pin_map[pin];
+
+    if (pin_info.adc_num == 0) {
+        shutdown("Not a valid ADC pin");
+    }
+
     ADC_Module *adc;
-    if ((chan >> 5) == 0)
-        adc = NS_ADC1;
-    if ((chan >> 5) == 1)
-        adc = NS_ADC2;
-    if ((chan >> 5) == 2)
-        adc = NS_ADC3;
-    if ((chan >> 5) == 3)
-        adc = NS_ADC4;
-    chan &= 0x1F;
+    switch (pin_info.adc_num) {
+        case 1: adc = NS_ADC1; break;
+        case 2: adc = NS_ADC2; break;
+#if CONFIG_MACH_N32G455
+        case 3: adc = NS_ADC3; break;
+        case 4: adc = NS_ADC4; break;
+#endif
+        default: shutdown("Invalid ADC number");
+    }
 
     // Enable the ADC
     uint32_t reg_temp;
     reg_temp = ADC_RCC_AHBPCLKEN;
+#if CONFIG_MACH_N32G455
     reg_temp |= (RCC_AHB_PERIPH_ADC1 | RCC_AHB_PERIPH_ADC2 |
                 RCC_AHB_PERIPH_ADC3 | RCC_AHB_PERIPH_ADC4);
+#else
+    reg_temp |= (RCC_AHB_PERIPH_ADC1 | RCC_AHB_PERIPH_ADC2);
+#endif
     ADC_RCC_AHBPCLKEN = reg_temp;
 
     reg_temp = ADC_RCC_CFG2;
@@ -134,7 +158,7 @@ gpio_adc_setup(uint32_t pin)
         gpio_peripheral(pin, GPIO_ANALOG, 0);
     }
 
-    return (struct gpio_adc){ .adc = adc, .chan = chan };
+    return (struct gpio_adc){ .adc = adc, .chan = pin_info.channel };
 }
 
 // Try to sample a value. Returns zero if sample ready, otherwise
