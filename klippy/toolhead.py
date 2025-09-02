20d9c84a9f78f418da3b098147002a8edff02dda
Kevin O'Connor <kevin@koconnor.net>
2025-09-01 21:20:00 -0400

    toolhead: Fix incorrect response message in SET_VELOCITY_LIMIT
    Commit f8da8099 incorrectly changed the order of variables in the
    log/response message of the SET_VELOCITY_LIMIT command.  Restore the
    correct order.
    
    Reported by @berkakinci.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/toolhead.py
+++ klippy/toolhead.py
@@ -662,7 +662,7 @@ class ToolHeadCommandHelper:
         msg = ("max_velocity: %.6f\n"
                "max_accel: %.6f\n"
                "minimum_cruise_ratio: %.6f\n"
-               "square_corner_velocity: %.6f" % (mv, ma, scv, mcr))
+               "square_corner_velocity: %.6f" % (mv, ma, mcr, scv))
         self.printer.set_rollover_info("toolhead", "toolhead: %s" % (msg,))
         if (max_velocity is None and max_accel is None
             and square_corner_velocity is None and min_cruise_ratio is None):
