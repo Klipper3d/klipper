22db9bb84e26eeef5b1281707fd536829c5933af
Kevin O'Connor <kevin@koconnor.net>
2025-09-07 12:18:28 -0400

    motion_queuing: Require explicit notification on a scan window change
    Don't try to infer when the step generation scan window may change.
    Instead, require the input_shaper and pressure_advance code call
    motion_queuing.check_step_generation_scan_windows() any time a
    scanning window may change.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/extras/input_shaper.py
+++ klippy/extras/input_shaper.py
@@ -138,6 +138,7 @@ class InputShaper:
         if self.toolhead is None:
             # Klipper initialization is not yet completed
             return
+        self.toolhead.flush_step_generation()
         ffi_main, ffi_lib = chelper.get_ffi()
         kin = self.toolhead.get_kinematics()
         for s in kin.get_steppers():
@@ -146,8 +147,9 @@ class InputShaper:
             is_sk = self._get_input_shaper_stepper_kinematics(s)
             if is_sk is None:
                 continue
-            self.toolhead.flush_step_generation()
             ffi_lib.input_shaper_update_sk(is_sk)
+        motion_queuing = self.printer.lookup_object("motion_queuing")
+        motion_queuing.check_step_generation_scan_windows()
     def _update_input_shaping(self, error=None):
         self.toolhead.flush_step_generation()
         ffi_main, ffi_lib = chelper.get_ffi()
@@ -164,6 +166,8 @@ class InputShaper:
                     continue
                 if not shaper.set_shaper_kinematics(is_sk):
                     failed_shapers.append(shaper)
+        motion_queuing = self.printer.lookup_object("motion_queuing")
+        motion_queuing.check_step_generation_scan_windows()
         if failed_shapers:
             error = error or self.printer.command_error
             raise error("Failed to configure shaper(s) %s with given parameters"
