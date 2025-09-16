c7365c8c585bb3127280651b95b0e9b87e5bdf77
Kevin O'Connor <kevin@koconnor.net>
2025-09-16 11:28:16 -0400

    extruder: Recheck the step generation scan windows on sync_to_extruder()
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- klippy/kinematics/extruder.py
+++ klippy/kinematics/extruder.py
@@ -50,9 +50,11 @@ class ExtruderStepper:
     def sync_to_extruder(self, extruder_name):
         toolhead = self.printer.lookup_object('toolhead')
         toolhead.flush_step_generation()
+        motion_queuing = self.printer.lookup_object('motion_queuing')
         if not extruder_name:
             self.stepper.set_trapq(None)
             self.motion_queue = None
+            motion_queuing.check_step_generation_scan_windows()
             return
         extruder = self.printer.lookup_object(extruder_name, None)
         if extruder is None or not isinstance(extruder, PrinterExtruder):
@@ -61,6 +63,7 @@ class ExtruderStepper:
         self.stepper.set_position([extruder.last_position, 0., 0.])
         self.stepper.set_trapq(extruder.get_trapq())
         self.motion_queue = extruder_name
+        motion_queuing.check_step_generation_scan_windows()
     def _set_pressure_advance(self, pressure_advance, smooth_time):
         old_smooth_time = self.pressure_advance_smooth_time
         if not self.pressure_advance:
