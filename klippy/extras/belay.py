d61bf239bec00f03e36949bf60fefd970c3883c4
HugoCLSC <hugo.santos.costa@gmail.com>
2025-10-28 19:46:10 +0000

    Update belay module

--- klippy/extras/belay.py
+++ klippy/extras/belay.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023-2024 Ryan Ghosh <rghosh776@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+
 DIRECTION_UPDATE_INTERVAL = 0.1
 POSITION_TIME_DIFF = 0.3
 
@@ -14,7 +15,16 @@ class Belay:
 
         # initial type-specific setup
         type_options = ["trad_rack", "extruder_stepper"]
-        self.type = config.getchoice("extruder_type", {t: t for t in type_options})
+        self.type = config.getchoice(
+            "extruder_type", {t: t for t in type_options}
+        )
+
+        self.stuck_timeout = config.getfloat("stuck_timeout", 5.0, above=1)
+        self.curr_stuck_timer = 0.0
+        self.printer.register_event_handler(
+            "idle_timeout:printing", self.handle_printing
+        )
+
         if self.type == "trad_rack":
             enable_events = ["trad_rack:synced_to_extruder"]
             disable_events = ["trad_rack:unsyncing_from_extruder"]
@@ -26,7 +36,9 @@ class Belay:
             self.enable_initial = True
 
         # register event handlers
-        self.printer.register_event_handler("klippy:connect", self.handle_connect)
+        self.printer.register_event_handler(
+            "klippy:connect", self.handle_connect
+        )
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
         for event in enable_events:
             self.printer.register_event_handler(event, self.handle_enable)
@@ -59,8 +71,13 @@ class Belay:
         self.disable_conditions = [lambda: self.enabled]
         self.gcode = self.printer.lookup_object("gcode")
         self.toolhead = None
-        self.update_direction_timer = self.reactor.register_timer(self.update_direction)
+        self.update_direction_timer = self.reactor.register_timer(
+            self.update_direction
+        )
 
+        self.update_stuck_timer = self.reactor.register_timer(
+            self.update_verify_stuck
+        )
         # register commands
         self.gcode.register_mux_command(
             "QUERY_BELAY",
@@ -76,6 +93,22 @@ class Belay:
             self.cmd_BELAY_SET_MULTIPLIER,
             desc=self.cmd_BELAY_SET_MULTIPLIER_help,
         )
+
+        # self.gcode.register_mux_command(
+        #     "QUERY_FILAMENT_TRACK_STATUS",
+        #     "BELAY",
+        #     self.name,
+        #     self.cmd_QUERY_FILAMENT_TRACK_STATUS,
+        #     desc=self.cmd_QUERY_FILAMENT_TRACK_STATUS_help,
+        # )
+
+        # self.gcode.register_mux_command(
+        #     "SET_BELAY_FILAMENT_TRACK",
+        #     "BELAY",
+        #     self.name,
+        #     self.cmd_SET_BELAY_FILAMENT_TRACK,
+        #     desc=self.cmd_SET_BELAY_FILAMENT_TRACK_help,
+        # )
 
         # register extruder_stepper-only commands
         if self.type == "extruder_stepper":
@@ -118,7 +151,9 @@ class Belay:
             if not condition():
                 return
         self.enabled = True
-        self.reactor.update_timer(self.update_direction_timer, self.reactor.NOW)
+        self.reactor.update_timer(
+            self.update_direction_timer, self.reactor.NOW
+        )
         self.update_multiplier()
 
     def handle_disable(self):
@@ -126,12 +161,24 @@ class Belay:
             if not condition():
                 return
         self.reset_multiplier()
-        self.reactor.update_timer(self.update_direction_timer, self.reactor.NEVER)
+        self.reactor.update_timer(
+            self.update_direction_timer, self.reactor.NEVER
+        )
         self.enabled = False
 
     def sensor_callback(self, eventtime, state):
         self.last_state = state
         if self.enabled:
+            virtual_sdcard = self.printer.lookup_object("virtual_sdcard")
+            file_pos = virtual_sdcard.get_status(eventtime).get(
+                "file_position"
+            )
+            if file_pos > 1:
+                self.timeout = self.reactor.monotonic() + self.stuck_timeout
+                if self.debug_level >= 1:
+                    self.gcode.respond_info(
+                        "Belay Pressure changed, increasing stuck timeout"
+                    )
             self.update_multiplier()
 
     def update_multiplier(self, print_msg=True):
@@ -141,9 +188,10 @@ class Belay:
         else:
             # compressed/backward or expanded/forward
             multiplier = self.multiplier_low
+
         self.set_multiplier(multiplier)
-        
-        if (print_msg and self.debug_level >= 1) :
+
+        if (print_msg and self.debug_level >= 1) or self.debug_level >= 2:
             self.gcode.respond_info(
                 "Set secondary extruder multiplier: %f" % multiplier
             )
@@ -168,8 +216,35 @@ class Belay:
                 self.gcode.respond_info(
                     "New Belay sensor direction: %s" % self.last_direction
                 )
+
             self.update_multiplier(False)
+
         return eventtime + DIRECTION_UPDATE_INTERVAL
+
+    def handle_printing(self, eventtime) -> None:
+        self.timeout = self.reactor.monotonic() + self.stuck_timeout
+        self.reactor.update_timer(self.update_stuck_timer, self.reactor.NOW)
+
+    def update_verify_stuck(self, eventtime) -> float:
+        idle_timeout = self.printer.lookup_object("idle_timeout")
+        state = idle_timeout.get_status(eventtime).get("state")
+        virtual_sdcard = self.printer.lookup_object("virtual_sdcard")
+
+        if state.lower() == "printing":
+            if self.debug_level >= 1:
+                self.gcode.respond_info(
+                    f"Belay stuck with timeout {self.timeout} timer: {self.reactor.monotonic()}"
+                )
+
+            if self.timeout > self.reactor.monotonic():
+                return self.reactor.monotonic() + 1.0
+
+            self.gcode.respond_info("Belay reports filament Stuck.")
+            self.gcode.run_script_from_command("PAUSE")
+            return self.reactor.NEVER
+        else:
+            self.curr_stuck_timer = 0
+            return self.reactor.NEVER
 
     cmd_QUERY_BELAY_help = "Report Belay sensor state"
 
@@ -181,11 +256,14 @@ class Belay:
         self.gcode.respond_info("belay {}: {}".format(self.name, state_info))
 
     cmd_BELAY_SET_MULTIPLIER_help = (
-        "Sets multiplier_high and/or multiplier_low. Does not persist across restarts."
+        "Sets multiplier_high and/or multiplier_low. Does not persist across"
+        " restarts."
     )
 
     def cmd_BELAY_SET_MULTIPLIER(self, gcmd):
-        self.multiplier_high = gcmd.get_float("HIGH", self.multiplier_high, minval=1.0)
+        self.multiplier_high = gcmd.get_float(
+            "HIGH", self.multiplier_high, minval=1.0
+        )
         self.multiplier_low = gcmd.get_float(
             "LOW", self.multiplier_low, minval=0.0, maxval=1.0
         )
@@ -198,6 +276,9 @@ class Belay:
         self.handle_disable()
         self._set_extruder_stepper(gcmd.get("STEPPER"))
         self.handle_enable()
+
+    def cmd_QUERY_FILAMENT_TRACK_STATUS(self, gcmd) -> None: ...
+    def cmd_SET_BELAY_FILAMENT_TRACK(self, gcmd) -> None: ...
 
     def get_status(self, eventtime):
         return {"last_state": self.last_state, "enabled": self.enabled}
