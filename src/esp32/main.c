91a40a19b985aec441c9679d437eea438ff3173f
Fermín Olaiz <ferminolaiz@gmail.com>
2025-11-11 16:15:20 -0300

    main: bypass FreeRTOS initialization

--- src/esp32/main.c
+++ src/esp32/main.c
@@ -1,103 +1,29 @@
 #include "main.h"
 
-#include <string.h>
-
 #include "command.h"
-#include "esp_log.h"
-#include "esp_task.h"
-#include "esp_task_wdt.h"
-#include "freertos/FreeRTOS.h"
-#include "sched.h"
+#include "esp_private/startup_internal.h"
+#include "esp_private/system_internal.h"
+#include "esp_rom_sys.h"
 
-void app_main()
+/**
+ * Hook into ESP secondary init functions to avoid initializing FreeRTOS [1].
+ *
+ * [1] https://github.com/fermino/esp-idf-example-no-freertos/blob/main/main/esp-no-freertos.c
+ */
+ESP_SYSTEM_INIT_FN(main_core0, SECONDARY, BIT(0), 1000)
 {
-    // @todo the stack depth is a wild guess, check it
-    xTaskCreate(
-        task_call_sched_main,
-        "sched_main",
-        8192,
-        NULL,
-        ESP_TASK_PRIO_MAX - 1,
-        NULL
-    );
-}
+    // Let UART finish sending init buffer's content
+    esp_rom_delay_us(500 * 1000);
 
-static void task_call_sched_main()
-{
-    // Give time to the IDLE task to reap app_main's memory before start breaking stuff :)
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    neutralize_freertos();
-
-    // Main klipper loop
     sched_main();
-
-    // If we're here, something went wrooong
-    try_shutdown("main loop finished");
-    // @todo implementar esp restart como tarea en el shutdown
-    esp_restart();
-    vTaskDelete(NULL);
+    try_shutdown("Main stopped.");
+    esp_restart_noos();
 }
 
-/**
- * ESP-IDF by default initializes a set of tasks that include FreeRTOS
- * housekeeping (2x IDLEx), Inter-Processor Call (2x IPCx) and the main task
- * (app_main). Using CONFIG_FREERTOS_UNICORE we can get rid of the IPC stuff,
- * and because the idle task only takes care of freeing memory from other
- * tasks, we can starve it (per the docs) to effectively get rid of all
- * context switches in our main task.
- *
- * @todo see if we can reduce FreeRTOS' frequency and if it's effective at
- * avoiding interrupts
- *
- * As a reference, our current tasks look like this (vTaskList()):
- * Name     State   Priority    StackSize   Num     CoreID
- * *******************************************************
- * main     X       1           1220        1       0
- * IDLE     R       0           1240        2       0
- *
- * See also:
- * https://docs.espressif.com/projects/esp-idf/en/v5.5.1/esp32/api-reference/system/freertos.html#id3
- * https://www.freertos.org/Documentation/02-Kernel/02-Kernel-features/01-Tasks-and-co-routines/15-Idle-task
- */
-static void neutralize_freertos()
-{
-    // Neutralize the TWDT for some tasks so that when we starve them we don't get an exception
-    neutralize_freertos_twdt();
-}
-
-/**
- * Disables the Task Watchdog Timer (TWDT) for the tasks that we will later
- * starve. See the docs at neutralize_freertos().
- *
- * See also:
- * https://docs.espressif.com/projects/esp-idf/en/v5.5.1/esp32/api-reference/system/wdts.html#task-watchdog-timer-twdt
- */
-static void neutralize_freertos_twdt()
-{
-    UBaseType_t tasks_n = uxTaskGetNumberOfTasks();
-    TaskStatus_t tasks[tasks_n];
-    uxTaskGetSystemState(tasks, tasks_n, NULL);
-
-    for (size_t i = 0; i < tasks_n; i++)
-    {
-        if (strcmp(tasks[i].pcTaskName, "IDLE") == 0)
-        {
-            ESP_LOGW(__FILE__, "Stopping TWDT for task %s", tasks[i].pcTaskName);
-            // @todo esp_task_wdt_deinit() ?
-            esp_task_wdt_delete(tasks[i].xHandle);
-            return;
-        }
-    }
-}
-
-
-// @todo: ??
+void command_reset(uint32_t* args) { esp_restart_noos(); }
 DECL_COMMAND_FLAGS(command_reset, HF_IN_SHUTDOWN, "reset");
 
-void command_reset(uint32_t* args)
-{
-    // timer_reset();
-    // restore_data();
-    // void *reset = (void *)RESET_VECTOR;
-    // goto *reset;
-}
+/**
+ * Placeholder to avoid linker errors
+ */
+void app_main() { esp_restart_noos(); }
