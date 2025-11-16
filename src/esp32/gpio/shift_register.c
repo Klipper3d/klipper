35398e108af1cf177bbd51fc0d5bbb43d63b8d8f
Fermín Olaiz <ferminolaiz@gmail.com>
2025-11-16 00:30:11 -0300

    Some working SR

--- src/esp32/gpio/shift_register.c
+++ src/esp32/gpio/shift_register.c
@@ -1,6 +1,8 @@
 #include "gpio/shift_register.h"
+#include "autoconf.h"
 #include "command.h"
 #include "esp_clk_tree.h"
+#include "esp_log.h"
 #include "esp_private/periph_ctrl.h"
 #include "esp_private/spi_common_internal.h"
 #include "hal/clk_tree_hal.h"
@@ -8,65 +10,62 @@
 #include "hal/spi_hal.h"
 #include "hal/spi_ll.h"
 
-// @todo move to menuconfig
-#define SPI_SR_BUS          (SPI2_HOST)
-// @todo test with 5 or 10MHz and check timings with logic analyzer
-#define SPI_SR_CLOCK_FREQ   (1 * 1000 * 1000)
-#define SPI_SR_DATA_PIN     (11)
-#define SPI_SR_CLK_PIN      (12)
-#define SPI_SR_LATCH_PIN    (13)
-#define SPI_SR_BYTE_NO      (2)
-#define SPI_SR_BIT_NO       (SPI_SR_BYTE_NO * 8)
-// @todo check max
+#if CONFIG_HAVE_GPIO_SR
+
+// @todo clause
+
+#define SR_SPI_HOST (SPI2_HOST)
+#define SR_BIT_NO   (CONFIG_SR_BYTE_NO * 8)
 
 // @todo -1?
-DECL_ENUMERATION_RANGE("pin", "SR_0", (1u << 7), SPI_SR_BIT_NO);
+// @todo check max
+DECL_ENUMERATION_RANGE("pin", "SR_0", (1u << 7), SR_BIT_NO);
 
 spi_dev_t* spi = NULL;
-
-volatile static uint8_t sr_data[SPI_SR_BYTE_NO];
+volatile static uint8_t sr_data[CONFIG_SR_BYTE_NO];
 
 void sr_init()
 {
-    spi = SPI_LL_GET_HW(SPI_SR_BUS);
+    spi = SPI_LL_GET_HW(SR_SPI_HOST);
 
     /**
      * Enable state machine clocks and reset everything
      */
     PERIPH_RCC_ATOMIC() {
-        spi_ll_enable_bus_clock(SPI_SR_BUS, true);
-        spi_ll_reset_register(SPI_SR_BUS);
+        spi_ll_enable_bus_clock(SR_SPI_HOST, true);
+        spi_ll_reset_register(SR_SPI_HOST);
     }
 
     /**
      * Configure IO
      */
     ESP_ERROR_CHECK(spicommon_bus_initialize_io(
-        SPI_SR_BUS,
+        SR_SPI_HOST,
         &(spi_bus_config_t) {
+            .mosi_io_num = CONFIG_SR_PIN_DATA,
+            .sclk_io_num = CONFIG_SR_PIN_CLK,
             .miso_io_num = -1,
-            .mosi_io_num = 5,
-            .sclk_io_num = 6,
             .quadwp_io_num = -1,
             .quadhd_io_num = -1,
         },
         SPICOMMON_BUSFLAG_MASTER,
         NULL
     ));
-    spicommon_cs_initialize(SPI_SR_BUS, 7, 0, 1);
+    spicommon_cs_initialize(SR_SPI_HOST, CONFIG_SR_PIN_LATCH, 0, 1);
 
     /**
      * Set up the whoooole thing
      */
-    spi_ll_enable_clock(SPI_SR_BUS, true);
+    spi_ll_enable_clock(SR_SPI_HOST, true);
     spi_ll_set_clk_source(spi, SPI_CLK_SRC_APB);
     spi_ll_master_init(spi);
     spi_ll_master_set_line_mode(spi, (spi_line_mode_t) { 1, 1, 1});
-    spi_ll_master_set_clock(spi, (int)clk_hal_apb_get_freq_hz(), SPI_SR_CLOCK_FREQ, 128);
+    spi_ll_master_set_clock(spi, (int)clk_hal_apb_get_freq_hz(), (CONFIG_SR_CLOCK_FREQ_MHZ * 1000 * 1000), 128);
     spi_ll_master_set_mode(spi, 0);
     spi_ll_master_select_cs(spi, 0);
     spi_ll_master_set_cs_setup(spi, 0);
     spi_ll_master_set_cs_hold(spi, 1);
+    // Set LATCH polarity (SPI CS)
     spi_ll_master_set_pos_cs(spi, 0, 0);
     spi_ll_master_keep_cs(spi, 0);
     spi_ll_set_half_duplex(spi, 0);
@@ -80,14 +79,32 @@ void sr_init()
     spi_ll_set_addr_bitlen(spi, 0);
     spi_ll_set_miso_bitlen(spi, 0);
     spi_ll_clear_int_stat(spi);
+    // Send LSB last
     spi_ll_set_tx_lsbfirst(spi, 0);
-    spi_ll_set_mosi_bitlen(spi, SPI_SR_BIT_NO);
+    spi_ll_set_mosi_bitlen(spi, SR_BIT_NO);
     spi_ll_enable_mosi(spi, true);
     spi_ll_apply_config(spi);
 
     // Initial write to the SR to set it to a known state
-    memset(sr_data, 0, SPI_SR_BYTE_NO);
+    for (uint8_t i = 0; i < CONFIG_SR_BYTE_NO; i++) {
+        sr_data[i] = 0;
+    }
     sr_write();
+
+
+    struct gpio_out gp = { .pin = 128 };
+    struct gpio_out gp2 = { .pin = 141 };
+
+    while (1) {
+        gpio_sr_write(gp, 1);
+        esp_rom_delay_us(5);
+        gpio_sr_write(gp2, 0);
+        esp_rom_delay_us(5);
+        gpio_sr_write(gp, 0);
+        esp_rom_delay_us(5);
+        gpio_sr_write(gp2, 1);
+        esp_rom_delay_us(5);
+    }
 }
 DECL_INIT(sr_init);
 
@@ -95,6 +112,26 @@ DECL_INIT(sr_init);
 // @todo check if transfer finished
 void sr_write()
 {
-    spi_ll_write_buffer(spi, sr_data, SPI_SR_BIT_NO);
+    uint8_t local_buffer[CONFIG_SR_BYTE_NO];
+    for (uint8_t i = 0; i < CONFIG_SR_BYTE_NO; i++) {
+        local_buffer[i] = sr_data[CONFIG_SR_BYTE_NO - 1 - i];
+    }
+    spi_ll_write_buffer(spi, local_buffer, SR_BIT_NO);
     spi_ll_user_start(spi);
 }
+
+void gpio_sr_write(struct gpio gpio, bool val)
+{
+    uint8_t bit_index = gpio.pin & 0b111;
+    uint8_t byte_index = (gpio.pin & 0b01111000) >> 3;
+
+    if (val) {
+        sr_data[byte_index] |= 1 << bit_index;
+    } else {
+        sr_data[byte_index] &= ~(1 << bit_index);
+    }
+
+    sr_write();
+}
+
+#endif
--- src/esp32/gpio/shift_register.h
+++ src/esp32/gpio/shift_register.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include "gpio/gpio.h"
+#include "autoconf.h"
+#include <stdbool.h>
+
+#define SR_GPIO_BITMASK (1ul << 7)
+
 void sr_write();
+void gpio_sr_write(struct gpio gpio, bool val);
+
+/**
+ * This function should be optimized out if the feature is
+ * disabled (CONFIG_HAVE_GPIO_SR == false)
+ */
+static inline bool __attribute__((always_inline)) gpio_is_sr(struct gpio gpio)
+{
+    return CONFIG_HAVE_GPIO_SR && gpio.pin & SR_GPIO_BITMASK;
+}
