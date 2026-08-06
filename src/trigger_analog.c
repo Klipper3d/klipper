Klipper3d/klipper

commit cab2f00408daa56d4006e91244a7427b194eea06

trigger_analog: Fix TT_ABS_GE handling on AVR mcu
The abs() function on AVR only handles 16-bit integers.  Implement
it manually.

Reported by @kmatzen.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/trigger_analog.c
@@ -5,7 +5,6 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include <stdlib.h> // abs
 #include "basecmd.h" // oid_alloc
 #include "board/io.h" // writeb
 #include "board/misc.h" // timer_read_time
@@ -90,7 +89,8 @@ check_trigger(struct trigger_analog *ta, uint32_t time, int32_t value)
 {
     switch (ta->trigger_type) {
     case TT_ABS_GE:
-        return abs(value) >= ta->trigger_value;
+        uint32_t abs_value = value < 0 ? -value : value;
+        return abs_value >= ta->trigger_value;
     case TT_GT:
         return value > ta->trigger_value;
     case TT_DIFF_PEAK_GT:

commit 07b9dff8f0e9092a3d5c8643d5313585f20f43e4

trigger_analog: Fix build error from label followed by declaration
Commit cab2f00 introduced a variable declaration directly after
a case label in check_trigger(), which some C compilers (eg,
gcc 10.2.1) reject with "a label can only be part of a statement
and a declaration is not a statement". Scope the case section
with braces so it builds correctly.

Signed-off-by: Leon Andrea Loeser <info@leon-loeser.de>

src/trigger_analog.c
@@ -88,9 +88,10 @@ static int
 check_trigger(struct trigger_analog *ta, uint32_t time, int32_t value)
 {
     switch (ta->trigger_type) {
-    case TT_ABS_GE:
+    case TT_ABS_GE: {
         uint32_t abs_value = value < 0 ? -value : value;
         return abs_value >= ta->trigger_value;
+    }
     case TT_GT:
         return value > ta->trigger_value;
     case TT_DIFF_PEAK_GT: